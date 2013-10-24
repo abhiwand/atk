465716d5d7107838a3f1cf49cbd4aea68ce64c57
rodorad <rene.o.dorado@intel.com>
2013-10-24 11:17:25 -0700

    finished templating the html.  broke up the mock html to has many reusable sections has possible to make reuse easy going forward.

--- tribeca-web/app/controllers/Application.scala
+++ tribeca-web/app/controllers/Application.scala
@@ -2,11 +2,14 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.libs.Json
 
 object Application extends Controller {
 
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.index("Index"))
   }
+
+
 
 }
