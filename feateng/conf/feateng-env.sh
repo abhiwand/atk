28f022b3315f2f0709c4abae31a119fdf51f27b1
Nezih Yigitbasi <nezih.yigitbasi@intel.com>
2013-10-25 09:23:16 +0300

    TRIB-740 added a configuration script to set the environment variables feature engineering depends on

---
+++ feateng/conf/feateng-env.sh
@@ -0,0 +1,8 @@
+export FEATURE_ENG_HOME=/home/user/nezih/workspace/feature_engineering
+export PIG_HOME=/home/user/pig-0.12.0
+export PATH=$PIG_HOME/bin:$PATH
+export JYTHONPATH=$FEATURE_ENG_HOME/py-scripts/
+
+echo "set FEATURE_ENG_HOME to $FEATURE_ENG_HOME"
+echo "set PIG_HOME to $PIG_HOME"
+echo "set JYTHONPATH to $JYTHONPATH"
