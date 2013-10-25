f5181395ea54862cf9fa98a4a0ed97761e78d5ff
rodorad <rene.o.dorado@intel.com>
2013-10-25 09:55:46 -0700

    a register controller that right now parses json and is trying to do the auth token request to google

---
+++ tribeca-web/app/controllers/Register.scala
@@ -0,0 +1,49 @@
+package controllers
+
+import play.api.mvc._
+import play.api.libs.json._
+import com.fasterxml.jackson.databind.JsonNode
+import play.api.libs.functional.syntax._
+import play.api.libs.ws.WS
+
+object Register extends Controller{
+  implicit val rds = (
+      (__ \ '_aa).read[String] and
+      (__ \ 'access_token).read[String] and
+      (__ \ 'authuser).read[String] and
+      (__ \ 'client_id).read[String] and
+      (__ \ 'code).read[String] and
+      (__ \ 'cookie_policy).read[String] and
+      (__ \ 'expires_at).read[String] and
+      (__ \ 'expires_in).read[String] and
+      (__ \ 'g_user_cookie_policy).read[String] and
+      (__ \ 'id_token).read[String] and
+      (__ \ 'issued_at).read[String] and
+      (__ \ 'prompt).read[String] and
+      (__ \ 'response_type).read[String] and
+      (__ \ 'scope).read[String] and
+      (__ \ 'session_state).read[String] and
+      (__ \ 'state).read[String] and
+      (__ \ 'token_type).read[String]
+    )tupled
+
+  val clientId = "141308260505-jf332k2mi49jggi2cugf08vk17u9s9rk.apps.googleusercontent.com"
+  val clientSecret = "0fp9P9isYAz_vrlyA9I1Jk_j"
+  val tokenVerify = "https://www.googleapis.com/oauth2/v1/tokeninfo?access_token="
+  var register = Action(parse.json){ request =>
+       request.body.validate.map{
+         case(aa, access_token, authUser, client_id, code, cookie_policy, expires_at, expires_in, g_user_cookie_policy,
+           id_token, issued_at, prompt, response_type, scope, session_state, state, token_type) =>
+
+           //val response = WS.url(tokenVerify).withQueryString("access_token", access_token).get();
+           //val body = response.value.get.
+
+           Ok(Json.obj("_access_token" -> access_token))
+       }.recoverTotal{
+          e => BadRequest("bad 123 request")
+       }
+
+
+     }
+
+}
