3f38bcb665ce162ccbf2fe51b21512e8c9e1949d
Nezih Yigitbasi <nezih.yigitbasi@intel.com>
2013-10-25 09:26:00 +0300

    TRIB-740 added a warning to the configuration script.

--- feateng/conf/feateng-env.sh
+++ feateng/conf/feateng-env.sh
@@ -3,6 +3,7 @@ export PIG_HOME=/home/user/pig-0.12.0
 export PATH=$PIG_HOME/bin:$PATH
 export JYTHONPATH=$FEATURE_ENG_HOME/py-scripts/
 
+echo "Don't forget to use the 'source' command to make these variables available to the current shell."
 echo "set FEATURE_ENG_HOME to $FEATURE_ENG_HOME"
 echo "set PIG_HOME to $PIG_HOME"
 echo "set JYTHONPATH to $JYTHONPATH"
