3b5be2ba9324695ae90faa2afaa3ba14a034cf54
rodorad <rene.o.dorado@intel.com>
2013-10-28 16:45:30 -0700

    update db schema to remove the picture url

--- tribeca-web/app/models/database/Users.scala
+++ tribeca-web/app/models/database/Users.scala
@@ -3,7 +3,7 @@ package models.database
 import play.api.db.slick.Config.driver.simple._
 
 case class User(uid: Option[Long], givenName:String, familyName:String, email:String, phone:String,
-                company:String, companyEmail:String, picture:String, registered:Boolean, clusterId:Option[String])
+                company:String, companyEmail:String, registered:Boolean, clusterId:Option[String])
 
 class Users extends Table[User]("user_info") {
   def uid = column[Long]("uid", O.PrimaryKey, O.AutoInc)
@@ -13,8 +13,8 @@ class Users extends Table[User]("user_info") {
   def phone = column[String]("phone")
   def company = column[String]("organization_name")
   def companyEmail = column[String]("organization_email")
-  def picture = column[String]("picture")
+  //def picture = column[String]("picture")
   def registered = column[Boolean]("registered")
   def clusterId = column[String]("cluster_id", O.Nullable)
-  def * = uid.? ~ givenName ~ familyName ~ email ~ phone ~ company ~ companyEmail ~ picture ~ registered ~ clusterId.? <> (User, User.unapply _)
+  def * = uid.? ~ givenName ~ familyName ~ email ~ phone ~ company ~ companyEmail ~ registered ~ clusterId.? <> (User, User.unapply _)
 }
