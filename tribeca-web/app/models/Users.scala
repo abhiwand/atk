d7ff33f1a51ecd363d979466621a57b57ff8a81e
schen55 <steven.shih-chi.chen@intel.com>
2013-10-29 14:30:27 -0700

    refactor

--- tribeca-web/app/models/Users.scala
+++ tribeca-web/app/models/Users.scala
@@ -10,16 +10,16 @@ import slick.driver.ExtendedProfile
 import play.api.db.slick.Profile
 import play.api.db.slick.DB
 
-import java.sql.{Types, CallableStatement}
+import java.sql.{ResultSet, Types, CallableStatement}
 
 object Users {
     var table = new database.Users
 
-    def register(user: User): RegistrationOutput = DB.withSession {
+    def register(user: User, statementGenerator : StatementGenerator): RegistrationOutput = DB.withSession {
         implicit session: scala.slick.session.Session =>
 
-
-            val cStmt: CallableStatement = session.conn.prepareCall("{call sp_register(?, ?, ?, ?, ?, ?, ?, ?, ?)}")
+            var callString =  "{call sp_register(?, ?, ?, ?, ?, ?, ?, ?, ?)}";
+            var cStmt = statementGenerator.getCallStatement(session, callString)
             cStmt.setString("given_name", user.givenName)
             cStmt.setString("family_name", user.familyName)
             cStmt.setString("email", user.email)
@@ -35,22 +35,31 @@ object Users {
             val errorCode = cStmt.getInt("errorCode")
             val errorMessage = cStmt.getString("errorMessage")
 
-
             var uid = 0
             if(hadResults) {
-                val rs = cStmt.getResultSet();
-                if(rs.next()) uid = rs.getInt("uid")
+                uid = getUidFromResultSet(cStmt.getResultSet())
             }
 
             val output = new RegistrationOutput(errorCode, errorMessage, loginAfterRegister, uid)
             return output
     }
 
-    def login(user: User) : LoginOutput   = DB.withSession {
+
+    private def getCallStatement(session: Session, callString: String): CallableStatement = {
+        return session.conn.prepareCall(callString)
+    }
+
+    private def getUidFromResultSet(rs: ResultSet): Int = {
+        var uid = 0
+        if (rs.next()) uid = rs.getInt("uid")
+        return uid
+    }
+
+    def login(user: User, statementGenerator : StatementGenerator) : LoginOutput   = DB.withSession {
         implicit session: scala.slick.session.Session =>
 
-
-            val cStmt: CallableStatement = session.conn.prepareCall("{call sp_login(?, ?, ?, ?)}")
+            var callString =  "{call sp_login(?, ?, ?, ?)}";
+            var cStmt = statementGenerator.getCallStatement(session, callString)
             cStmt.setString("email", user.email)
             cStmt.registerOutParameter("loginSuccessful", Types.BIGINT)
             cStmt.registerOutParameter("errorCode", Types.BIGINT)
@@ -63,8 +72,7 @@ object Users {
 
             var uid = 0
             if(hadResults) {
-                val rs = cStmt.getResultSet();
-                if(rs.next()) uid = rs.getInt("uid")
+                uid = getUidFromResultSet(cStmt.getResultSet())
             }
 
             val output = new LoginOutput(errorCode, errorMessage, loginSuccessful, uid)
@@ -81,31 +89,6 @@ object Users {
       return users.last
     }else{
       return null
-    }
-  }
-
-
-  def insert(user: User): Long = DB.withSession{ implicit session: scala.slick.session.Session =>
-
-    var uid = findByEmail(user.email)
-    if(uid == 0){
-      uid = table.insert(user)
-    }
-    return uid
-  }
-
-  val emailExist =for{
-    email <- Parameters[String]
-    u <- table if u.email === email
-  }yield u
-
-  def findByEmail(email: String): Long = DB.withSession {  implicit session: scala.slick.session.Session =>
-
-    val test2 = emailExist(email).list
-    if(test2.length > 0){
-      return test2.last.uid.get
-    }else{
-      return 0
     }
   }
 }
