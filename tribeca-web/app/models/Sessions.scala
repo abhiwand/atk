1d365d92b2341e609e6e2bf3a4d9d5c0bd7985ab
rodorad <rene.o.dorado@intel.com>
2013-10-29 10:42:50 -0700

    update the session table with timestamp and add crud for sessions

--- tribeca-web/app/models/Sessions.scala
+++ tribeca-web/app/models/Sessions.scala
@@ -1,15 +1,55 @@
 package models
 
+import models.database._
+import play.api.Play.current
+import play.api.db.slick.Config.driver.simple._
+import scala.slick.session.Session
+
+import slick.driver.ExtendedProfile
+import play.api.db.slick.Profile
+import play.api.db.slick.DB
+import models.database.Session
+
 object Sessions {
   var table = new database.Sessions
 
-  def createSessionId{
+  def getSession(sessionId: String): Query[Sessions, database.Session] = DB.withSession{implicit session: scala.slick.session.Session =>
+      return for { se <- table if se.Id === sessionId} yield se
+  }
+
+  //crud
+  def createSession(uid: Long): String = DB.withSession{implicit session: scala.slick.session.Session =>
+    val sessionId = createSessionId
+    val successful = table.insert(Session(sessionId,uid,"",System.currentTimeMillis/1000))
+    if(successful == 1){
+      return sessionId
+    }else{
+      return null
+    }
+  }
+
+  def readSession(sessionId: String): database.Session = DB.withSession{implicit session: scala.slick.session.Session =>
+    val userSesssions = getSession(sessionId).list
+    if(userSesssions.length > 0 ){
+      return userSesssions.last
+    } else{
+      return null
+    }
+  }
+
+  def updateSession(userSession: models.database.Session) = DB.withSession{implicit session: scala.slick.session.Session =>
+    val userSes = getSession(userSession.Id)
+    userSes.update(userSession)
+  }
+
+  def deleteSession(sessionId: String) = DB.withSession{implicit session: scala.slick.session.Session =>
+    val userSes = getSession(sessionId)
+    userSes.delete
+  }
 
 
-    /*//String uuid=session("uuid");
-    if(uuid==null) {
-      val uuid = java.util.UUID.randomUUID().toString();
-      //session("uuid", uuid);
-    }*/
+
+  def createSessionId(): String = {
+    java.util.UUID.randomUUID().toString()
   }
 }
