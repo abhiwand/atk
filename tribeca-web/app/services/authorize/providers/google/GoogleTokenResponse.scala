f54ec80a1a3b5dcb2fa01c5f9fdafbc9835455d9
rodorad <rene.o.dorado@intel.com>
2013-10-29 11:58:51 -0700

    remove a param from the json validate class

--- tribeca-web/app/services/authorize/providers/google/GoogleTokenResponse.scala
+++ tribeca-web/app/services/authorize/providers/google/GoogleTokenResponse.scala
@@ -2,8 +2,8 @@ package services.authorize.providers.google
 
 import services.authorize.{TokenResponse}
 
-class GoogleTokenResponse(_aa:String, access_token: String, authuser: String, client_id: String)
-  extends TokenResponse(_aa, access_token, authuser, client_id){
+class GoogleTokenResponse(access_token: String, authuser: String, client_id: String)
+  extends TokenResponse(access_token, authuser, client_id){
 
 }
 
