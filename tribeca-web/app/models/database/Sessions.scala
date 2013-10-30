3b07d408f1696daf31db2293529915560bc8fe60
schen55 <steven.shih-chi.chen@intel.com>
2013-10-30 11:42:29 -0700

    make sessions table singleton object

--- tribeca-web/app/models/database/Sessions.scala
+++ tribeca-web/app/models/database/Sessions.scala
@@ -4,7 +4,7 @@ import play.api.db.slick.Config.driver.simple._
 
 case class Session(Id:String, uid:Long, data:String, var timestamp:Long)
 
-class Sessions extends Table[Session]("Sessions"){
+object Sessions extends Table[Session]("Sessions"){
   def Id = column[String]("Id", O.PrimaryKey)
   def uid = column[Long]("uid", O.NotNull)
   def data = column[String]("data")
