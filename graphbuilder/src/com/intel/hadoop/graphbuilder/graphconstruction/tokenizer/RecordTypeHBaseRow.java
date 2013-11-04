a2bbfa49c925548cbd7749ac46bc16b9677d4340
Nathan Segerlind <nathan.l.segerlind@intel.com>
2013-11-04 00:15:40 -0800

    TRIB-767 Added javadoc for RecordTypeHBaseRow

--- graphbuilder/src/com/intel/hadoop/graphbuilder/graphconstruction/tokenizer/RecordTypeHBaseRow.java
+++ graphbuilder/src/com/intel/hadoop/graphbuilder/graphconstruction/tokenizer/RecordTypeHBaseRow.java
@@ -6,14 +6,25 @@ import org.apache.hadoop.hbase.client.Result;
 import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
 
 /**
- * RecordTypeHBaseRow is a way to treat the hbase reader output as one data object, which is what the parser expects,
- * as opposed to two objects, which is what the hbase reader returns
+ * Encapsulates hbase reader output as one data object.
+ *
+ * It has two parts:
+ * <ul>
+ *     <li>{@code ImmutableBytesWritable row }  the hbase row</li>
+ *     <li>{@code Result columns} the columns of the row</li>
+ * </ul>
+ * @see com.intel.hadoop.graphbuilder.graphconstruction.inputmappers.HBaseReaderMapper
  */
 public class RecordTypeHBaseRow {
 
     private ImmutableBytesWritable row;
     private Result                 columns;
 
+    /**
+     * Constructor that takes a row and its columns.
+     * @param row
+     * @param columns
+     */
     public RecordTypeHBaseRow(ImmutableBytesWritable row, Result columns) {
         this.row     = row;
         this.columns = columns;
