8b5657f7c680e44ee700875df7a120d747cc7ae2
rodorad <rene.o.dorado@intel.com>
2013-11-05 09:39:32 -0800

    change field name in registration table

--- tribeca-web/app/models/database/Registration.scala
+++ tribeca-web/app/models/database/Registration.scala
@@ -2,13 +2,13 @@ package models.database
 
 import play.api.db.slick.Config.driver.simple._
 
-case class Registration(uid:Long,name:String,
+case class Registration(uid:Long,myName:String,
                         organization_name: String,  organization_phone: String,organization_email: String,
                         experience:Int, role:String, whyParticipate:String, whatTools:String )
 
 object Registrations extends Table[Registration]("user_registration"){
   def uid = column[Long]("uid", O.PrimaryKey)
-  def name = column[String]("name")
+  def myName = column[String]("myName")
   def organization_name = column[String]("organization_name")
   def organization_phone = column[String]("organization_phone")
   def organization_email = column[String]("organization_email")
@@ -16,5 +16,5 @@ object Registrations extends Table[Registration]("user_registration"){
   def role = column[String]("role")
   def whyParticipate = column[String]("why_participate")
   def whatTools = column[String]("what_tools")
-  def * = uid ~ name ~ organization_name ~ organization_phone ~ organization_email ~ experience ~ role ~ whyParticipate ~ whatTools <> (Registration, Registration.unapply _)
+  def * = uid ~ myName ~ organization_name ~ organization_phone ~ organization_email ~ experience ~ role ~ whyParticipate ~ whatTools <> (Registration, Registration.unapply _)
 }
