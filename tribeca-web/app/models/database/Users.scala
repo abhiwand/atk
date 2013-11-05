e8472f1bfe820338c9af05434707b3494a3caf0c
rodorad <rene.o.dorado@intel.com>
2013-11-05 14:37:54 -0800

    clean up the user database object and add files to the uploaded files list when it's complete

--- tribeca-web/app/models/database/Users.scala
+++ tribeca-web/app/models/database/Users.scala
@@ -2,8 +2,7 @@ package models.database
 
 import play.api.db.slick.Config.driver.simple._
 
-case class User(uid: Option[Long], givenName: String, familyName: String, email: String,/* phone: String,
-                organization_name: String, organization_email: String,*/ registered: Boolean,
+case class User(uid: Option[Long], givenName: String, familyName: String, email: String, registered: Boolean,
                 ipythonUrl: Option[String], clusterId: Option[String])
 
 object Users extends Table[User]("user_info") {
@@ -15,12 +14,6 @@ object Users extends Table[User]("user_info") {
     def familyName = column[String]("family_name")
 
     def email = column[String]("email", O.NotNull)
-
-    /*def phone = column[String]("phone")
-
-    def organization_name = column[String]("organization_name")
-
-    def organization_email = column[String]("organization_email")*/
 
     def registered = column[Boolean]("registered")
 
