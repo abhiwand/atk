1571e4e3982e6e41b51a4238978011259bb97b54
Yi Zou <yi.zou@intel.com>
2013-11-06 01:37:35 -0800

    [TRIB-360] Add usage and command line options to genhosts script
    Signed-off-by: Yi Zou <yi.zou@intel.com>

--- aws/deploy/scripts/IntelAnalytics_cluster_genhosts.sh
+++ aws/deploy/scripts/IntelAnalytics_cluster_genhosts.sh
@@ -2,4 +2,32 @@
 # Description: Used for preparing hosts file for the target cluster
 source IntelAnalytics_cluster_env.sh
 
-IA_generate_hosts_file ${1} ${2} ${IA_CLUSTERS}
+function usage()
+{
+    echo "usage: --cluster-id <cid> [--cluster-size <csize>]"
+    exit 1
+}
+
+# check inputs
+csize=4
+while [ $# -gt 0 ]
+do
+    case "$1" in
+    --cluster-id)
+        cid=$2
+        shift 2
+        ;;
+    --cluster-size)
+        csize=$2
+        shift 2
+        ;;
+    *)
+        usage
+        ;;
+    esac
+done
+
+if [ -z "${cid}" ] || [ -z "${csize}" ]; then
+    usage
+fi
+IA_generate_hosts_file ${cid} ${csize} ${IA_CLUSTERS}
