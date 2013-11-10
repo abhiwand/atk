2f770fa6c87968bb62ecf421f1253071e5578438
Yi Zou <yi.zou@intel.com>
2013-11-09 20:13:08 -0800

    [TRIB-360] fix the ia config helper script
    log dir is changed in the node image
    
    Signed-off-by: Yi Zou <yi.zou@intel.com>

--- aws/deploy/IntelAnalytics/bin/ia_cluster_config.sh
+++ aws/deploy/IntelAnalytics/bin/ia_cluster_config.sh
@@ -11,6 +11,16 @@ IA_PIG=${IA_ROOT}/pig
 IA_MAHOUT=${IA_ROOT}/mahout
 IA_GIRAPH=${IA_ROOT}/girap
 
+
+IA_HDOOP_LOG=`grep "^export HADOOP_LOG_DIR" ${IA_HDOOP}/conf/hadoop-env.sh | awk -F"=" '{print $2}'`
+if [ -z "${IA_HDOOP_LOG}" ]; then
+	IA_HDOOP_LOG=${IA_HDOOP}/logs
+fi
+IA_HBASE_LOG=`grep "^export HBASE_LOG_DIR" ${IA_HBASE}/conf/hbase-env.sh | awk -F"=" '{print $2}'`
+if [ -z "${IA_HBASE_LOG}" ]; then
+	IA_HBASE_LOG=${IA_HBASE}/logs
+fi
+
 # nodes, use hadoo slaves opr hbase regionservers
 IA_NODES=${IA_HDOOP}/conf/slaves
 CMMD=$1
@@ -30,16 +40,15 @@ do
                 fi
                 rsync -avzl -e ssh ${IA_HDOOP}/conf/ ${node}:${IA_HDOOP}/conf/
                 rsync -avzl -e ssh ${IA_HBASE}/conf/ ${node}:${IA_HBASE}/conf/
-#               rsync -avzl -e ssh ${IA_ZKEEP}/conf/ ${node}:${IA_ZKEEP}/conf/
         elif [ "${CMMD}" == "--rmlogs" ]
         then
-                ssh ${node} "rm -rf ${IA_HDOOP}/logs/* ${IA_HBASE}/logs/*"
+                ssh ${node} "rm -rf ${IA_HDOOP_LOG}/* ${IA_HBASE_LOG}/*"
         elif [ "${CMMD}" == "--rmlogs-hadoop" ]
         then
-                ssh ${node} "rm -rf ${IA_HDOOP}/logs/*"
+                ssh ${node} "rm -rf ${IA_HDOOP_LOG}/*"
         elif [ "${CMMD}" == "--rmlogs-hbase" ]
         then
-                ssh ${node} "rm  -rf ${IA_HBASE}/logs/*"
+                ssh ${node} "rm  -rf ${IA_HBASE_LOG}/*"
         elif [ "${CMMD}" == "--rmlogs-zookeeper" ]
         then
                 ssh ${node} "rm ${IA_ZKEEP}/logs/*"
