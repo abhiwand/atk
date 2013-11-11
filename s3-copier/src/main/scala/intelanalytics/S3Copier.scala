1c12962fb73eb8c2adb860bd2342a1688a0119e0
Bryn Keller <bryn.keller@intel.com>
2013-11-10 19:25:49 -0800

    Code cleanup, documentation

--- s3-copier/src/main/scala/intelanalytics/S3Copier.scala
+++ s3-copier/src/main/scala/intelanalytics/S3Copier.scala
@@ -1,3 +1,26 @@
+//////////////////////////////////////////////////////////////////////////////
+// INTEL CONFIDENTIAL
+//
+// Copyright 2013 Intel Corporation All Rights Reserved.
+//
+// The source code contained or described herein and all documents related to
+// the source code (Material) are owned by Intel Corporation or its suppliers
+// or licensors. Title to the Material remains with Intel Corporation or its
+// suppliers and licensors. The Material may contain trade secrets and
+// proprietary and confidential information of Intel Corporation and its
+// suppliers and licensors, and is protected by worldwide copyright and trade
+// secret laws and treaty provisions. No part of the Material may be used,
+// copied, reproduced, modified, published, uploaded, posted, transmitted,
+// distributed, or disclosed in any way without Intel's prior express written
+// permission.
+//
+// No license under any patent, copyright, trade secret or other intellectual
+// property right is granted to or conferred upon you by disclosure or
+// delivery of the Materials, either expressly, by implication, inducement,
+// estoppel or otherwise. Any license under such intellectual property rights
+// must be express and approved by Intel in writing.
+//////////////////////////////////////////////////////////////////////////////
+
 import awscala._, s3._
 import scalaz._
 import Scalaz._
@@ -8,6 +31,7 @@ package com.intel.intelanalytics {
 import awscala.sqs.{Message, SQS}
 import scalax.io.{Input, StandardOpenOption}
 
+//TODO: make this app work using distcp instead
 //import org.apache.hadoop.tools.{DistCpOptions, DistCp}
 
 import play.api.libs.json.Json
@@ -18,18 +42,40 @@ import org.apache.hadoop.fs.{FileSystem, Path => HdPath}
 import scala.concurrent._
 import ExecutionContext.Implicits.global
 
+/**
+ * Status information
+ * @param name the name that should appear for the user who is tracking this status
+ * @param progress the progress toward completion. This should be a number between 0 and 100.
+ */
 case class Status(name: String, progress: Float)
 
+/**
+ * Configuration for the S3Copier application
+ */
 case class Config(bucket: String = "gaopublic", prefix: String = "invalid", destination: String = "/user/gao", queue: String = "gao", statusDestination: String = "/tmp/s3-copier")
 
-//TODO: LICENSE
+/**
+ * Watches SQS for 'create' messages, copies the file named therein to HDFS.
+ * Generates .status files containing status information about the transfers that are
+ * in progress, so that other applications can report status information to the user
+ */
 object S3Copier {
 
+  /**
+   * Stub for later connecting with a proper logging system
+   */
+  //TODO: event logging
   def log[T](message: String): Option[T] = {
     println(message)
     None
   }
 
+  /**
+   * Generate a JSON file representing the given status
+   *
+   * @param progressFolder the folder where the file should be written
+   * @param status the status object to serialize to JSON
+   */
   def writeProgress(progressFolder: String, status: Status) = {
     implicit val StatusFormat = Json.format[Status]
     val json = Json.toJson(status)
@@ -41,6 +87,9 @@ object S3Copier {
     (path /(s"${status.name}.status", '/')).write(Json.stringify(json))
   }
 
+  /**
+   * Parse command line options
+   */
   def parseConfig(args: Array[String]): Config = {
     val parser = new scopt.OptionParser[Config]("s3-copier") {
       head("s3-copier", "1.x")
@@ -67,7 +116,14 @@ object S3Copier {
     }
   }
 
+  /**
+   * Reads command arguments, watches an SQS queue for messages, dispatches them for processing
+   */
   def main(args: Array[String]) {
+
+    // It appears to be impossible to get through Intel's proxywall with this.
+    // I've also tried -D arguments as well as environment variables, even all three
+    // together, nothing worked. So you'll have to do testing in AWS.
 
     //    val config = new ClientConfiguration()
     //                      .withProxyHost("proxy.jf.intel.com")
@@ -76,12 +132,11 @@ object S3Copier {
 
     val config = parseConfig(args)
 
-    println("Creating S3 object")
-    //    val client = new ProxyS3Client(Credentials.defaultEnv, config)
+    log("Creating S3 object")
     implicit val s3 = S3().at(Region.US_WEST_2)
 
     implicit val sqs = SQS().at(Region.US_WEST_2)
-    println("Getting/creating queue")
+    log("Getting/creating queue")
     val queue = sqs.queue(config.queue) getOrElse sqs.createQueue(config.queue)
     val inProgress = mutable.Map[String, Future[Status]]()
     val configuration = new Configuration()
@@ -96,7 +151,7 @@ object S3Copier {
           log("Final results:")
           inProgress.foreach(kv =>
             try {
-              println(Await.result(kv._2, Duration.Inf).toString)
+              log(Await.result(kv._2, Duration.Inf).toString)
             } catch {
               case e: Exception => log(e.toString)
             })
@@ -107,16 +162,21 @@ object S3Copier {
         case e: Exception => log(e.toString)
       }
     }
-
-
-
-    //    log(s"Files in bucket ${config.bucket} starting with ${config.prefix}:")
-    //    s3.bucket(config.bucket).get.keys()
-    //        .filter(_.startsWith(config.prefix))
-    //        .foreach(log)
   }
 
 
+  /**
+   * Parses the message, and if it is valid and appropriate for this configuration,
+   * copies the file from S3 to HDFS.
+   *
+   * @param msg the SQS message
+   * @param config
+   * @param s3
+   * @param sqs
+   * @param configuration
+   * @param fs
+   * @param inProgress
+   */
   def processMessage(msg: Message, config: Config, s3: S3,
                      sqs: SQS,
                      configuration: Configuration,
@@ -139,26 +199,32 @@ object S3Copier {
 
     file.foreach {
       f =>
-        val result: Future[Status] = copyFile(f, config, configuration, fs)
+        val result: Future[Status] = copyFile(f, config, fs)
         inProgress.put(f.key, result)
         msg.destroy()
     }
   }
 
-  def copyFile(f: S3Object, config: Config, configuration: Configuration, fs: FileSystem): Future[Status] = {
-    val len = f.getObjectMetadata.getContentLength
-    val name = f.key.substring(config.prefix.length)
+  /**
+   * Copies the given file from S3 to HDFS, and generates a status file
+   * that it updates during the process.
+   * @param s3Object the file to copy
+   * @param config the copier configuration
+   * @param fs the hdfs filesystem to use for copying
+   */
+  def copyFile(s3Object: S3Object, config: Config, fs: FileSystem): Future[Status] = {
+    val len = s3Object.getObjectMetadata.getContentLength
+    val name = s3Object.key.substring(config.prefix.length)
     var status = Status(name, 0)
     writeProgress(config.statusDestination, status)
     val localPath = Path.fromString(config.statusDestination) / (name, '/')
     future {
-      val resource = scalax.io.Resource.fromInputStream(f.content)
+      val resource = scalax.io.Resource.fromInputStream(s3Object.content)
       localPath.outputStream(StandardOpenOption.Create).doCopyFrom(resource.inputStream)
       log(s"Wrote to ${localPath.path}")
       status = status.copy(progress = 50)
       writeProgress(config.statusDestination, status)
       log(s"Local exists: ${localPath.exists}")
-      val fs = FileSystem.get(configuration)
 
       fs.copyFromLocalFile(new HdPath("file://" + localPath.path), new HdPath(config.destination + "/" + name))
       localPath.delete(force = true)
@@ -168,23 +234,27 @@ object S3Copier {
       status
     }
 
+    //TODO: Use DistCp instead. Currently doesn't work, always get 403 errors
+    //from S3, no matter how lax we make the S3 permissions.
+    //We'll use job progress from distcp to update the status file, that will give
+    //better granularity of progress updates.
 
-    //    val source = f.publicUrl.toString
-    //    val destination = new HdPath(config.destination + "/" + f.key)
+    //    val source = s3Object.publicUrl.toString
+    //    val destination = new HdPath(config.destination + "/" + s3Object.key)
     //    val distCp = new DistCp(configuration, new DistCpOptions(new HdPath(source), destination))
     //    log("Starting distcp")
     //    val job = distCp.execute()
     //    val result = future {
     //      while (!job.isComplete) {
     //        status = status.copy(currentSize = fs.getContentSummary(destination).getLength)
-    //        log(s"updated progress for ${f.key} : ${status.progress()}")
+    //        log(s"updated progress for ${s3Object.key} : ${status.progress()}")
     //        writeProgress(config.statusDestination, status)
     //        Thread.sleep(1000)
     //      }
     //      if (job.isSuccessful) {
-    //        log(s"Message processed: ${f.key}")
+    //        log(s"Message processed: ${s3Object.key}")
     //      } else {
-    //        log("Failed to copy ${f.key}")
+    //        log("Failed to copy ${s3Object.key}")
     //      }
     //      status
     //    }
--- s3-copier/src/main/scala/intelanalytics/aws/package.scala
+++
@@ -1,9 +0,0 @@
-package com.intel.intelanalytics
-
-//TODO: LICENSE
-
-package object aws {
-  val access_key = None //Play.application.configuration.getString("aws.access_key").get
-  val secret_access_key = None //Play.application.configuration.getString("aws.secret_access_key").get
-
-}
