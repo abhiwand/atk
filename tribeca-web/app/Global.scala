888b6c77d43875d35e2291ac91909654b1d6d037
rodorad <rene.o.dorado@intel.com>
2013-11-10 17:40:17 -0800

    create bucket on app start

--- tribeca-web/app/Global.scala
+++ tribeca-web/app/Global.scala
@@ -7,9 +7,11 @@ import scala.concurrent.Future
 import scala.Some
 import scala.Some
 import play.api.Play.current
+import services.aws.S3
 
 
 object Global extends GlobalSettings{
+
   override def onLoadConfig(config: Configuration, path: File, classloader: ClassLoader, mode: Mode.Mode): Configuration = {
     //if we get a play mode environment variable use that instead of the default one from play
     val playMode = if(System.getProperty("play.config") == null) mode.toString.toLowerCase else System.getProperty("play.config").toLowerCase
@@ -30,6 +32,12 @@ object Global extends GlobalSettings{
     //pass the regular play mode since our own custom mode is only use for loading a config
     super.onLoadConfig(modeSpecificConfig, path, classloader, mode)
   }
+
+  override def onStart(app : play.api.Application){
+    //create s3 bucket to hold user files
+    S3.createBucket()
+  }
+
   /*try this on prod with elb infront of it
   override def onRouteRequest(request: RequestHeader): Option[Handler] = {
     if (Play.isDev && !request.headers.get("x-forwarded-proto").getOrElse("").contains("https")) {
