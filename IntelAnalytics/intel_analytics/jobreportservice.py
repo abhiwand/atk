ddc1721d9d0659b39082fa1ef95baf9a97a0713e
schen55 <steven.shih-chi.chen@intel.com>
2013-11-21 10:48:32 -0800

    execute strategy is strategy object is not None

--- IntelAnalytics/intel_analytics/jobreportservice.py
+++ IntelAnalytics/intel_analytics/jobreportservice.py
@@ -4,6 +4,10 @@ which can be from for example stdout or stderr from executing
 commands.
 """
 class JobReportService:
+
+    def __init__(self):
+        self.report_strategy = None
+
     def set_report_strategy(self, report_strategy):
         """
         assign a strategy instance to be use
@@ -16,7 +20,8 @@ class JobReportService:
         giving report with the assigned strategy for single input line
         :param line:
         """
-        self.report_strategy.report(line)
+        if self.report_strategy is not None:
+            self.report_strategy.report(line)
 
     def report_lines(self, lines):
         """
