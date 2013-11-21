b9328b32ef1e5cbc4e595a83fce567c5a220cba3
schen55 <steven.shih-chi.chen@intel.com>
2013-11-20 16:54:07 -0800

    show animated progress bar. When progress reaches 100 percent, remove animation

--- IntelAnalytics/intel_analytics/progress.py
+++ IntelAnalytics/intel_analytics/progress.py
@@ -3,34 +3,39 @@ import time
 from IPython.display import HTML, Javascript, display
 from threading import Thread
 
+
 class Progress:
-	def __init__(self, name, max_value = 100):
-		self.name = name
-		self.id = str(uuid.uuid4())
-		self.max_value = max_value
-		self.value = 0
+    def __init__(self, name, max_value=100):
+        self.name = name
+        self.id = str(uuid.uuid4())
+        self.max_value = max_value
+        self.value = 0
 
-	def _repr_html_(self):
-		pb = HTML(
-		"""
-		<div class='status'>
-			<span class="label span3">%s</span>
-			<span class="span5 progress progress-striped">
-		  		<div id="%s" class="bar" style= "width:0%%">&nbsp;</div>
-			</span> 
-		</div>
-		""" % (self.name, self.id))
-		display(pb)
-		return pb
+    def _repr_html_(self):
+        pb = HTML(
+            """
+            <div id="%s" class='status'>
+                <span class="label span3">%s</span>
+                <span class="span5 progress progress-striped active">
+                      <div class="bar" style= "width:0%%">&nbsp;</div>
+                </span>
+            </div>
+            """ % (self.id, self.name))
+        display(pb)
+        return pb
+
+    def update(self, value):
+        self.value = value
+        display(Javascript("$('#%s div').first().width('%i%%')" %
+                           (self.id, (float(value) / float(self.max_value)) * 100)))
+
+        if(value == 100):
+            self._disable_animation()
+
+    def delete(self):
+        display(Javascript("$('#%s div').first().parents('.status').remove()" % self.id))
 
 
-	def update(self, value):
-		self.value = value;
-		display(Javascript("$('div#%s').width('%i%%')" % 
-					(self.id, (float(value) / float(self.max_value)) * 100)))
-
-	def delete(self):
-		display(Javascript("$('div#%s').parents('.status').remove()" %self.id))
-
-	
+    def _disable_animation(self):
+        display(Javascript("$('#%s span').last().removeClass(\"active\")" % self.id))
 
