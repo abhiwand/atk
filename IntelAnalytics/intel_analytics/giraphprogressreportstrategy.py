ac961995ee5c98ac338fb1755cec4d1390cbe161
schen55 <steven.shih-chi.chen@intel.com>
2013-11-22 08:59:14 -0800

    giraph progress stops at 50% then wait for finish

--- IntelAnalytics/intel_analytics/giraphprogressreportstrategy.py
+++ IntelAnalytics/intel_analytics/giraphprogressreportstrategy.py
@@ -2,27 +2,28 @@ from intel_analytics.mapreducelogutil import MapReduceProgress
 from mapreducelogutil import find_progress
 from progressreportstrategy import ProgressReportStrategy
 import re
+from threading import Thread
+import time
 
 job_completion_pattern = re.compile(r".*?mapred.JobClient: Job complete")
+
+
 class GiraphProgressReportStrategy(ProgressReportStrategy):
+
+    def __init__(self):
+        self.job_progress_bar_list = []
+        self.progress_list = []
+        self.job_progress_bar_list.append(self.get_new_progress_bar("Progress"))
+        self.progress_list.append(MapReduceProgress(0, 0))
 
     def report(self, line):
         progress = find_progress(line)
         if progress:
-            if len(self.job_progress_bar_list) == 0:
-                self.job_progress_bar_list.append(self.get_new_progress_bar("Progress"))
-                self.progress_list.append(progress)
-
             # giraph is a mapper only job
-            self.job_progress_bar_list[-1].update(progress.mapper_progress)
-            progress.total_progress = progress.mapper_progress
-            self.progress_list[-1] = progress
-
-            # mapper job finishes, create second progress bar automatically since
-            # giraph does not print any message indicating beginning of the second phase
-            if progress.mapper_progress == 100:
-                self.job_progress_bar_list.append(self.get_new_progress_bar("Progress"))
-                self.progress_list.append(MapReduceProgress(0, 0))
+            progressGiraph = MapReduceProgress(progress.mapper_progress * 0.5, 0)
+            self.job_progress_bar_list[-1].update(progressGiraph.mapper_progress)
+            progressGiraph.total_progress = progressGiraph.mapper_progress
+            self.progress_list[-1] = progressGiraph
 
         if self._is_computation_complete(line):
             self.progress_list[-1] = MapReduceProgress(100, 100)
@@ -34,3 +35,6 @@ class GiraphProgressReportStrategy(ProgressReportStrategy):
             return True
         else:
             return False
+
+
+
