f69a93fa7c2e88ed369f11aea390954138ef3040
Briton Barker <briton.barker@intel.com>
2013-11-22 11:11:22 -0800

    added config unit tests

--- IntelAnalytics/intel_analytics/config.py
+++ IntelAnalytics/intel_analytics/config.py
@@ -9,12 +9,13 @@ from string import Template
 import os
 import importlib
 
-__all__ = ['global_config', 'Config', "get_keys_from_template"]
+__all__ = ['get_global_config', 'Config', "get_keys_from_template"]
 
 # todo: figure out the correct way to get this:
 properties_file = '/'.join([os.getenv('INTEL_ANALYTICS_HOME', os.getcwd()),
                             'intel_analytics',
                             'intel_analytics.properties'])
+
 
 def get_env_vars(names):
     """
@@ -32,17 +33,19 @@ def get_env_vars(names):
         raise Exception("Environment vars not set: " + ", ".join(missing))
     return env_vars
 
+
 # todo: move get_keys_from_template to a more general utils module:
 def get_keys_from_template(template):
     """
     Screens a template for all the keys requires for substitution
     """
     from collections import defaultdict
-    d = defaultdict(lambda : None)
+    d = defaultdict(lambda: None)
     template.substitute(d)
     keys = d.keys()
     keys.sort()
     return keys
+
 
 # todo: move dynamic_import to a more general utils module:
 def dynamic_import(attr_path):
@@ -59,31 +62,42 @@ def dynamic_import(attr_path):
         attr = getattr(module, attr_name)
     except ImportError:
         raise ValueError("Error trying to find '{0}' in module '{1}'"
-        .format(attr_name, module_path))
+                         .format(attr_name, module_path))
     return attr
 
 
 class Config(object):
 
-    def __init__(self, srcfile=properties_file):
+    def __init__(self, srcfile=None):
         self.srcfile = srcfile
-        self.load_defaults(srcfile)
+        if srcfile is not None:
+            self.load(srcfile)
 
-    def load_defaults(self, srcfile=None):
+    def load(self, srcfile=None):
+        srcfile = srcfile or self.srcfile
         if srcfile is None:
-            srcfile=self.srcfile
+            raise Exception('Configuration source file not specified')
+
         self.default_props = Properties()
-        #with open(srcfile, 'r') as src: Pig uses jython 2.5, so with is not there!
+        lines = []
+        #with open(srcfile, 'r') as src: Pig uses jython 2.5, so can't use with
         src = open(srcfile, 'r')
         try:
-            template = Template(src.read())
-            env_keys = get_keys_from_template(template)
-            env_vars = get_env_vars(env_keys)
-            lines = template.substitute(env_vars).split(os.linesep)
-            self.default_props.load_lines(lines)
+            while 1:
+                line = src.readline()
+                if not line:
+                    break
+                line = line.strip()
+                if len(line) > 0 and line[0] != '!' and line[0] != '#':
+                    lines.append(line)
         finally:
             src.close()
-        
+        template = Template(os.linesep.join(lines))
+        env_keys = get_keys_from_template(template)
+        env_vars = get_env_vars(env_keys)
+        lines = template.substitute(env_vars).split(os.linesep)
+        self.default_props.load_lines(lines)
+        self.srcfile = srcfile
         self.reset()
 
     def reset(self):
@@ -107,18 +121,33 @@ class Config(object):
     def __setitem__(self, key, value):
         self.props[key] = value
 
+    def __delitem__(self, key):
+        del self.props[key]
+
     def __repr__(self):
         r = []
         items = self.props.items()
         items.sort()
-        for k,v in items:
+        for k, v in items:
             r.append(k + "=" + v)
         return "\n".join(r)
 
     def raise_missing_parameters_error(self, missing):
         m = missing if isinstance(missing, str) else ", ".join(missing)
         raise Exception("Configuration file '" + self.srcfile +
-                        "' is missing parameters: " + m )
+                        "' is missing parameters: " + m)
 
-global_config = Config()
+# Global Config Singleton
+try:
+    global_config = Config(properties_file)
+except Exception as e:
+    import sys
+    sys.stderr.write("""
+WARNING - could not load default properties file {0} because:
+  {1}
 
+Global Configuration will be empty until property loaded.
+Try global_config.load()
+""".format(properties_file, str(e)))
+    sys.stderr.flush()
+    global_config = Config()
