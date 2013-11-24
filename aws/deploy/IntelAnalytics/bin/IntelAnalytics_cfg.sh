173a114468a1d725bb5eb3a915cef19f0ac1c6f5
Yi Zou <yi.zou@intel.com>
2013-11-24 14:55:19 -0800

    [TRIB-1145] Fix the helper script
    Signed-off-by: Yi Zou <yi.zou@intel.com>

--- aws/deploy/IntelAnalytics/bin/IntelAnalytics_cfg.sh
+++ aws/deploy/IntelAnalytics/bin/IntelAnalytics_cfg.sh
@@ -18,52 +18,73 @@ then
 	echo "Useage: $(basename $0) [--synconf|--jpsls|--rmlogs|--rmlogs-hadoop|--rmlogs-base]"
         exit -1
 fi
+
 for node in `cat ${NODES}`
 do
-        echo "## \"${CMMD}\":working on cluster node ${node}..."
-        if [ "${CMMD}" == "--synconf" ]
+    if [ "${CMMD}" == "--synconf" ]
+    then
+        echo "## \"${CMMD}\":syncing config files for node ${node}..."
+        if [ "${node}" = "`hostname -s`" ]
         then
-                if [ "${node}" = "`hostname -s`" ]
-                then
-                        echo "excluding self as ${node}..."
-                        continue;
-                fi
-                rsync -avzl -e ssh ${HADOOP_HOME}/conf/ ${node}:${HADOOP_HOME}/conf/
-                rsync -avzl -e ssh ${HBASE_HOME}/conf/ ${node}:${HBASE_HOME}/conf/
-        elif [ "${CMMD}" == "--rmlogs" ]
-        then
-                ssh ${node} "rm -rf ${HADOOP_LOG}/* ${HBASE_LOG}/*"
-        elif [ "${CMMD}" == "--rmlogs-hadoop" ]
-        then
-                ssh ${node} "rm -rf ${HADOOP_LOG}/*"
-        elif [ "${CMMD}" == "--rmlogs-hbase" ]
-        then
-                ssh ${node} "rm  -rf ${HBASE_LOG}/*"
-        elif [ "${CMMD}" == "--jpsls" ]
-        then
-                ssh ${node} "jps -l | grep -v jps"
-        elif [ "${CMMD}" == "--start" ]
-		# start dfs
-		${HADOOP_HOME}/bin/start-dfs.sh
-		# start mapred
-		${HADOOP_HOME}/bin/start-mapred.sh
-		# start hbase
-		${HBASE_HOME}/bin/start-hbase.sh
-		# start hbase thrift
-		${HBASE_HOME}/bin/hbase-daemon.sh start thrift
-		# start titan server
-		${TITAN_SERVER_HOME}/bin/start-rexstitan.sh
-        elif [ "${CMMD}" == "--stop" ]
-		# stop titan server
-		${TITAN_SERVER_HOME}/bin/stop-rexstitan.sh
-		# stop hbase thrift
-		${HBASE_HOME}/bin/hbase-daemon.sh stop thrift
-		# stop hbase
-		${HBASE_HOME}/bin/stop-hbase.sh
-		# stop mapred
-		${HADOOP_HOME}/bin/stop-mapred.sh
-		# stop dfs
-		${HADOOP_HOME}/bin/stop-dfs.sh
+                echo "excluding self as ${node}..."
+                continue;
         fi
+        rsync -avzl -e ssh ${HADOOP_HOME}/conf/ ${node}:${HADOOP_HOME}/conf/
+        rsync -avzl -e ssh ${HBASE_HOME}/conf/ ${node}:${HBASE_HOME}/conf/
+    elif [ "${CMMD}" == "--rmlogs" ]
+    then
+        echo "## \"${CMMD}\":clean up logs for node ${node}..."
+        ssh ${node} "rm -rf ${HADOOP_LOG}/* ${HBASE_LOG}/*"
+    elif [ "${CMMD}" == "--rmlogs-hadoop" ]
+    then
+        echo "## \"${CMMD}\":clean up hadoop logs for node ${node}..."
+        ssh ${node} "rm -rf ${HADOOP_LOG}/*"
+    elif [ "${CMMD}" == "--rmlogs-hbase" ]
+    then
+        echo "## \"${CMMD}\":clean up hbase logs for node ${node}..."
+        ssh ${node} "rm  -rf ${HBASE_LOG}/*"
+    elif [ "${CMMD}" == "--jpsls" ]
+    then
+        echo "## \"${CMMD}\":show jvm process status for node ${node}..."
+        ssh ${node} "jps -l | grep -v jps"
+    fi
 done
+# master only commands
+if [ "master" == "`grep \`hostname\` /etc/hosts | awk '{print $2}'`" ]
+then
+    if [ "${CMMD}" == "--start" ]
+    then 
+        echo "## \"${CMMD}\":Starting all services..."
+        # start dfs
+        ${HADOOP_HOME}/bin/start-dfs.sh
+        # start mapred
+        ${HADOOP_HOME}/bin/start-mapred.sh
+        # start hbase
+        ${HBASE_HOME}/bin/start-hbase.sh
+        # start hbase thrift
+        ${HBASE_HOME}/bin/hbase-daemon.sh start thrift
+        # start titan server
+        ${TITAN_SERVER_HOME}/bin/start-rexstitan.sh
+        # 
+    elif [ "${CMMD}" == "--stop" ]
+    then
+        echo "## \"${CMMD}\":Stopping all services..."
+        # stop titan server
+        ${TITAN_SERVER_HOME}/bin/stop-rexstitan.sh
+        # stop hbase thrift
+        ${HBASE_HOME}/bin/hbase-daemon.sh stop thrift
+        # stop hbase
+        ${HBASE_HOME}/bin/stop-hbase.sh
+        # stop mapred
+        ${HADOOP_HOME}/bin/stop-mapred.sh
+        # stop dfs
+        ${HADOOP_HOME}/bin/stop-dfs.sh
+    elif [ "${CMMD}" == "--titan-load-gods" ]
+    then
+        ${TITAN_HOME}/bin/gremlin.sh `dirname $0`/IntelAnalytics_load.grem
 
+    elif [ "${CMMD}" == "--titan-test-gods" ]
+    then
+        ${TITAN_HOME}/bin/gremlin.sh `dirname $0`/IntelAnalytics_test.grem
+    fi
+fi
