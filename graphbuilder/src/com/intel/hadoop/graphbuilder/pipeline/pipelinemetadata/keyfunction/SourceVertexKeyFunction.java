6e6d2820eff48fd00956049880b8695388522e4d
Mark McCarron-Fraser <markx.a.mccarron-fraser@intel.com>
2013-11-26 16:34:52 -0800

    Edited the JavaDoc in these files.

--- graphbuilder/src/com/intel/hadoop/graphbuilder/pipeline/pipelinemetadata/keyfunction/SourceVertexKeyFunction.java
+++ graphbuilder/src/com/intel/hadoop/graphbuilder/pipeline/pipelinemetadata/keyfunction/SourceVertexKeyFunction.java
@@ -6,14 +6,14 @@ import com.intel.hadoop.graphbuilder.graphelements.Edge;
 import com.intel.hadoop.graphbuilder.graphelements.Vertex;
 
 /**
- * Generate key for map reduce by hashing vertices by their ID and edges by the ID of their destination vertex.
+ * Generates a key for map reduce by hashing the vertices by their ID and edges by the IDs of their destination vertex.
  *
  * @see KeyFunction
  */
 public class SourceVertexKeyFunction  implements KeyFunction {
 
     /**
-     * Generate an integer hash of an edge using its destination vertex
+     * Generates an integer hash of an edge using its destination vertex.
      *
      * @param edge
      * @return  hash code of the edge's destination vertex ID
@@ -23,7 +23,7 @@ public class SourceVertexKeyFunction  implements KeyFunction {
     }
 
     /**
-     * Generate an integer hash of a vertex by hashing its ID
+     * Generates an integer hash of a vertex by hashing its ID.
      *
      * @param vertex
      * @return  hash code of the  vertex ID
