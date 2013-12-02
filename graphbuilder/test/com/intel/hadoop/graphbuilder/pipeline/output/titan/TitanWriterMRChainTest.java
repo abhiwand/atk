f1beea998d9709ab973bbeac26675ecd94e61c2a
rodorad <rene.o.dorado@intel.com>
2013-12-02 09:48:03 -0800

    end of day check int nothing exiting

---
+++ graphbuilder/test/com/intel/hadoop/graphbuilder/pipeline/output/titan/TitanWriterMRChainTest.java
@@ -0,0 +1,30 @@
+package com.intel.hadoop.graphbuilder.pipeline.output.titan;
+
+
+import com.intel.hadoop.graphbuilder.pipeline.mergeduplicates.propertygraphelement.PropertyGraphElements;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(PropertyGraphElements.class)
+public class TitanWriterMRChainTest {
+
+    @Before
+    public void setUp(){
+
+    }
+
+    @After
+    public void tearDown(){
+
+    }
+
+    @Test
+    public void test(){
+
+    }
+}
