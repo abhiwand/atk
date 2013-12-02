9094db9c3a7e396fcee155dc182f32d71cdb608a
schen55 <steven.shih-chi.chen@intel.com>
2013-12-02 10:37:30 -0800

    generate the next progress bar when sensing progress decrease.

--- IntelAnalytics/tests/test_progressreportstrategy.py
+++ IntelAnalytics/tests/test_progressreportstrategy.py
@@ -13,6 +13,17 @@ class TestProgressReportStrategy(unittest.TestCase):
         self.progressReportStrategy = ProgressReportStrategy()
         self.assertEquals(0, len(self.progressReportStrategy.get_all_map_reduce_jobs_progress_list()))
 
+    def test_1_job_with_repeat_0_progress(self):
+        self.progressReportStrategy = ProgressReportStrategy()
+        self.progressReportStrategy.report("13/11/14 14:35:53 INFO mapred.JobClient:  map 0% reduce 0%")
+        self.progressReportStrategy.report("13/11/14 14:35:53 INFO mapred.JobClient:  map 0% reduce 0%")
+        self.assertEquals(1, self.progressReportStrategy.get_total_map_reduce_job_count())
+        progress = self.progressReportStrategy.get_all_map_reduce_jobs_progress_list()[0]
+        self.assertEquals(0, progress.mapper_progress)
+        self.assertEquals(0, progress.reducer_progress)
+        self.assertEquals(0, progress.total_progress)
+        self.assertEquals("Step 2", self.progressReportStrategy.get_next_step_title())
+
     def test_1_job_with_progress(self):
         self.progressReportStrategy = ProgressReportStrategy()
         self.progressReportStrategy.report("13/11/14 14:35:58 INFO mapred.JobClient:  map 66% reduce 0%")
@@ -58,6 +69,7 @@ class TestProgressReportStrategy(unittest.TestCase):
     def test_second_job_completion(self):
         self.progressReportStrategy = ProgressReportStrategy()
         self.progressReportStrategy.report("13/11/14 14:36:07 INFO mapred.JobClient:  map 100% reduce 100%")
+        self.progressReportStrategy.report("13/11/14 14:35:53 INFO mapred.JobClient:  map 33% reduce 0%")
         self.progressReportStrategy.report("13/11/14 14:36:07 INFO mapred.JobClient:  map 100% reduce 100%")
         self.assertEquals(2, self.progressReportStrategy.get_total_map_reduce_job_count())
         progress = self.progressReportStrategy.get_all_map_reduce_jobs_progress_list()[-1]
