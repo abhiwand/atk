027fa0ab0c2d92d24c80ca7dac9b4febdf098535
Bryn Keller <bryn.keller@intel.com>
2013-12-06 03:01:46 -0800

    Fixed some path problems and moved some things from one rpm to another

--- scripts/rpm/fetch_sources.sh
+++ scripts/rpm/fetch_sources.sh
@@ -1,9 +1,8 @@
 . ./versions.sh
 
-if [ ! -d SOURCES ]
-  then
-    mkdir SOURCES
-fi
+rm -rf SOURCES
+
+mkdir SOURCES
 
 pushd SOURCES
 
@@ -22,22 +21,21 @@ rm -rf $source_folder/intel_analytics
 rm -rf $source_folder/bin/*python*
 rm -rf $source_folder/conf/*python*
 rm -rf $source_folder/conf/intel_analytics.properties
-mkdir $python_folder
-cp -R $src/intel_analytics $python_folder
 
-pushd $python_folder
-cp -R $src/intel_analytics .
-popd
+
+mkdir $python_folder
+mkdir $python_folder/conf
+
+cp -R $src/intel_analytics $python_folder
+cp $src/conf/intel_analytics.properties $python_folder/conf
+cp -R $src/conf/ipython_profile $python_folder/conf
+mkdir $python_folder/bin
+cp -R $src/bin/IntelAnalytics-ipython $python_folder/bin
 
 mkdir $python_dependencies_folder
 
-cp $source_folder/install_pyenv.sh $python_dependencies_folder/
-mkdir $python_dependencies_folder/bin
-cp -R $src/bin/IntelAnalytics-ipython $python_dependencies_folder/bin
-mkdir $python_dependencies_folder/conf
-cp $src/conf/intel_analytics.properties $python_dependencies_folder/conf
 cp $src/install_pyenv.sh $python_dependencies_folder
-cp -R $src/conf/ipython_profile $python_folder/conf
+
 tar czvf $source_folder.tar.gz $source_folder
 tar czvf $python_folder.tar.gz $python_folder
 tar czvf $python_dependencies_folder.tar.gz $python_dependencies_folder
