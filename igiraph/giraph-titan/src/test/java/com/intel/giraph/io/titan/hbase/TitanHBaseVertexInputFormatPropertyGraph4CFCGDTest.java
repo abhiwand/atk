d3d595fe1c0605986673203376d4146714936499
Xia Zhu <xia.zhu@intel.com>
2013-12-09 13:41:32 -0800

    update test cases to make sure it runs even when titan was disable before test

--- igiraph/giraph-titan/src/test/java/com/intel/giraph/io/titan/hbase/TitanHBaseVertexInputFormatPropertyGraph4CFCGDTest.java
+++ igiraph/giraph-titan/src/test/java/com/intel/giraph/io/titan/hbase/TitanHBaseVertexInputFormatPropertyGraph4CFCGDTest.java
@@ -41,6 +41,8 @@ import org.apache.giraph.conf.GiraphConfiguration;
 import org.apache.giraph.conf.ImmutableClassesGiraphConfiguration;
 import org.apache.giraph.utils.InternalVertexRunner;
 import org.apache.hadoop.hbase.client.HBaseAdmin;
+import org.apache.hadoop.hbase.HTableDescriptor;
+import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.log4j.Logger;
 import org.json.JSONArray;
@@ -126,10 +128,17 @@ public class TitanHBaseVertexInputFormatPropertyGraph4CFCGDTest {
         EDGE_TYPE_PROPERTY_KEY.set(giraphConf, "edgeType");
 
         HBaseAdmin hbaseAdmin = new HBaseAdmin(giraphConf);
-        if (hbaseAdmin.isTableAvailable(GIRAPH_TITAN_STORAGE_TABLENAME.get(giraphConf))) {
-            hbaseAdmin.disableTable(GIRAPH_TITAN_STORAGE_TABLENAME.get(giraphConf));
-            hbaseAdmin.deleteTable(GIRAPH_TITAN_STORAGE_TABLENAME.get(giraphConf));
+        String tableName = GIRAPH_TITAN_STORAGE_TABLENAME.get(giraphConf);
+        //even delete an existing table needs the table is enabled before deletion
+        if (hbaseAdmin.isTableDisabled(tableName)) {
+            hbaseAdmin.enableTable(tableName);
         }
+
+        if (hbaseAdmin.isTableAvailable(tableName)) {
+            hbaseAdmin.disableTable(tableName);
+            hbaseAdmin.deleteTable(tableName);
+        }
+
 
         conf = new ImmutableClassesGiraphConfiguration<LongWritable, VertexData4CGDWritable, EdgeDataWritable>(
             giraphConf);
