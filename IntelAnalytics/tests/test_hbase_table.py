e1bb56f882e89d6edbcd5edba7699e1b3365cc73
schen55 <steven.shih-chi.chen@intel.com>
2013-12-10 14:34:28 -0800

    create unit tests

--- IntelAnalytics/tests/test_hbase_table.py
+++ IntelAnalytics/tests/test_hbase_table.py
@@ -1,7 +1,9 @@
+import os
 import unittest
+from intel_analytics.config import global_config as config
 from intel_analytics.table.builtin_functions import EvalFunctions
 from intel_analytics.table.hbase.schema import ETLSchema
-from intel_analytics.table.hbase.table import HBaseTable
+from intel_analytics.table.hbase.table import HBaseTable, HBaseFrameBuilderFactory
 from tests.mock import patch, Mock, PropertyMock, MagicMock
 
 class HbaseTableTest(unittest.TestCase):
@@ -39,7 +41,18 @@ class HbaseTableTest(unittest.TestCase):
         self.assertEqual("new_col1", result_holder["feature_names"][-1])
         self.assertEqual("bytearray", result_holder["feature_types"][-1])
 
-        #todo: validate call arguments
+        self.assertEqual("pig", result_holder["call_args"][0])
+        self.assertEqual('col1', result_holder["call_args"][result_holder["call_args"].index('-f') + 1])
+        self.assertEqual('test_table', result_holder["call_args"][result_holder["call_args"].index('-i') + 1])
+        self.assertEqual('test_table', result_holder["call_args"][result_holder["call_args"].index('-o') + 1])
+        self.assertEqual('ABS', result_holder["call_args"][result_holder["call_args"].index('-t') + 1])
+        self.assertEqual('col1,col2,col3', result_holder["call_args"][result_holder["call_args"].index('-u') + 1])
+        self.assertEqual('long,chararray,long', result_holder["call_args"][result_holder["call_args"].index('-r') + 1])
+        self.assertEqual('new_col1', result_holder["call_args"][result_holder["call_args"].index('-n') + 1])
+
+        script_path = os.path.join(config['pig_py_scripts'], 'pig_transform.py')
+        self.assertTrue(script_path in result_holder["call_args"])
+
 
     @patch('intel_analytics.table.hbase.table.ETLHBaseClient')
     def test_get_first_N(self, etl_base_client_class):
@@ -158,6 +171,71 @@ class HbaseTableTest(unittest.TestCase):
         html = table.to_html()
         expected = '<table border="1"><tr><th>address</th><th>name</th></tr><tr><td>1234 xyz st</td><td>A</td></tr><tr><td>5678 def ave</td><td>B</td></tr></table>'
         self.assertEqual(expected, html)
+
+
+    @patch('intel_analytics.table.hbase.table.hbase_frame_builder_factory')
+    @patch('intel_analytics.table.hbase.table.ETLHBaseClient')
+    @patch('intel_analytics.table.hbase.table.call')
+    @patch('intel_analytics.table.hbase.table.ETLSchema')
+    def test__drop(self, etl_schema_class, call_method, etl_base_client_class, hbase_frame_builder_factory):
+
+        result_holder = {}
+        object = ETLSchema()
+        object.load_schema = Mock(return_value = "1")
+
+        def call_side_effect(arg, report_strategy):
+            result_holder["call_args"] = arg
+
+        def register_side_effect(key, table_name):
+            result_holder["key"] = key
+            result_holder["output_table"] = table_name
+
+        object.save_schema = Mock()
+
+        object.feature_names = ["col1", "col2", "col3"]
+        object.feature_types = ["long", "chararray", "long"]
+        etl_schema_class.return_value = object
+
+        call_method.return_value = None
+        call_method.side_effect = call_side_effect
+
+        frame_name = "test_frame"
+        hbase_frame_builder_factory.name_registry.register = Mock(side_effect = register_side_effect)
+        hbase_frame_builder_factory.name_registry.get_key = Mock(return_value = frame_name)
+
+        table_name = "test_table"
+        file_name = "test_file"
+        output_table = "output_table"
+        table = HBaseTable(table_name, file_name)
+        table._HBaseTable__drop(output_table, 'col1')
+
+        self.assertEqual(frame_name, result_holder["key"])
+        self.assertEqual(output_table, result_holder["output_table"])
+
+        self.assertEqual("pig", result_holder["call_args"][0])
+        self.assertEqual(table_name, result_holder["call_args"][result_holder["call_args"].index('-i') + 1])
+        self.assertEqual(output_table, result_holder["call_args"][result_holder["call_args"].index('-o') + 1])
+        self.assertEqual('col1,col2,col3', result_holder["call_args"][result_holder["call_args"].index('-n') + 1])
+        self.assertEqual('long,chararray,long', result_holder["call_args"][result_holder["call_args"].index('-t') + 1])
+        self.assertEqual('col1', result_holder["call_args"][result_holder["call_args"].index('-f') + 1])
+
+    @patch('intel_analytics.table.hbase.table.hbase_frame_builder_factory')
+    def test_dropna(self, hbase_frame_builder_factory):
+
+        result_holder = {}
+        table_name = "test_table"
+        file_name = "test_file"
+        table = HBaseTable(table_name, file_name)
+        hbase_frame_builder_factory.name_registry.get_key = Mock(return_value = "test_frame")
+        def drop_side_effect(output_table, column_name=None, how=None, replace_with=None):
+            result_holder["column_name"] = column_name
+            result_holder["how"] = how
+
+        column_to_clean = "col1"
+        table._HBaseTable__drop = Mock(side_effect = drop_side_effect)
+        table.dropna(column_name = column_to_clean)
+        self.assertEqual(column_to_clean, result_holder["column_name"])
+        self.assertEqual("any", result_holder["how"])
 
 
 if __name__ == '__main__':
