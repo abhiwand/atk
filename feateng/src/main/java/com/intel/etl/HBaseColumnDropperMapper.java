d1113bdd47cde07c50ee566cbf0db3166b78e2dd
schen55 <steven.shih-chi.chen@intel.com>
2013-12-16 16:34:44 -0800

    doing delete in batch mode

--- feateng/src/main/java/com/intel/etl/HBaseColumnDropperMapper.java
+++ feateng/src/main/java/com/intel/etl/HBaseColumnDropperMapper.java
@@ -1,44 +1,56 @@
 package com.intel.etl;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.KeyValue;
 import org.apache.hadoop.hbase.client.Delete;
 import org.apache.hadoop.hbase.client.HTable;
 import org.apache.hadoop.hbase.client.Result;
 import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
 import org.apache.hadoop.hbase.mapreduce.TableMapper;
+import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.hadoop.io.IntWritable;
 
-import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class HBaseColumnDropperMapper extends TableMapper<IntWritable, IntWritable> {
 
-    HTable table;
+    byte[] columnFamilyBytes;
+    byte[] columnNameBytes;
+    List<Delete> deleteList = new ArrayList<Delete>();
+
     @Override
     public void setup(Context context) {
 
         Configuration conf = context.getConfiguration();
-        String tableName = conf.get(HBaseColumnDropper.TABLE_NAME);
-        try {
-            table = new HTable(context.getConfiguration(), tableName);
-        } catch(IOException e) {
-
-        }
+        columnFamilyBytes = conf.get(HBaseColumnDropper.COLUMN_FAMILY).getBytes();
+        columnNameBytes = conf.get(HBaseColumnDropper.COLUMN_NAME).getBytes();
     }
 
     @Override
     public void map(ImmutableBytesWritable row, Result columns, Context context) {
 
-        Configuration conf = context.getConfiguration();
-        String columnFamily = conf.get(HBaseColumnDropper.COLUMN_FAMILY);
-        String columnName = conf.get(HBaseColumnDropper.COLUMN_NAME);
+        KeyValue kv = columns.getColumnLatest(columnFamilyBytes, columnNameBytes);
 
+        if(kv == null || kv.split() == null)
+            return;
+
+        Long timestamp = Bytes.toLong(kv.split().getTimestamp());
         Delete delete = new Delete(row.get());
-        delete.deleteColumn(columnFamily.getBytes(), columnName.getBytes());
-        try {
-            table.delete(delete);
-        } catch(IOException e) {
+        delete.deleteColumn(columnFamilyBytes, columnNameBytes, timestamp);
+        deleteList.add(delete);
+    }
 
+    @Override
+    protected void cleanup(Context context) {
+
+        String tableName = context.getConfiguration().get(HBaseColumnDropper.TABLE_NAME);
+        try {
+            HTable table = new HTable(context.getConfiguration(), tableName);
+            table.batch(deleteList);
+        } catch(Exception e) {
+            System.out.println("Error in submitting batch command to hbase");
         }
     }
 }
