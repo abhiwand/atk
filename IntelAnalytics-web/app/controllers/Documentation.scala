560e8cf59ce8d7589afb2d3ae53b4fe70ee893ce
Dorado, Rene O <rene.o.dorado@intel.com>
2013-12-17 16:18:41 -0800

    delete the old documentation. add specific templates for all the various doc api pages. added config params for all the doc api pages that will be hosted externally

--- IntelAnalytics-web/app/controllers/Documentation.scala
+++ IntelAnalytics-web/app/controllers/Documentation.scala
@@ -2,30 +2,36 @@ package controllers
 
 import play.api.mvc._
 import controllers.Session._
+import play.api.Play
+import play.api.Play.current
 
 object Documentation extends Controller{
 
   /**
    * main documentation page
    */
-  def documentation = Authenticated{ request =>
+  def index = Authenticated{ request =>
     Ok(views.html.documen.index("API Documentation", request.user.userInfo))
   }
 
   def pythonApi = Authenticated{ request =>
-    Ok(views.html.documen.pythonApi("Python API", request.user.userInfo));
+    Ok(views.html.documen.pythonApi("Python API", request.user.userInfo,
+      Play.application.configuration.getString("documentation.python").getOrElse("")));
   }
 
   def pythonPigApi = Authenticated{ request =>
-    Ok(views.html.documen.pythonPigApi("Python Pig API", request.user.userInfo))
+    Ok(views.html.documen.pythonPigApi("Python Pig API", request.user.userInfo,
+      Play.application.configuration.getString("documentation.python.pig").getOrElse("")))
   }
 
   def javaApi = Authenticated{ request =>
-    Ok(views.html.documen.javaApi("Java API", request.user.userInfo))
+    Ok(views.html.documen.javaApi("Java API", request.user.userInfo,
+      Play.application.configuration.getString("documentation.java").getOrElse("")))
   }
 
   def javaPigApi = Authenticated{ request =>
-    Ok(views.html.documen.javaPigApi("Java Pig API", request.user.userInfo))
+    Ok(views.html.documen.javaPigApi("Java Pig API", request.user.userInfo,
+      Play.application.configuration.getString("documentation.java.pig").getOrElse("")))
   }
 
 }
