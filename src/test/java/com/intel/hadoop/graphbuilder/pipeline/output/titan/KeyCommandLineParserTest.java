7f19d431d52ac21ed04ce12c14198e06edc23fed
Todd Lisonbee <todd.lisonbee@intel.com>
2014-01-20 01:24:38 -0800

    TRIB-1075 adding missing Intel header, adding more unit tests.

--- src/test/java/com/intel/hadoop/graphbuilder/pipeline/output/titan/KeyCommandLineParserTest.java
+++ src/test/java/com/intel/hadoop/graphbuilder/pipeline/output/titan/KeyCommandLineParserTest.java
@@ -1,3 +1,22 @@
+/**
+ * Copyright (C) 2013 Intel Corporation.
+ *     All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * For more about this software visit:
+ *     http://www.01.org/GraphBuilder
+ */
 package com.intel.hadoop.graphbuilder.pipeline.output.titan;
 
 import org.junit.Test;
@@ -20,7 +39,23 @@ public class KeyCommandLineParserTest {
     }
 
     @Test
-    public void testParseKeyCommandLine() throws Exception {
+    public void testParseKeyCommandLine_WithOne() throws Exception {
+
+        // invoke method under test
+        List<GBTitanKey> list = new KeyCommandLineParser().parse("cf:userId;String;U;V");
+
+        assertEquals(1, list.size());
+
+        GBTitanKey userKey = list.get(0);
+        assertEquals("cf:userId", userKey.getName());
+        assertEquals(String.class, userKey.getDataType());
+        assertTrue(userKey.isVertexIndex());
+        assertFalse(userKey.isEdgeIndex());
+        assertTrue(userKey.isUnique());
+    }
+
+    @Test
+    public void testParseKeyCommandLine_WithTwo() throws Exception {
 
         // invoke method under test
         List<GBTitanKey> list = new KeyCommandLineParser().parse("cf:userId;String;U;V,cf:eventId;E;Long");
@@ -41,5 +76,16 @@ public class KeyCommandLineParserTest {
         assertTrue(eventKey.isEdgeIndex());
         assertFalse(eventKey.isUnique());
 
+    }
+
+    @Test
+    public void testParseKeyCommandLine_UniqueMustBeVertex() throws Exception {
+
+        // invoke method under test
+        List<GBTitanKey> list = new KeyCommandLineParser().parse("name;U");
+
+        GBTitanKey userKey = list.get(0);
+        assertTrue(userKey.isVertexIndex());
+        assertTrue(userKey.isUnique());
     }
 }
