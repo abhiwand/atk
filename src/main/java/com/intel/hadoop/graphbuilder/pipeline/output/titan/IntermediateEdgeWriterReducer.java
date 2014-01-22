01226ea640f4b08099aee3e70545d76456687d19
Todd Lisonbee <todd.lisonbee@intel.com>
2014-01-22 02:20:23 -0800

    Converting class level variable to local

--- src/main/java/com/intel/hadoop/graphbuilder/pipeline/output/titan/IntermediateEdgeWriterReducer.java
+++ src/main/java/com/intel/hadoop/graphbuilder/pipeline/output/titan/IntermediateEdgeWriterReducer.java
@@ -64,7 +64,6 @@ public class IntermediateEdgeWriterReducer extends Reducer<IntWritable,
     private final KeyFunction keyFunction = new SourceVertexKeyFunction();
     private IntWritable            outKey;
     private SerializedGraphElement outValue;
-    private Class                  outClass;
 
     private static enum Counters {
         EDGE_PROPERTIES_WRITTEN,
@@ -101,7 +100,7 @@ public class IntermediateEdgeWriterReducer extends Reducer<IntWritable,
 
         intermediateEdgeWriterReducerCallback = new IntermediateEdgeWriterReducerCallback();
 
-        outClass = context.getMapOutputValueClass();
+        Class outClass = context.getMapOutputValueClass();
         outKey   = new IntWritable();
 
         try {
