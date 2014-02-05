2fd5fc5db6c6077d392349f8a3944c1a3936b58a
schen55 <steven.shih-chi.chen@intel.com>
2014-02-04 16:25:23 -0800

    write complete graphml in reducer

--- feateng/src/main/java/com/intel/graph/GraphExportReducer.java
+++ feateng/src/main/java/com/intel/graph/GraphExportReducer.java
@@ -1,6 +1,5 @@
 package com.intel.graph;
 
-import com.intel.etl.HBaseColumnDropper;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FSDataOutputStream;
 import org.apache.hadoop.fs.FileSystem;
@@ -9,13 +8,19 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
+import javax.xml.XMLConstants;
+import javax.xml.stream.XMLOutputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamWriter;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class GraphExportReducer extends Reducer<LongWritable, Text, LongWritable, Text> {
 
     private FSDataOutputStream output;
+    XMLStreamWriter writer = null;
     @Override
     protected void setup(Context context) throws IOException, InterruptedException {
 
@@ -32,6 +37,53 @@ public class GraphExportReducer extends Reducer<LongWritable, Text, LongWritable
         Map<String, String> vertexKeyTypes = new HashMap<String, String>();
         Map<String, String> edgeKeyTypes = new HashMap<String, String>();
 
+        vertexKeyTypes.put("_id","long");
+        vertexKeyTypes.put("_gb_ID","long");
+        vertexKeyTypes.put("etl-cf:vertex_type","chararray");
+
+        edgeKeyTypes.put("etl-cf:edge_type","chararray");
+        edgeKeyTypes.put("etl-cf:weight","long");
+
+        final XMLOutputFactory inputFactory = XMLOutputFactory.newInstance();
+        inputFactory.setProperty("escapeCharacters", false);
+
+        try {
+            writer = inputFactory.createXMLStreamWriter(output, "UTF8");
+            writer.writeStartDocument();
+            writer.writeStartElement(GraphMLTokens.GRAPHML);
+            writer.writeAttribute(GraphMLTokens.XMLNS, GraphMLTokens.GRAPHML_XMLNS);
+            //XML Schema instance namespace definition (xsi)
+            writer.writeAttribute(XMLConstants.XMLNS_ATTRIBUTE + ":" + GraphMLTokens.XML_SCHEMA_NAMESPACE_TAG,
+                    XMLConstants.W3C_XML_SCHEMA_INSTANCE_NS_URI);
+            //XML Schema location
+            writer.writeAttribute(GraphMLTokens.XML_SCHEMA_NAMESPACE_TAG + ":" + GraphMLTokens.XML_SCHEMA_LOCATION_ATTRIBUTE,
+                    GraphMLTokens.GRAPHML_XMLNS + " " + GraphMLTokens.DEFAULT_GRAPHML_SCHEMA_LOCATION);
+
+            Set<String> keySet = vertexKeyTypes.keySet();
+            for (String key : keySet) {
+                writer.writeStartElement(GraphMLTokens.KEY);
+                writer.writeAttribute(GraphMLTokens.ID, key);
+                writer.writeAttribute(GraphMLTokens.FOR, GraphMLTokens.NODE);
+                writer.writeAttribute(GraphMLTokens.ATTR_NAME, key);
+                writer.writeAttribute(GraphMLTokens.ATTR_TYPE, vertexKeyTypes.get(key));
+                writer.writeEndElement();
+            }
+            keySet = edgeKeyTypes.keySet();
+            for (String key : keySet) {
+                writer.writeStartElement(GraphMLTokens.KEY);
+                writer.writeAttribute(GraphMLTokens.ID, key);
+                writer.writeAttribute(GraphMLTokens.FOR, GraphMLTokens.EDGE);
+                writer.writeAttribute(GraphMLTokens.ATTR_NAME, key);
+                writer.writeAttribute(GraphMLTokens.ATTR_TYPE, edgeKeyTypes.get(key));
+                writer.writeEndElement();
+            }
+
+            writer.writeStartElement(GraphMLTokens.GRAPH);
+            writer.writeAttribute(GraphMLTokens.ID, GraphMLTokens.G);
+            writer.writeAttribute(GraphMLTokens.EDGEDEFAULT, GraphMLTokens.DIRECTED);
+        } catch (XMLStreamException e) {
+            throw new RuntimeException("Failed to write header");
+        }
     }
 
     @Override
@@ -39,11 +91,25 @@ public class GraphExportReducer extends Reducer<LongWritable, Text, LongWritable
 
         for(Text value: values){
             try {
-                output.writeBytes(value.toString());
-                output.writeBytes("\n");
-            } catch (IOException e) {
-                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                writer.writeCharacters(value.toString());
+                writer.writeCharacters("\n");
+            } catch (XMLStreamException e) {
+                throw new RuntimeException("Failed to write graph element data");
             }
+        }
+
+    }
+
+    @Override
+    protected void cleanup(Context context) {
+        try {
+            writer.writeEndElement(); // graph
+            writer.writeEndElement(); // graphml
+            writer.writeEndDocument();
+            writer.flush();
+            writer.close();
+        } catch (XMLStreamException e) {
+            throw new RuntimeException("Failed to write closing tags");
         }
 
     }
