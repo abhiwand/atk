47d5d725c2a0e694408c35ce467e126f02faf223
Kushal Datta <kushal.datta@intel.com>
2014-02-06 19:34:28 -0500

    TRIB-1310b: fixed indentation

--- src/main/java/com/intel/pig/udf/eval/CreatePropGraphElements.java
+++ src/main/java/com/intel/pig/udf/eval/CreatePropGraphElements.java
@@ -62,8 +62,10 @@ import java.util.concurrent.TimeUnit;
  * <pre>
  * {@code
  * REGISTER target/graphbuilder-2.0alpha-with-deps.jar;
- * x = LOAD 'examples/data/employees.csv' USING PigStorage(',') AS (id: int, name: chararray, age: int, dept: chararray, managerId: int, tenure: chararray);
- * DEFINE CreatePropGraphElements com.intel.pig.udf.eval.CreatePropGraphElements('-v "name=age,managerId" -e "name,dept,worksAt,tenure"');
+ * x = LOAD 'examples/data/employees.csv' USING PigStorage(',') AS (id: int, name: chararray, age: int,
+ * dept: chararray, managerId: int, tenure: chararray);
+ * DEFINE CreatePropGraphElements com.intel.pig.udf.eval.CreatePropGraphElements('-v "name=age,managerId" -e "name,
+ * dept,worksAt,tenure"');
  * pge = FOREACH x GENERATE flatten(CreatePropGraphElements(*));
  *    }
  * </pre>
