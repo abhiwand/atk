29732d804fa28275d4a1b182ac7905761fccdd5a
schen55 <steven.shih-chi.chen@intel.com>
2014-02-07 18:25:49 -0800

    refactor

--- feateng/src/main/java/com/intel/graph/GraphExporter.java
+++ feateng/src/main/java/com/intel/graph/GraphExporter.java
@@ -20,6 +20,8 @@ import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.IOException;
 import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class GraphExporter {
@@ -28,13 +30,13 @@ public class GraphExporter {
     public static final String VERTEX_SCHEMA = "Vertex Schema";
     public static final String EDGE_SCHEMA = "Edge Schema";
 
-    public static void main(String[] args) throws ParseException, ParserConfigurationException, SAXException {
+    public static void main(String[] args) throws ParseException, ParserConfigurationException, SAXException, IOException, InterruptedException, ClassNotFoundException {
 
         Parser parser = new PosixParser();
         Options options = new Options();
-        options.addOption("v", true,  "vertex schema");
-        options.addOption("e", true,  "edge schema");
-        options.addOption("f", true,  "File Name");
+        options.addOption("v", true, "vertex schema");
+        options.addOption("e", true, "edge schema");
+        options.addOption("f", true, "File Name");
         options.addOption("o", true, "Faunus query output directory");
         options.addOption("q", true, "query definition xml");
         CommandLine cmd = parser.parse(options, args);
@@ -49,73 +51,78 @@ public class GraphExporter {
         Path exporterOutputDir = new Path(new File(outputDir.toString(), "exporter").toString());
         String queryString = cmd.getOptionValue("q");
 
-        try {
-            FileSystem fs = FileSystem.get(new Configuration());
-            Configuration conf = new Configuration();
-            conf.set(FILE, fileName);
-            String vertexSchema = cmd.getOptionValue("v");
-            conf.set(VERTEX_SCHEMA, vertexSchema);
-            String edgeSchema = cmd.getOptionValue("e");
-            conf.set(EDGE_SCHEMA, edgeSchema);
-            Job job = new Job(conf, "Export graph");
-            job.setJarByClass(GraphExporter.class);
-            job.setMapperClass(GraphExportMapper.class);
-            job.setReducerClass(GraphExportReducer.class);
 
-            if (fs.exists(outputDir)) {
-                fs.delete(outputDir, true);
-            }
+        Configuration conf = new Configuration();
+        conf.set(FILE, fileName);
+        conf.set(VERTEX_SCHEMA, cmd.getOptionValue("v"));
+        conf.set(EDGE_SCHEMA, cmd.getOptionValue("e"));
+        Job job = new Job(conf, "Export graph");
+        job.setJarByClass(GraphExporter.class);
+        job.setMapperClass(GraphExportMapper.class);
+        job.setReducerClass(GraphExportReducer.class);
 
-            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-            InputSource is = new InputSource(new StringReader(queryString));
-            Document doc = dBuilder.parse(is);
-
-            NodeList nList = doc.getElementsByTagName("statement");
-            for (int temp = 0; temp < nList.getLength(); temp++) {
-                Node nNode = nList.item(temp);
-                String statement = nNode.getFirstChild().getNodeValue();
-                Runtime r = Runtime.getRuntime();
-
-                String subStepOutputDir = new File(queryOutputDir.toString(), "step-" + temp).toString();
-                String[] commandArray = new String[]{faunusGremlinFile, "-i", faunusPropertiesFile, statement, "-Dfaunus.output.location=" + subStepOutputDir};
-                Process p = r.exec(commandArray);
-                p.waitFor();
-
-                FileStatus[] fileStatuses = fs.listStatus(new Path(subStepOutputDir));
-                Path resultFolder = getResultFolder(fileStatuses);
-                Path resultFile = new Path(resultFolder, "sideeffect*");
-                TextInputFormat.addInputPath(job, resultFile);
-            }
-
-
-
-            TextOutputFormat.setOutputPath(job, exporterOutputDir);
-            job.setInputFormatClass(TextInputFormat.class);
-            job.setOutputFormatClass(TextOutputFormat.class);
-            job.setNumReduceTasks(1);
-            job.waitForCompletion(true);
-
-        } catch (IOException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        } catch (InterruptedException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        FileSystem fs = FileSystem.get(new Configuration());
+        if (fs.exists(outputDir)) {
+            fs.delete(outputDir, true);
         }
+
+        List<String> statements = getStatementListFromXMLString(queryString);
+        for (int i = 0; i < statements.size(); i++) {
+            String statement = statements.get(i);
+            Runtime r = Runtime.getRuntime();
+
+            String subStepOutputDir = new File(queryOutputDir.toString(), "step-" + i).toString();
+            String[] commandArray = new String[]{faunusGremlinFile, "-i", faunusPropertiesFile, statement, "-Dfaunus.output.location=" + subStepOutputDir};
+            Process p = r.exec(commandArray);
+            p.waitFor();
+
+            FileStatus[] fileStatuses = fs.listStatus(new Path(subStepOutputDir));
+
+            /* Faunus generates a folder for each map reduce job. For query that requires multiple map reduce
+            * jobs, the query result is written to the last created folder. */
+            Path resultFolder = getResultFolder(fileStatuses);
+
+            /* The query results are return to sideeffect files */
+            Path resultFile = new Path(resultFolder, "sideeffect*");
+
+            /* add the query output to exporter's input path for collecting the query results */
+            TextInputFormat.addInputPath(job, resultFile);
+        }
+
+        TextOutputFormat.setOutputPath(job, exporterOutputDir);
+        job.setInputFormatClass(TextInputFormat.class);
+        job.setOutputFormatClass(TextOutputFormat.class);
+        job.setNumReduceTasks(1);
+        job.waitForCompletion(true);
+    }
+
+    private static List<String> getStatementListFromXMLString(String queryXML) throws ParserConfigurationException, IOException, SAXException {
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+        InputSource is = new InputSource(new StringReader(queryXML));
+        Document doc = dBuilder.parse(is);
+
+        List<String> statements = new ArrayList<String>();
+        NodeList nList = doc.getElementsByTagName("statement");
+        for (int temp = 0; temp < nList.getLength(); temp++) {
+            Node nNode = nList.item(temp);
+            String statement = nNode.getFirstChild().getNodeValue();
+            statements.add(statement);
+        }
+        return statements;
     }
 
     public static Path getResultFolder(FileStatus[] fileStatuses) {
 
-        if(fileStatuses == null || fileStatuses.length == 0)
+        if (fileStatuses == null || fileStatuses.length == 0)
             throw new RuntimeException("FileStatus list is empty.");
 
         Path result = fileStatuses[0].getPath();
-        for(FileStatus status : fileStatuses) {
+        for (FileStatus status : fileStatuses) {
             int digit = Integer.parseInt(status.getPath().getName().replace("job-", ""));
             int currentMaxDigit = Integer.parseInt(result.getName().replace("job-", ""));
 
-            if(digit > currentMaxDigit)
+            if (digit > currentMaxDigit)
                 result = status.getPath();
         }
         return result;
