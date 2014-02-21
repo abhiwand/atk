61ddc5241b61ddfec7f518926fcbe0041f3361e2
Todd Lisonbee <todd.lisonbee@intel.com>
2014-02-21 04:09:47 -0800

    Merge remote-tracking branch 'remotes/origin/TRIB-1501' into sprint_9
    This is a WEIRD merge because it includes merging from GB2 repo into source_code repo.
    
    I deleted the old source_code/graphbuilder module in the process.
    
    Conflicts:
        IntelAnalytics/intel_analytics/graph/biggraph.py
        IntelAnalytics/intel_analytics/table/hbase/table.py
        IntelAnalytics/pom.xml
        graphbuilder/pom.xml

---
+++ graphbuilder/src/main/java/com/intel/hadoop/graphbuilder/graphelements/VertexID.java
@@ -0,0 +1,199 @@
+/**
+ * Copyright (C) 2013 Intel Corporation.
+ *     All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * For more about this software visit:
+ *     http://www.01.org/GraphBuilder
+ */
+package com.intel.hadoop.graphbuilder.graphelements;
+
+import com.intel.hadoop.graphbuilder.types.StringType;
+import com.intel.hadoop.graphbuilder.util.HashUtil;
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.hadoop.io.WritableComparable;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+/**
+ * The identifying information of a vertex: Its name, and its  its label.
+ *
+ * <p>
+ * It is used in the initial reducer of an <code>GraphGenerationJob</code>to detect and handle duplicate edges.
+ * Edges are considered duplicate if they have identical source, destination and label.
+ * Property maps are <i>not</i> used for purposes of comparison.
+ *    </p>
+ * @see com.intel.hadoop.graphbuilder.pipeline.output.titan.IntermediateEdgeWriterReducer
+ */
+public class VertexID<VidType extends WritableComparable> implements WritableComparable<VertexID<VidType>> {
+
+    private StringType label = null;
+    private VidType    name  = null;
+
+    /**
+     * Default, no argument constructor.
+     */
+    private VertexID() {
+    }
+
+    /**
+     * Construct <code>VertexID</code> from parameters.
+     * @param name The <code>VidType</code> vertex name
+     * @param label  The <code>String</code> vertex label
+     */
+    public VertexID(VidType name, StringType label) {
+        this.label = label;
+        this.name  = name;
+    }
+    /**
+     * Construct <code>VertexID</code> from parameters.
+     * @param name The <code>VidType</code> vertex name
+     */
+    public VertexID(VidType name) {
+        this.name  = name;
+        this.label = null;
+    }
+    /**
+     * @return The label of the vertex.
+     */
+    public StringType getLabel() {
+        return this.label;
+    }
+
+    /**
+     * Set the vertex label.
+     * @param label  the new vertex label
+     */
+    public void setLabel(StringType label) {
+        this.label = label;
+    }
+
+    /**
+     * @return The name of the vertex.
+     */
+    public VidType getName() {
+        return this.name;
+    }
+
+    /**
+     * Set the name of the vertex.
+     * @param name
+     */
+    public void setName(VidType name) {
+        this.name = name;
+    }
+    /**
+     * Equality test: Is the other object a <code>VertexID</code> whose components are equal to those of this vertexID?
+     *
+     * Because the <code>equals</code> method of the underlying <code>StringType</code> type for <code>label</code> and <code>name</code>
+     * depends only upon the equality of the underlying strings, the <code>equals</code> method <code>VertexID</code> depends only
+     * upon the string equality of the labels and names.
+     * @param obj  Any object.
+     * @return   true if the object is a VertexID of value equal to the current <code>VertexID</code>,
+     * false otherwise
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof VertexID) {
+            VertexID k = (VertexID) obj;
+            return (ObjectUtils.equals(this.label, k.label)
+                    && ObjectUtils.equals(this.name, k.name));
+        }
+        return false;
+    }
+
+    /**
+     * Hash of the VertexID is determined by the label/name pair as <code>StringType</code> objects.
+     *
+     * Because hash codes of <code>StringType</code> objects depend only upon underlying string value, the hashcode of a
+     * <code>VertexID</code> depends only on the underlying string values of its label and name.
+     *
+     * @return  hash of the VertexID
+     * @see StringType
+     */
+    @Override
+    public int hashCode() {
+        if (label == null && name == null) {
+            return 0xdeadbeef;
+        } else if (label == null && name != null) {
+            return name.hashCode();
+        } else if (label != null && name == null) {
+            return label.hashCode();
+        } else {
+            return HashUtil.hashPair(label, name);
+        }
+    }
+
+    /**
+     * compareTo implementation simply calls the equals method
+     * @param vertexID
+     */
+    public int compareTo(VertexID<VidType> vertexID) {
+        return equals(vertexID) ? 0 : 1;
+    }
+
+    /**
+     * @return a string representation of the <code>VertexID</code>
+     */
+    @Override
+    public String toString() {
+        if (label == null && name == null) {
+            return "null vertex";
+        } else if (label == null && name != null) {
+            return this.name.toString();
+        } else if (label != null && name == null) {
+            return "null vertex with label " + label;
+        } else {
+            return label + "." + name.toString();
+        }
+    }
+
+    /**
+     * Reads an edge from an input stream.
+     * @param input The input stream.
+     * @throws java.io.IOException
+     */
+    @Override
+    public void readFields(DataInput input) throws IOException {
+        boolean hasLabel = input.readBoolean();
+
+        name.readFields(input);
+
+        if (hasLabel) {
+            label.readFields(input);
+        }  else {
+            label = null;
+        }
+    }
+
+    /**
+     * Writes an edge to an output stream.
+     * @param output The output stream.
+     * @throws IOException
+     */
+    @Override
+    public void write(DataOutput output) throws IOException {
+        boolean hasLabel = (label != null);
+
+        output.writeBoolean(hasLabel);
+
+        name.write(output);
+
+        if (hasLabel) {
+            label.write(output);
+        }
+    }
+}
---
+++ graphbuilder/src/test/java/com/intel/hadoop/graphbuilder/pipeline/output/titan/KeyCommandLineParserTest.java
@@ -0,0 +1,114 @@
+/**
+ * Copyright (C) 2013 Intel Corporation.
+ *     All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * For more about this software visit:
+ *     http://www.01.org/GraphBuilder
+ */
+package com.intel.hadoop.graphbuilder.pipeline.output.titan;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class KeyCommandLineParserTest {
+
+    @Test
+    public void testParse_Empty() throws Exception {
+
+        // invoke method under test
+        List<GBTitanKey> gbTitanKeyList = new KeyCommandLineParser().parse("");
+
+        assertEquals(0, gbTitanKeyList.size());
+    }
+
+    @Test
+    public void testParse_WithOne() throws Exception {
+
+        // invoke method under test
+        List<GBTitanKey> list = new KeyCommandLineParser().parse("cf:userId;String;U;V");
+
+        assertEquals(1, list.size());
+
+        GBTitanKey userKey = list.get(0);
+        assertEquals("cf:userId", userKey.getName());
+        assertEquals(String.class, userKey.getDataType());
+        assertTrue(userKey.isVertexIndex());
+        assertFalse(userKey.isEdgeIndex());
+        assertTrue(userKey.isUnique());
+    }
+
+    @Test
+    public void testParse_WithTwo() throws Exception {
+
+        // invoke method under test
+        List<GBTitanKey> list = new KeyCommandLineParser().parse("cf:userId;String;U;V,cf:eventId;E;Long");
+
+        assertEquals(2, list.size());
+
+        GBTitanKey userKey = list.get(0);
+        assertEquals("cf:userId", userKey.getName());
+        assertEquals(String.class, userKey.getDataType());
+        assertTrue(userKey.isVertexIndex());
+        assertFalse(userKey.isEdgeIndex());
+        assertTrue(userKey.isUnique());
+
+        GBTitanKey eventKey = list.get(1);
+        assertEquals("cf:eventId", eventKey.getName());
+        assertEquals(Long.class, eventKey.getDataType());
+        assertFalse(eventKey.isVertexIndex());
+        assertTrue(eventKey.isEdgeIndex());
+        assertFalse(eventKey.isUnique());
+
+    }
+
+    @Test
+    public void testParse_UniqueMustBeVertex() throws Exception {
+
+        // invoke method under test
+        List<GBTitanKey> list = new KeyCommandLineParser().parse("name;U");
+
+        GBTitanKey userKey = list.get(0);
+        assertTrue(userKey.isVertexIndex());
+        assertTrue(userKey.isUnique());
+    }
+
+    @Test
+    public void testParse_DataType() throws Exception {
+        assertTypeStringIsParsedToType("String", String.class);
+        assertTypeStringIsParsedToType("Integer", Integer.class);
+        assertTypeStringIsParsedToType("Long", Long.class);
+        assertTypeStringIsParsedToType("Float", Float.class);
+        assertTypeStringIsParsedToType("Double", Double.class);
+    }
+
+    /**
+     * Assert that the supplied typeString is parsed to the expectedDataType
+     * @param typeString a type string supported by the parser: String, Long, ...
+     * @param expectedDataType the expected dataType class for that string: String.class, Long.class, ...
+     */
+    private void assertTypeStringIsParsedToType(String typeString, Class expectedDataType) {
+
+        // invoke method under test
+        List<GBTitanKey> list = new KeyCommandLineParser().parse("name;" + typeString );
+
+        GBTitanKey key = list.get(0);
+        assertEquals(expectedDataType, key.getDataType());
+    }
+}
