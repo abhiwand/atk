af233e8499d768e64de2d88dc576d2ed47e816b7
Bryn Keller <bryn.keller@intel.com>
2014-02-24 06:27:12 -0800

    Restructured rpm build scripts and arranged for precompiled virtpy environment

--- scripts/rpm/build_rpms.sh
+++
@@ -1,4 +0,0 @@
-. ./versions.sh
-rpmbuild --clean --define "_topdir ${PWD}" --define "BUILD_NUMBER $BUILD_NUMBER" --define "TIMESTAMP $TIMESTAMP" -bb SPECS/intelanalytics.spec
-rpmbuild --clean --define "_topdir ${PWD}" --define "BUILD_NUMBER $BUILD_NUMBER" --define "TIMESTAMP $TIMESTAMP" -bb SPECS/intelanalytics-python-deps-localbuild.spec
-rpmbuild --clean --define "_topdir ${PWD}" --define "BUILD_NUMBER $BUILD_NUMBER" --define "TIMESTAMP $TIMESTAMP" -bb SPECS/intelanalytics-python.spec
