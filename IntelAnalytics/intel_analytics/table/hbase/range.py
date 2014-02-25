97e709443bfc401114140fae6d88a7f44c48560b
Joyesh Mishra <joyesh.mishra@intel.com>
2014-02-25 09:29:49 -0800

    TRIB-742 Change range.py local variables

--- IntelAnalytics/intel_analytics/table/hbase/range.py
+++ IntelAnalytics/intel_analytics/table/hbase/range.py
@@ -46,24 +46,26 @@ class ETLRange:
     __range_mixed = 5		# For mixed input e.g. 1,4:10:2,12
     __range_depth = 6		# For ranges as depths
 
-    __d = "[-+]?[0-9]+(\.[0-9]+)?"                                # decimal pattern
-    __n = "-?[0-9]+"                                              # number pattern
-    __s = "%s:%s:%s" % (__d,__d,__d)				  	  # mixed pattern
+    __float_format = "[-+]?[0-9]+(\.[0-9]+)?"                              		# decimal pattern
+    __int_format = "-?[0-9]+"                                              		# number pattern
+    __step_float_format = "%s:%s:%s" % (__float_format,__float_format,__float_format)	# step pattern with floats
+    __step_int_format   = "%s:%s:%s" % (__int_format,__int_format,__int_format)	        # step pattern with ints
+
 
     def __init__(self, range):
         self.range_expression = "".join(range.split())		  # Remove all white space characters
 	
-	d,n,s = ETLRange.__d, ETLRange.__n, ETLRange.__s
+	d,n,sd,sn = ETLRange.__float_format, ETLRange.__int_format, ETLRange.__step_float_format, ETLRange.__step_int_format
  
         if (self.range_expression in ["date","week","month","year","dayofweek","monthofyear"]):
             self.range_type = ETLRange.__range_timeseries
-        elif (re.match("^%s:%s:%s$" % (n,n,n), self.range_expression)):
+        elif (re.match("^%s$" % (sn), self.range_expression)):
             self.range_type = ETLRange.__range_stepint
-        elif (re.match("^%s:%s:%s$" % (d,d,d), self.range_expression)):
+        elif (re.match("^%s$" % (sd), self.range_expression)):
             self.range_type = ETLRange.__range_stepfloat
         elif (re.match("^%s(,%s)+$" % (d,d),  self.range_expression)):
             self.range_type = ETLRange.__range_csv
-        elif (re.match("^(%s|%s)(,(%s|%s))+$" % (d,s,d,s),  self.range_expression)):
+        elif (re.match("^(%s|%s)(,(%s|%s))+$" % (d,sd,d,sd),  self.range_expression)):
             self.range_type = ETLRange.__range_mixed
         elif (re.match("^%s$" % (d),  self.range_expression)):  
             self.range_type = ETLRange.__range_depth
@@ -107,12 +109,12 @@ class ETLRange:
             range_str += "%f" %(max)
 
         elif (self.range_type == ETLRange.__range_mixed):
-	    d,s = ETLRange.__d, ETLRange.__s
+	    d,sd = ETLRange.__float_format, ETLRange.__step_float_format
 	    range_list = []
             for i in self.range_expression.split(','):
                 if (re.match("^%s$" % (d),i)):
                     range_list.append(i)
-                elif (re.match("^%s$" % (s),i)):
+                elif (re.match("^%s$" % (sd),i)):
                     range_list.append(ETLRange(i).toString())
             range_str = ",".join(range_list)
 
