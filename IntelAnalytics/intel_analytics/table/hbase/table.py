daeac952956581481a925a86a817c7b71d0b2b71
Joyesh Mishra <joyesh.mishra@intel.com>
2014-02-26 23:42:48 -0800

    TRIB-1450 Initial commit

--- IntelAnalytics/intel_analytics/table/hbase/table.py
+++ IntelAnalytics/intel_analytics/table/hbase/table.py
@@ -42,6 +42,7 @@ from intel_analytics.logger import stdout_logger as logger
 from intel_analytics.subproc import call
 from intel_analytics.report import MapOnlyProgressReportStrategy, PigJobReportStrategy
 from pydoop.hdfs.path import exists
+import matplotlib.pyplot as plt
 
 MAX_ROW_KEY = 'max_row_key'
 
@@ -197,6 +198,67 @@ class HBaseTable(object):
 
         return HBaseTable(new_table_name, self.file_name)
 
+    def get_column_statistics(self, column_name, force_recomputation):
+
+        hist_file = '%s_%s_histogram' % (self.table_name, column_name)
+        stat_file = '%s_%s_stats' % (self.table_name, column_name)
+
+        if not (os.path.isfile(hist_file) and os.path.isfile(stat_file)) or force_recomputation:
+
+            etl_schema = ETLSchema()
+            etl_schema.load_schema(self.table_name)
+    
+            feature_names_as_str = etl_schema.get_feature_names_as_CSV()
+            feature_types_as_str = etl_schema.get_feature_types_as_CSV()
+    
+            args = get_pig_args('pig_column_stats.py')
+    	    args.extend(['-i', self.table_name, 
+                    '-n', feature_names_as_str,
+                    '-t', feature_types_as_str,
+                    '-c', column_name])
+    
+            return_code = call(args, report_strategy = etl_report_strategy())
+            if return_code:
+                raise HBaseTableException('Could not generate statistics')
+
+        g = lambda x: 'hadoop fs -cat %s/part* > %s' % (x, x)
+
+        cmd_hist, cmd_stat = g(hist_file), g(stat_file)
+        
+        call(cmd_hist, shell=True)
+        call(cmd_stat, shell=True)
+        
+        with open(hist_file) as h:
+            hlines = h.readlines()
+            
+        with open(stat_file) as s:
+            slines = s.readlines()
+        
+        data_x = []
+        data_y = []
+        for i in range(len(hlines)):
+            t = hlines[i].split()
+            data_x.append(int(t[0]))
+            data_y.append(int(t[1]))
+            
+        n, bins, patches = plt.hist(data_x, weights=data_y)
+
+        max_coordinate = int([s.split('=')[1] for s in slines if "max" in s][0])
+
+        latex_symbols = {'max' : '$\max$', 'min' : '$\min$', 'avg' : '$\mu$', 'stdev' : '$\sigma$', 'var' : '$\sigma^2$'}
+        for i,j in latex_symbols.iteritems():
+            slines = [w.replace(i, j) for w in slines]
+        
+        stats = "".join(sorted(slines))
+        
+        plt.xlabel(column_name)
+        plt.ylabel('frequency')
+        plt.title('Column Statistics - %s' % (column_name))
+        plt.text(max_coordinate + 25, 0, r'%s' % (stats), fontsize = 18)
+        plt.grid(True)
+        plt.show()
+
+        return slines
 
     def drop_columns(self, columns):
         etl_schema = ETLSchema()
