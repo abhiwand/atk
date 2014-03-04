51a3b17a62533a8020e123c979f06a228bd366cf
Briton Barker <briton.barker@intel.com>
2014-03-03 16:58:01 -0800

    copy fixes to create-html-public.sh (see TRIB-1978)

--- IntelAnalytics/intel_analytics/doc/create-html-public.sh
+++ IntelAnalytics/intel_analytics/doc/create-html-public.sh
@@ -1,3 +1,42 @@
+#!/bin/bash
+
+##############################################################################
+# INTEL CONFIDENTIAL
+#
+# Copyright 2014 Intel Corporation All Rights Reserved.
+#
+# The source code contained or described herein and all documents related to
+# the source code (Material) are owned by Intel Corporation or its suppliers
+# or licensors. Title to the Material remains with Intel Corporation or its
+# suppliers and licensors. The Material may contain trade secrets and
+# proprietary and confidential information of Intel Corporation and its
+# suppliers and licensors, and is protected by worldwide copyright and trade
+# secret laws and treaty provisions. No part of the Material may be used,
+# copied, reproduced, modified, published, uploaded, posted, transmitted,
+# distributed, or disclosed in any way without Intel's prior express written
+# permission.
+#
+# No license under any patent, copyright, trade secret or other intellectual
+# property right is granted to or conferred upon you by disclosure or
+# delivery of the Materials, either expressly, by implication, inducement,
+# estoppel or otherwise. Any license under such intellectual property rights
+# must be express and approved by Intel in writing.
+##############################################################################
+set -e
+
+if [[ -f /usr/lib/IntelAnalytics/virtpy/bin/activate ]]; then
+    ACTIVATE_FILE=/usr/lib/IntelAnalytics/virtpy/bin/activate
+else
+    ACTIVATE_FILE=/usr/local/virtpy/bin/activate
+fi
+
+if [[ ! -f $ACTIVATE_FILE ]]; then
+    echo "Virtual Environment is not installed please execute install_pyenv.sh to install."
+    exit 1
+fi
+
+source $ACTIVATE_FILE
+
 # Make sure env vars are set: CLUSTER_HOSTS, HADOOP_HOME, TITAN_HOME
 set ch = "bogus CLUSTER_HOSTS var set for doc creation"
 set hh = "bogus HADOOP_HOME var set for doc creation"
@@ -13,9 +52,15 @@ if [ -z "$TITAN_HOME" ]; then
     export TITAN_HOME=th
 fi
 
-python intel_analytics/doc/efuncgen.py > intel_analytics/doc/source/efunc.rst
-make -B html -C intel_analytics/doc
-zip -r intel_analytics_pydoc.zip intel_analytics/doc/build
+DIR="$( cd "$( dirname "$BASH_SOURCE[0]}" )" && pwd )"
+# see http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
+
+pushd $DIR
+
+python efuncgen.py > source/efunc.rst
+make -B html
+zip -r intel_analytics_pydoc.zip build
+popd
 
 # undo if we made any changes above
 if [ "$CLUSTER_HOSTS" == ch ]; then
