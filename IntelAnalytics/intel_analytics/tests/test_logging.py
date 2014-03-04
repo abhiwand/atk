9179511b556b68964256705d0fbd5d400e8caf32
schen55 <steven.shih-chi.chen@intel.com>
2014-03-04 10:09:24 -0800

    1. get_file_name_from_datetime receive datetime 2. use os.path.join to form a path for the new file 3. mock filehandler in unit test

--- IntelAnalytics/intel_analytics/tests/test_logging.py
+++ IntelAnalytics/intel_analytics/tests/test_logging.py
@@ -1,22 +1,31 @@
-from logging import FileHandler
+import logging
+import unittest
+
+import datetime
 import time
 
-import unittest
-from intel_analytics.logger import stdout_logger, get_file_name_from_datetime
 
+class FakeFileHandler:
+    def __init__(self, filename, mode='a', encoding=None, delay=0):
+        pass
+
+logging.FileHandler = FakeFileHandler
+from intel_analytics.logger import stdout_logger
 
 class LoggingTest(unittest.TestCase):
+
     def test_propagation(self):
         self.assertEqual(0, stdout_logger.propagate)
 
     def test_only_file_handler(self):
-        for handler in stdout_logger.handlers:
-            self.assertTrue(isinstance(handler, FileHandler))
+        self.assertEqual(1, len(stdout_logger.handlers))
+        self.assertTrue(isinstance(stdout_logger.handlers[0], logging.FileHandler))
 
     def test_distinct_file_name(self):
-        filename1 = get_file_name_from_datetime()
+        from intel_analytics.logger import get_file_name_from_datetime
+        filename1 = get_file_name_from_datetime(datetime.datetime.now())
         time.sleep(0.001)
-        filename2 = get_file_name_from_datetime()
+        filename2 = get_file_name_from_datetime(datetime.datetime.now())
         self.assertNotEqual(filename1, filename2)
 
 
