cafce90b6ab3dcfc8a9ff1d99689f477730f367f
rodorad <rene.o.dorado@intel.com>
2014-03-11 22:55:33 -0700

    add aws scrips

---
+++ aws/deploy/scripts/IntelAnalytics_setup_user.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Description: Used for generating default IA running account and the default ssh 
+# Note: Expected to be executed from admin node
+
+source IntelAnalytics_setup_env.sh
+
+for n in `cat ${IA_HOSTS}`
+do
+	ssh -t -i ${IA_PEM} ${n} "sudo groupadd -g ${IA_UID};"
+	ssh -t -i ${IA_PEM} ${n} "sudo useradd -g ${IA_UID} -u ${IA_UID} -s /bin/bash -m -d /home/${IA_USR} ${IA_USR};"
+	ssh -t -i ${IA_PEM} ${n} "echo ${IA_USR} | sudo passwd --stdin ${IA_USR};"
+	scp -i ${IA_PEM} -r ${IA_USRSSH} ${n}:/home/${IA_USR}
+	ssh -t -i ${IA_PEM} ${n} "sudo chown -R ${IA_USR}.${IA_USR} /home/${IA_USR}"
+	ssh -t -i ${IA_PEM} ${n} 'sudo chmod 700 /home/${IA_USR}/.ssh'
+done
