ea6b0999f1e412749105db4dd37f0e170b1605a1
schen55 <steven.shih-chi.chen@intel.com>
2014-03-13 16:01:44 -0700

    define BeforeDate and AfterDate pig udf for comparing datetime

---
+++ feateng/src/test/java/com/intel/pig/udf/AfterDateTest.java
@@ -0,0 +1,70 @@
+package com.intel.pig.udf;
+
+import org.apache.pig.EvalFunc;
+import org.apache.pig.data.Tuple;
+import org.apache.pig.data.TupleFactory;
+import org.apache.pig.impl.PigContext;
+import org.joda.time.DateTime;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class AfterDateTest {
+    EvalFunc<?> testFn;
+    @Before
+    public void setup() throws Exception {
+        testFn = (EvalFunc<?>) PigContext
+                .instantiateFuncFromSpec("com.intel.pig.udf.AfterDate");
+    }
+
+    @Test(expected=IOException.class)
+    public void invalid_input() throws IOException {
+        DateTime d1 = new DateTime(2014, 3, 13, 11, 50, 0, 0);
+        List<DateTime> list = new ArrayList<DateTime>();
+        list.add(d1);
+
+        Tuple inTuple = TupleFactory.getInstance().newTuple(list);
+        testFn.exec(inTuple);
+    }
+
+    @Test
+    public void date_equal() throws IOException {
+        DateTime d1 = new DateTime(2014, 3, 13, 11, 50, 0, 0);
+        DateTime d2 = new DateTime(2014, 3, 13, 11, 50, 0, 0);
+        List<DateTime> list = new ArrayList<DateTime>();
+        list.add(d1);
+        list.add(d2);
+
+        Tuple inTuple = TupleFactory.getInstance().newTuple(list);
+        assertEquals(false, testFn.exec(inTuple));
+    }
+
+    @Test
+    public void date_bigger() throws IOException {
+        DateTime d1 = new DateTime(2015, 3, 13, 11, 50, 0, 0);
+        DateTime d2 = new DateTime(2014, 3, 13, 11, 50, 0, 0);
+        List<DateTime> list = new ArrayList<DateTime>();
+        list.add(d1);
+        list.add(d2);
+
+        Tuple inTuple = TupleFactory.getInstance().newTuple(list);
+        assertEquals(true, testFn.exec(inTuple));
+    }
+
+    @Test
+    public void date_smaller() throws IOException {
+        DateTime d1 = new DateTime(2014, 3, 13, 11, 50, 0, 0);
+        DateTime d2 = new DateTime(2015, 3, 13, 11, 50, 0, 0);
+        List<DateTime> list = new ArrayList<DateTime>();
+        list.add(d1);
+        list.add(d2);
+
+        Tuple inTuple = TupleFactory.getInstance().newTuple(list);
+        assertEquals(false, testFn.exec(inTuple));
+    }
+}
