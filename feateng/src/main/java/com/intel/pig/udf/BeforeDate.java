fe520e41061f80abfce04f0536a8e72a1a3c7900
schen55 <steven.shih-chi.chen@intel.com>
2014-03-13 16:01:44 -0700

    define BeforeDate and AfterDate pig udf for comparing datetime

---
+++ feateng/src/main/java/com/intel/pig/udf/BeforeDate.java
@@ -0,0 +1,22 @@
+package com.intel.pig.udf;
+
+
+import org.apache.pig.FilterFunc;
+import org.apache.pig.data.Tuple;
+import org.joda.time.DateTime;
+
+import java.io.IOException;
+
+public class BeforeDate extends FilterFunc {
+    @Override
+    public Boolean exec(Tuple objects) throws IOException {
+
+        if(objects.size() != 2)
+            throw new IOException("Have to pass two DateTime objects");
+
+        DateTime first = (DateTime) objects.get(0);
+        DateTime second = (DateTime) objects.get(1);
+
+        return first.isBefore(second);
+    }
+}
