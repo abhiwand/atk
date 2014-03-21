3c9037a5413b7bd785152f3c7b02569b7f7344e3
Joyesh Mishra <joyesh.mishra@intel.com>
2014-03-21 13:41:37 -0700

    TRIB-1450 Correct unit tests

--- IntelAnalytics/intel_analytics/tests/test_hbase_table.py
+++ IntelAnalytics/intel_analytics/tests/test_hbase_table.py
@@ -923,6 +923,9 @@ class HbaseTableTest(unittest.TestCase):
 
     @patch('intel_analytics.table.hbase.table.call')
     @patch('intel_analytics.table.hbase.table.ETLSchema')
+    @patch('intel_analytics.table.hbase.table.__get_column_statistics_filenames')
+    @patch('intel_analytics.table.hbase.table.__plot_column_distribution')
+    @patch('intel_analytics.table.hbase.table.__create_hist_stat_file_from_all_data')
     def test_column_statistics(self, etl_schema_class, call_method):
 
         table_name = "original_table"
