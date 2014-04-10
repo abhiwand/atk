a3d54f46adb2704950f2d30c75a60ee206b9a4f7
rodorad <rene.o.dorado@intel.com>
2014-04-10 13:26:58 -0700

    add initial packaing for both rpm and deb.

--- package/package_sbt.sh
+++ package/package_sbt.sh
@@ -8,11 +8,15 @@ pwd
 echo "sbt package "
 sbt package
 
+echo "tar sbt package output"
 mkdir -p usr/lib/intelanalytics/
 
 cp -R target/scala-2.10/* usr/lib/intelanalytics/
 
 tar -pczf source.tar.gz usr
 
-popd ..
+popd 
 
+tar -pczf source.tar.gz ../usr
+
+echo "done packaging sbt" 
