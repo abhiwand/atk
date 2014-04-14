a709880cac4465ff793cb0bdf0c53f02ec3d6b59
Mohitdeep Singh <mohitdee@GAO-WS4.(none)>
2014-04-14 15:38:43 -0700

    TRIB-2293 Incorporating code review comments and more test cases

--- engine/src/main/scala/com/intel/intelanalytics/engine/Parser.scala
+++ engine/src/main/scala/com/intel/intelanalytics/engine/Parser.scala
@@ -23,23 +23,65 @@
 
 package com.intel.intelanalytics.engine
 
-//The parser object
-object Parser{
-	 // function to split a string in different columns
-	 def parse_csv(s: String): List[String] = {
-	 	//pattern to match 
-	 	val ptn = "(('[^']*')|([^,]+))".r
-	 	//find pattern and create a list of string
-	 	return  ptn.findAllMatchIn(s).map(_.toString).toList
-	 }
+/** This object parses comma delimited strings into List[String]
+  * Usage:
+  * scala> import com.intelanalytics.engine.Row
+  * scala> val out = Row.parseRecord("foo,bar")
+  * scala> val out = Row.parseRecord("a,b,\"foo,is this ,bar\",foobar ")
+  * scala> val out = Row.parseRecord(" a,b,'',\"\"  ")
+  * Modified from: http://poundblog.wordpress.com/2013/06/06/a-scala-parser-combinator-grammar-for-csv/
+  */
+import util.parsing.combinator.RegexParsers
 
-	 def main(args: Array[String]){
-	 	val test = "foo and bar,bar and foo,'foo, is bar'"
-	 	val row = parse_csv(test)
-	 	
-	 	
-	 	for (i <- 0 to row.length-1){
-	 		println(row(i))
-	 	}
-	 }
+trait RecordParser extends RegexParsers {
+  /** A generic interface to handle parsing logic*/
+  override val whiteSpace = """[ \t]""".r
+  
+  override val skipWhitespace = false
+  
+  /** Takes in the string and applies the parsing logic   */
+  def apply(line: String) : List[String]={
+  	parseAll(record,line).get
+  }
+
+  
+  def record:Parser[List[String]] = repsep(field,",") ^^ {case x => x}
+  
+  // Specify various portions of strings and how to handle them and the order of precedence
+  def field: Parser[String] = TEXT ||| SINGLEQUOTES | STRING  |EMPTY
+ 
+  val STRING: Parser[String] = whiteSpace.* ~> "\"" ~> rep("\"\"" | """[^\"]""".r) <~ "\"" <~ whiteSpace.* ^^ makeString
+  val TEXT: Parser[String] = rep1("""[^,\n\r\"]""".r) ^^ makeText
+  val EMPTY: Parser[String] = "" ^^ makeEmpty
+ 
+  def SINGLEQUOTES = "'" ~> "[^']+".r <~ "'" ^^ {case a => (""/:a)(_+_)}
+  def makeText: (List[String]) => String
+  def makeString: (List[String]) => String
+  def makeEmpty: (String) => String
+}
+ 
+
+ 
+trait RecordParserAction {
+  /** Implementation of text,string and empty cases handler*/
+  // no trimming of WhiteSpace
+  def makeText = (text: List[String]) => text.mkString("")
+  // remove embracing quotation marks
+  // replace double quotes by single quotes
+  def makeString = (string: List[String]) => string.mkString("").replaceAll("\"\"", "\"")
+  
+  // modify result of EMPTY token if required
+  def makeEmpty = (string: String) => ""
+}
+
+ 
+object Row extends RecordParser with RecordParserAction {
+  /** Creates a Row with given input String
+    *
+    * @param line to be parsed
+    */
+    def parseRecord(line:String):List[String]={
+    	//println(apply(record))
+     return apply(line)
+  }  
 }
