65e57ff7875bd919223747a3a359201aa23d816c
Briton Barker <briton.barker@intel.com>
2014-04-16 09:48:23 -0700

    frame updates, enabled add_column on little frame

--- python/intelanalytics/tests/test_little_frame.py
+++ python/intelanalytics/tests/test_little_frame.py
@@ -53,11 +53,44 @@ data_source_nenfr_na = SimpleDataSource(schema=schema_nenfr, rows=rows_nenfr)
 
 class TestLittleFrame(unittest.TestCase):
 
+    def validate_schema(self, frame, schema):
+        frame_schema = frame.schema
+        self.assertEquals(len(schema), len(frame_schema))
+        for n, t in schema:
+            self.assertEquals(frame_schema[n], t)
+
+    def validate_rows(self, frame, rows):
+        for i, r in enumerate(frame.take(len(rows))):
+            self.assertEquals(rows[i], r)
+
+    def validate_drop(self, frame, src_rows, cases):
+        self.assertEquals(len(cases), frame.count())
+        for i, r in enumerate(frame.take(100)):
+            self.assertEquals(src_rows[cases[i]], r)
+
+    def validate_drop_ab_123(self, frame, cases):
+        self.validate_drop(frame, rows_ab_123, map(lambda x: x-1, cases))
+
+    def validate_drop_nenfr(self, frame, cases):
+        self.validate_drop(frame, rows_nenfr, map(lambda x: x-1, cases))
+
     def test_simple_creation_and_repr(self):
         f = LittleFrame(data_source_ab_123)
         r = repr(f)
         #print r
         self.assertEquals(expected_repr_ab_123, r)
+
+    def test_take(self):
+        f = LittleFrame(data_source_nenfr_na)
+        rows = f.take(10)
+        self.assertEquals(9, len(rows))
+        for i, r in enumerate(rows):
+            self.assertEquals(rows_nenfr[i], r)
+
+    def test_take_roundtrip(self):
+        f = LittleFrame(data_source_nenfr_na)
+        g = LittleFrame(SimpleDataSource(schema=f.schema, rows=f.take(100)))
+        self.assertEquals(repr(f), repr(g))
 
     def test_drop(self):
         f = LittleFrame(data_source_ab_123)
@@ -71,24 +104,6 @@ class TestLittleFrame(unittest.TestCase):
         f = LittleFrame(data_source_ab_123)
         f.filter(lambda x: x.a == 2)
         self.validate_drop_ab_123(f, [2])
-
-    def test_take(self):
-        f = LittleFrame(data_source_nenfr_na)
-        rows = f.take(10)
-        self.assertEquals(9, len(rows))
-        for i, r in enumerate(rows):
-            self.assertEquals(rows_nenfr[i], r)
-
-    def validate_drop(self, frame, src_rows, cases):
-        self.assertEquals(len(cases), frame.count())
-        for i, r in enumerate(frame.take(100)):
-            self.assertEquals(src_rows[cases[i]], r)
-
-    def validate_drop_ab_123(self, frame, cases):
-        self.validate_drop(frame, rows_ab_123, map(lambda x: x-1, cases))
-
-    def validate_drop_nenfr(self, frame, cases):
-        self.validate_drop(frame, rows_nenfr, map(lambda x: x-1, cases))
 
     def test_dropna_no_args(self):
         f = LittleFrame(data_source_nenfr_na)
@@ -114,3 +129,56 @@ class TestLittleFrame(unittest.TestCase):
         f = LittleFrame(data_source_nenfr_na)
         f.dropna(all, ['n', 'en'])
         self.validate_drop_nenfr(f, [1, 2, 3, 4, 5, 6, 9])
+
+    def test_add_column_1(self):
+        f = LittleFrame(data_source_ab_123)
+        f.add_column('a_new', lambda r: r['a'] * 3)
+        #print repr(f)
+        self.validate_schema(f, [('a', int32), ('b', str), ('a_new', int64)])
+        self.validate_rows(f, [(1, 'one', 3), (2, 'two', 6), (3, 'three', 9)])
+
+    def test_add_column_2(self):
+        f = LittleFrame(data_source_ab_123)
+        f.add_column(('a3', 'bnew'), lambda r: (r['a'] * 3, r['b'] + '_new'))
+        #print repr(f)
+
+    def test_get_names_from_schema(self):
+        from intelanalytics.little.frame import _get_names_from_schema
+        self.assertEquals(('x',), _get_names_from_schema('x'))
+        self.assertEquals(('x',), _get_names_from_schema(('x',)))
+        self.assertEquals(('x',), _get_names_from_schema(('x', int32)))
+        self.assertEquals(('x', 'y'), _get_names_from_schema(('x', 'y')))
+        self.assertEquals(('x', 'y'), _get_names_from_schema((('x', int32), 'y')))
+        self.assertEquals(('x', 'y', 'z'), _get_names_from_schema((('x', int32), 'y', ('z', int32))))
+        self.assertEquals(('x', 'y', 'z'), _get_names_from_schema((('x', int32), ('y', str), ('z', int32))))
+
+    def test_get_pairs_from_schema(self):
+        from intelanalytics.little.frame import _get_pairs_from_column_schema
+        self.assertEquals([('x', unknown)], _get_pairs_from_column_schema('x'))
+        self.assertEquals([('x', unknown)], _get_pairs_from_column_schema(('x',)))
+        self.assertEquals([('x', int32)], _get_pairs_from_column_schema(('x', int32)))
+        self.assertEquals([('x', unknown), ('y', unknown)], _get_pairs_from_column_schema(('x', 'y')))
+        self.assertEquals([('x', int32), ('y', unknown)], _get_pairs_from_column_schema((('x', int32), 'y')))
+        self.assertEquals([('x', int32), ('y', unknown), ('z', int32)], _get_pairs_from_column_schema((('x', int32), 'y', ('z', int32))))
+        self.assertEquals([('x', int32), ('y', str), ('z', int32)], _get_pairs_from_column_schema((('x', int32), ('y', str), ('z', int32))))
+
+    def test_rename_column(self):
+        f = LittleFrame(data_source_ab_123)
+        f.rename_column('a', 'a_prime')
+        self.validate_schema(f, [('a_prime', int32), ('b', str)])
+        self.validate_rows(f, [(1, 'one'), (2, 'two'), (3, 'three')])
+
+    def test_rename_column_multiple_names(self):
+        f = LittleFrame(data_source_nenfr_na)
+        f.rename_column(('en', 'fr'), ('english', 'french'))
+        self.validate_schema(f, [('n', int32), ('english', str), ('french', str)])
+
+    def test_delete_column(self):
+        f = LittleFrame(data_source_nenfr_na)
+        f.delete_column('en')
+        self.validate_schema(f, [('n', int32), ('fr', str)])
+
+    def test_delete_column_multiple(self):
+        f = LittleFrame(data_source_nenfr_na)
+        f.delete_column(['fr', 'en'])
+        self.validate_schema(f, [('n', int32)])
