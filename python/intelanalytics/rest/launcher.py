b2b080c0013d9ab06999e5f2e1fea8051436fd28
schen55 <steven.shih-chi.chen@intel.com>
2014-04-17 16:08:18 -0700

    define shutdown method to shutdown the webhook

--- python/intelanalytics/rest/launcher.py
+++ python/intelanalytics/rest/launcher.py
@@ -2,22 +2,18 @@ from multiprocessing import Process
 import multiprocessing
 from hooks import WebhookServer
 
-server = WebhookServer()
 message_queue = multiprocessing.Queue()
 
-def launch(port):
-    server_process = Process(target=start_webhook_server, args=(message_queue, port))
-    server_process.start()
+class Launcher(object):
 
-def shutdown():
-    stop_webhookserver()
+    def __init__(self):
+        self.server = WebhookServer()
 
+    def launch(self, port):
+        self.server_process = Process(target=self.server.start, args=(port, message_queue))
+        self.server_process.start()
 
+    def shutdown(self):
+        self.server_process.terminate()
 
-def start_webhook_server(message_queue, port):
-    server.start(port, message_queue)
-
-
-def stop_webhookserver():
-    server.stop()
 
