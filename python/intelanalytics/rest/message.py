b3186e5138776e775ade04cb15f007126f5db46e
schen55 <steven.shih-chi.chen@intel.com>
2014-04-17 16:34:05 -0700

    remove status_code

--- python/intelanalytics/rest/message.py
+++ python/intelanalytics/rest/message.py
@@ -1,19 +1,14 @@
 class Message(object):
-    def __init__(self, job_id, status_code, message):
+    def __init__(self, job_id, content):
         self._job_id = job_id
-        self._status_code = status_code
-        self._message = message
+        self._content = content
 
     @property
     def job_id(self):
         return self._job_id
 
     @property
-    def status_code(self):
-        return self._status_code
-
-    @property
-    def message(self):
-        return self._message
+    def content(self):
+        return self._content
 
 
