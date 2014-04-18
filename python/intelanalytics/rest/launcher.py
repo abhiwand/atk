bb9c68a4e134a39687ecf01c8a400217751055e7
schen55 <steven.shih-chi.chen@intel.com>
2014-04-18 12:20:53 -0700

    1. add legal info 2. create test

--- python/intelanalytics/rest/launcher.py
+++ python/intelanalytics/rest/launcher.py
@@ -4,25 +4,23 @@ import multiprocessing
 from hooks import WebhookServer
 
 message_queue = multiprocessing.Queue()
+web_server = WebhookServer()
 
 class _Launcher(object):
-
-    def __init__(self):
-        self.server = WebhookServer()
 
     def __del__(self):
         self.shutdown()
 
     def launch(self, port):
-        self.server_process = Process(target=self.server.start, args=(port, message_queue))
+        self.server_process = Process(target=web_server.start, args=(port, message_queue))
         self.server_process.start()
 
     def shutdown(self):
         self.server_process.terminate()
 
 
-launcher = _Launcher()
-atexit.register(launcher.shutdown)
+webhook_launcher = _Launcher()
+atexit.register(webhook_launcher.shutdown)
 
 
 
