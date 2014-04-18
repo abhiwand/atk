59e03b3acb2b51fa51074995b123a202955b8c25
schen55 <steven.shih-chi.chen@intel.com>
2014-04-18 12:20:53 -0700

    1. add legal info 2. create test

--- python/intelanalytics/rest/message.py
+++ python/intelanalytics/rest/message.py
@@ -1,14 +1,37 @@
+##############################################################################
+# INTEL CONFIDENTIAL
+#
+# Copyright 2014 Intel Corporation All Rights Reserved.
+#
+# The source code contained or described herein and all documents related to
+# the source code (Material) are owned by Intel Corporation or its suppliers
+# or licensors. Title to the Material remains with Intel Corporation or its
+# suppliers and licensors. The Material may contain trade secrets and
+# proprietary and confidential information of Intel Corporation and its
+# suppliers and licensors, and is protected by worldwide copyright and trade
+# secret laws and treaty provisions. No part of the Material may be used,
+# copied, reproduced, modified, published, uploaded, posted, transmitted,
+# distributed, or disclosed in any way without Intel's prior express written
+# permission.
+#
+# No license under any patent, copyright, trade secret or other intellectual
+# property right is granted to or conferred upon you by disclosure or
+# delivery of the Materials, either expressly, by implication, inducement,
+# estoppel or otherwise. Any license under such intellectual property rights
+# must be express and approved by Intel in writing.
+##############################################################################
+
 class Message(object):
     def __init__(self, job_id, content):
-        self._job_id = job_id
-        self._content = content
+        self.__job_id = job_id
+        self.__content = content
 
     @property
     def job_id(self):
-        return self._job_id
+        return self.__job_id
 
     @property
     def content(self):
-        return self._content
+        return self.__content
 
 
