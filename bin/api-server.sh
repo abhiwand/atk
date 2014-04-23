c0714fa15aa44b9afee9d7ad6d0c8d07e61aa8fe
Bryn Keller <bryn.keller@intel.com>
2014-04-23 06:27:02 -0700

    Added api-server.sh and updated the build config

---
+++ bin/api-server.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -o nounset
+set -o errexit
+
+DIR="$( cd "$( dirname "$0" )" && pwd )"
+
+echo $DIR
+
+if [[ -f $DIR/../launcher/target/scala-2.10/launcher.jar ]]
+then
+    LAUNCHER=$DIR/../launcher/target/scala-2.10/launcher.jar
+    CONFDIR=$DIR/../api-server/src/main/resources
+else
+    LAUNCHER=$DIR/launcher.jar
+    CONFDIR=$DIR/conf
+fi
+
+java -cp $CONFDIR:$LAUNCHER com.intel.intelanalytics.component.Boot api-server com.intel.intelanalytics.service.ServiceApplication
