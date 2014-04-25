c023057ce63e199eb799640a5f7ffa94d819c1d8
Todd Lisonbee <todd.lisonbee@intel.com>
2014-04-25 07:19:56 -0700

    Adding scaladoc

--- graphbuilder-3/src/main/scala/com/intel/graphbuilder/driver/spark/titan/GraphBuilderConfig.scala
+++ graphbuilder-3/src/main/scala/com/intel/graphbuilder/driver/spark/titan/GraphBuilderConfig.scala
@@ -35,7 +35,7 @@ import com.intel.graphbuilder.util.SerializableBaseConfiguration
  * @param edgeRules rules for parsing Edges
  * @param titanConfig connect to Titan
  * @param biDirectional true to create an Edge in the opposite direction for each one parsed
- * @param append true to append to an existing Graph, slower because each write requires a read.
+ * @param append true to append to an existing Graph, slower because each write requires a read (incremental graph construction).
  * @param retainDanglingEdges true to add extra vertices for dangling edges, false to drop dangling edges
  * @param inferSchema true to automatically infer the schema from the rules and, if needed, the data, false if the schema is already defined.
  */
