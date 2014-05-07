d4b04fcee025c75f38c6471c2e0f5127a3262dde
rodorad <rene.o.dorado@intel.com>
2014-05-07 16:58:41 -0700

    added a python 2.7.5 package script since we might need it for centos

---
+++ package/config/python-2.7.5/rpm.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+source common.sh
+
+log "Build rpm package"
+
+packageName=$1
+tarFile=$2
+TAR_FILE=$tarFile
+version=$3
+
+#deleteOldBuildDirs
+
+#log "package name: $packageName, tar file: $tarFile, version: $version, script path: $SCRIPTPATH"
+
+#log "copy and rename: $tarFile"
+#cp $tarFile $SCRIPTPATH/rpm/SOURCES/${packageName}-${version}.tar.gz
+
+#SUMMARY="$packageName-$version Build number: $BUILD_NUMBER. TimeStamp $TIMESTAMP"
+#DESCRIPTION=$SUMMARY
+
+#REQUIRES=" java >= 1.7"
+
+#rpmSpec > $SCRIPTPATH/rpm/SPECS/$packageName.spec
+
+topDir="$SCRIPTPATH/rpm"
+#exit 1
+pushd $SCRIPTPATH/rpm
+
+log "clean up build dirs"
+rm -rf BUILD/*
+rm -rf BUILDROOT/*
+
+
+log $BUILD_NUMBER
+
+echo "top dir $topDir"
+
+rpmbuild --define "_topdir $topDir"  --define "BUILD_NUMBER $BUILD_NUMBER" --define "VERSION $VERSION" -ba SPECS/$packageName.spec
+
+popd $SCRIPTPATH/rpm 
+
