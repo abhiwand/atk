d0990e74e13b1bba1a0f6ebd8c8de999b69ef026
Briton Barker <briton.barker@intel.com>
2014-05-16 15:16:19 -0700

    checkpoint command.py

--- python/intelanalytics/rest/command.py
+++ python/intelanalytics/rest/command.py
@@ -21,12 +21,44 @@ class Command(object):
     def get_payload(self):
         return dict(self.__dict__)
 
-    def update(self, payload):    # command issue reponse provides info
+    def update(self, payload):    # command issue response provides info
         if self.id == 0:
             self.id = payload['id']
         elif self.id != payload['id']:
             raise ValueError("Received a different command id from server?")
         self.complete = payload['complete']
+
+
+import time
+
+
+class Polling(object):
+
+    def poll(self,
+             command,
+             interval_secs=0.5,
+             backoff_factor=2,
+             timeout_secs=10):  # TODO - timeout appropriateness at scale :^)
+
+        start_time = time.time()
+        if self._get_completion_status(command):
+            return True
+        while True:
+            time.sleep(interval_secs)
+            wait_time = time.time() - start_time
+            if self._get_completion_status(command):
+                return True
+            if wait_time > timeout_secs:
+                msg = "Polling timeout for command %s after ~%d seconds" \
+                      % (command.name, wait_time)
+                logger.error(msg)
+                raise RuntimeError(msg)
+            interval_secs *= backoff_factor
+
+    @staticmethod
+    def _get_completion_status(command):
+        response = rest_http.get(command.uri)
+        return response.json()['complete']
 
 
 class Executor(object):
@@ -37,17 +69,24 @@ class Executor(object):
     def issue_command(self, command):
         logger.info("Issuing command " + command.name)
         self._enqueue_command(command)
-        response = rest_http.post("commands", command.get_payload())
-        command.update(response.json())
-        if command.complete:
-            self._dequeue_command(command)
-        return response
+        try:
+            response = rest_http.post("commands", command.get_payload())
+            command.update(response.json())
+            if command.complete:
+                self._dequeue_command(command)
+            return response
+        except KeyboardInterrupt:
+            self.cancel_command(command)
 
     def _enqueue_command(self, command):
         self.queue.append(command)
 
     def _dequeue_command(self, command):
         self.queue.remove(command)
+
+    def cancel_command(self, command):
+        # TODO - implement command cancellation
+        self._dequeue_command(command)
 
 
 executor = Executor()
