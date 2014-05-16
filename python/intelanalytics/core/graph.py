b3cba735c53fef9de83504fef428b24284bc8ec8
Briton Barker <briton.barker@intel.com>
2014-05-16 11:21:06 -0700

    repair some python merge destruction

--- python/intelanalytics/core/graph.py
+++ python/intelanalytics/core/graph.py
@@ -21,6 +21,23 @@
 # must be express and approved by Intel in writing.
 ##############################################################################
 f, f2 = {}, {}
+"""
+BigGraph object
+
+Examples
+--------
+>>> movie_vertex = VertexRule('movie', f['movie'], {'genre': f['genre']})
+
+>>> user_vertex = VertexRule('user', f['user'], {'age': f['age_1']})
+
+>>> rating_edge = EdgeRule('rating', movie_vertex, user_vertex, {'weight':f['score'}])
+
+>>> oscars_vertex_prop = VertexRule('movie', f2['film'], {'oscars': f2['oscars']})
+
+>>> g = BigGraph([user_vertex, movie_vertex, rating_edge, oscars_vertex_prop])
+
+"""
+
 import logging
 logger = logging.getLogger(__name__)
 import uuid
@@ -28,158 +45,6 @@ import uuid
 from intelanalytics.core.serialize import to_json
 from intelanalytics.core.column import BigColumn
 
-class BigGraph(object):
-    """
-    Creates a big graph.
-
-    Parameters
-    ----------
-    rules : list of rules
-        list of rules which specify how the graph will be created
-
-    Examples
-    --------
-    >>> g = BigGraph([user_vertex, movie_vertex, rating_edge, extra_movie_rule])
-    """
-    def __init__(self, rules=None):
-        pass
-
-class Rule(object):
-    """
-    Graph building rule base class.
-    """
-    pass
-
-class VertexRule(Rule):
-    """
-    Specifies creation of a vertex.
-
-    Parameters
-    ----------
-    label : str OR BigColumn
-        vertex label, static string or pulled from BigColumn source
-    value : BigColumn
-        vertex value
-    props : Dictionary
-        vertex properties of the form property_name:property_value
-                              property_name is a string, and property_value is a literal
-                              value or a BigColumn source, which must be from same
-                              BigFrame as value arg
-
-    Examples
-    --------
-    >>> movie_vertex = VertexRule('movie', f['movie'], genre=f['genre'])
-    >>> user_vertex = VertexRule('user', f['user'], age=f['age_1'])
-    >>> g = BigGraph([user_vertex, movie_vertex, rating_edge, oscars_vertex_prop])
-    """
-
-    def __init__(self, label, value, **props):
-        pass
-
-class EdgeRule(Rule):
-    """
-    Specifies creation of an edge.
-
-    Parameters
-    ----------
-    label : str OR BigColumn
-        vertex label, static string or pulled from BigColumn source
-    src : VertexRule
-        source vertex; must be from same BigFrame as dst
-    dst : VertexRule
-        source vertex; must be from same BigFrame as src
-    is_directed : bool
-        indicates the edge is directed
-    props : Dictionary
-        vertex properties of the form property_name:property_value
-                              property_name is a string, and property_value is a literal
-                              value or a BigColumn source, which must be from same
-                              BigFrame as value src, dst
-
-    Examples
-    --------
-    >>> rating_edge = EdgeRule('rating', movie_vertex, user_vertex, weight=f['score'])
-    """
-
-    def __init__(self, label, src, dst, is_directed=False, **props):
-        pass
-
-
-class PropertyRule(Rule):
-    """
-    To do.
-    """
-    pass
-
-class VertexPropertyRule(PropertyRule):
-    """
-    Specifies attachment of additional properties to a vertex.
-
-    Parameters
-    ----------
-    vertex : VertexRule
-        target vertex for property attachment
-    match_value : BigColumn
-        # more appropriate name?
-        BigColumn source whose value must match against the value of the target vertex
-    props : Dictionary
-        vertex properties of the form property_name:property_value
-        property_name is a string, and property_value is a literal
-        value or a BigColumn source
-
-    Examples
-    --------
-    >>> extra_movie_rule = VertexPropertyRule(vertex_movie, f2['movie'], oscars=f2['oscars'])
-    """
-
-    def __init__(self, vertex, match_value, **props):
-        pass
-
-
-class EdgePropertyRule(PropertyRule):
-    """
-    Specifies attachment of additional properties to an edge.
-
-    Parameters
-    ----------
-    edge : EdgeRule
-        target edge for property attachment
-    match_src : BigColumn
-        # more appropriate name?
-        BigColumn source whose value must match against the value
-        of the target edge's source vertex
-    match_dst : BigColumn
-        # more appropriate name?
-        BigColumn source whose value must match against the value
-        of the target edge's destination vertex
-    props : Dictionary (optional)
-        edge properties of the form property_name:property_value
-        property_name is a string, and property_value is a literal
-        value or a BigColumn source
-
-    Examples
-    --------
-    >>> extra_rating_rule = EdgePropertyRule(rating_edge, f2['movie'], f2['user'], rotten_tomatoes=f2['rt'])
-    """
-    def __init__(self, edge, match_src, match_dst, **props):
-        pass
-
-
-class GraphRule(Rule):
-    """
-    Specifies graph properties.
-
-    Parameters
-    ----------
-    vid : str
-        the name of the vertex id  # ie today's _gb_ID
-
-    Examples
-    --------
-    >>> extra_rating_rule = EdgePropertyRule(rating_edge, f2['movie'], f2['user'], rotten_tomatoes=f2['rt'])
-    """
-    def __init__(self, vid=None):
-        pass
 
 def _get_backend():
     from intelanalytics.core.config import get_graph_backend
@@ -189,6 +54,7 @@ def _get_backend():
 def get_graph_names():
     """Gets the names of BigGraph objects available for retrieval"""
     return _get_backend().get_graph_names()
+
 
 def get_graph(name):
     """Retrieves the named BigGraph object"""
@@ -259,7 +125,7 @@ class Rule(object):
 
 class VertexRule(Rule):
     """
-    Specifies a vertex and vertex properties.
+    Specifies a vertex and vertex properties
 
     Parameters
     ----------
@@ -301,7 +167,7 @@ class VertexRule(Rule):
 
 class EdgeRule(Rule):
     """
-    Specifies an edge and edge properties.
+    Specifies an edge and edge properties
 
     Parameters
     ----------
@@ -358,7 +224,7 @@ class EdgeRule(Rule):
 
 class BigGraph(object):
     """
-    Creates a big graph.
+    Creates a big graph
 
     Parameters
     ----------
