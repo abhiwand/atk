c0344033afb0cff003f3236b1a40c3cede0ebb1b
rodorad <rene.o.dorado@intel.com>
2014-05-23 10:24:08 -0700

    TRIB-2722 update the api-server script to look for new jars and add spray version to the application.conf file for api-server

--- bin/api-server.sh
+++ bin/api-server.sh
@@ -1,17 +1,36 @@
 #!/bin/bash
 set -o errexit
-
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 
 echo $DIR
 
-if [[ -f $DIR/../launcher/target/scala-2.10/launcher.jar ]]
+pushd $DIR/..
+jars="engine-spark*-with-dependencies.jar api-server*-with-dependencies.jar engine-0*-with-dependencies.jar interfaces*-with-dependencies.jar "
+
+if [ "$DIR/stage" != "" ]; then
+	rm -rf $DIR/stage
+fi
+
+mkdir -p $DIR/stage/lib
+
+for jar in $jars
+do 
+	jarPath=$(find .  $DIR/../package -path $DIR -prune -o -name $jar -print )
+	echo $jarPath
+	cp -v $jarPath $DIR/stage/lib/
+done
+	jarPath=$(find . -path $DIR/../package -path $DIR -prune -o -name launcher*-with-dependencies.jar -print)
+	echo $jarPath
+	cp -v $jarPath $DIR/stage
+	cp -v $jarPath $DIR/stage/lib/
+
+
+if [[ -f $DIR/stage/launcher-0.8.0-SNAPSHOT-jar-with-dependencies.jar ]]
 then
-    LAUNCHER=$DIR/../launcher/target/scala-2.10/launcher.jar
+    LAUNCHER=$DIR/stage/launcher-0.8.0-SNAPSHOT-jar-with-dependencies.jar
     CONFDIR=$DIR/../api-server/src/main/resources
 else
-    LAUNCHER=$DIR/launcher.jar
-    CONFDIR=$DIR/conf
+    echo "else"
 fi
 
 if [[ -n "$EXTRA_CONF" ]]
