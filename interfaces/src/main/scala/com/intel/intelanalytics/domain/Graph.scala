a679bc964bf2a95bc1b7b6b0dae6879aa6ba42a6
nlsegerl <nathan.l.segerlind@intel.com>
2014-05-27 13:48:01 -0700

    TRIB-2503  GB REST API
    Comment cleanup.

--- interfaces/src/main/scala/com/intel/intelanalytics/domain/Graph.scala
+++ interfaces/src/main/scala/com/intel/intelanalytics/domain/Graph.scala
@@ -3,8 +3,12 @@ package com.intel.intelanalytics.domain
 import com.intel.intelanalytics.domain.graphconstruction.{ EdgeRule, VertexRule, OutputConfiguration }
 
 /**
- * NLS TODO:   what are the required pieces of data in the graph interfaces?
- * so far, we just know that it has to have a name
+ *  TODO:   what are the required pieces of data in the graph interfaces?
+ * so far, we just know that it has to have a name... getting the schema from the graph is a tricky thing,
+ * since the load step allows the data to determine the schema without telling us what it is ... perhaps
+ * the best option would be to set up a "check schema" utility that opens the titan graph, gets its schema
+ * and we store it in the metatable, and then dirty it if we modify the graph
+ *
  * @param name
  */
 
