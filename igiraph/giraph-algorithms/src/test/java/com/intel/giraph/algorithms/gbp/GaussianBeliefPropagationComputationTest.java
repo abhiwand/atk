6f174b7149babaad78b39df6c8f31d14763664cc
Xia Zhu <xia.zhu@intel.com>
2014-05-29 04:15:40 -0700

    TRIB-2507 udpate output format

--- igiraph/giraph-algorithms/src/test/java/com/intel/giraph/algorithms/gbp/GaussianBeliefPropagationComputationTest.java
+++ igiraph/giraph-algorithms/src/test/java/com/intel/giraph/algorithms/gbp/GaussianBeliefPropagationComputationTest.java
@@ -68,15 +68,15 @@ public class GaussianBeliefPropagationComputationTest {
     @Test
     public void testSymmetricCase1() throws Exception {
         int graphSize = 3;
-        HashMap<Long, Double[]> expectedValues = new HashMap<>();
+        HashMap<Long, Double> expectedValues = new HashMap<>();
         String[] graph = new String[] {
             "[0,[10,3],[[1,2],[2,1]]]",
             "[1,[10,4],[[0,2]]]",
             "[2,[16,5],[[0,1]]]"
         };
-        expectedValues.put(0L, new Double[]{ 10.0, 3.0, 1.0});
-        expectedValues.put(1L, new Double[]{ 10.0, 4.0, 2.0});
-        expectedValues.put(2L, new Double[]{ 16.0, 5.0, 3.0});
+        expectedValues.put(0L, 1.0);
+        expectedValues.put(1L, 2.0);
+        expectedValues.put(2L, 3.0);
         conf.set("gbp.convergenceThreshold", "0.005");
         conf.set("gbp.outerLoop", "true");
         runTest(conf, graph, graphSize, expectedValues);
@@ -92,16 +92,16 @@ public class GaussianBeliefPropagationComputationTest {
     @Test
     public void testSymmetricCase2() throws Exception {
         int graphSize = 3;
-        HashMap<Long, Double[]> expectedValues = new HashMap<>();
+        HashMap<Long, Double> expectedValues = new HashMap<>();
 
         String[] graph = new String[] {
             "[0,[-6,1],[[1,-2],[2,3]]]",
             "[1,[0,1],[[0,-2]]]",
             "[2,[2,1],[[0,3]]]"
         };
-        expectedValues.put(0L, new Double[]{-6.0, 1.0,  1.0});
-        expectedValues.put(1L, new Double[]{ 0.0, 1.0,  2.0});
-        expectedValues.put(2L, new Double[]{ 2.0, 1.0, -1.0});
+        expectedValues.put(0L, 1.0);
+        expectedValues.put(1L, 2.0);
+        expectedValues.put(2L, -1.0);
         conf.set("gbp.convergenceThreshold", "0.001");
         conf.set("gbp.outerLoop", "false");
         runTest(conf, graph, graphSize, expectedValues);
@@ -116,15 +116,15 @@ public class GaussianBeliefPropagationComputationTest {
     @Test
     public void testAsymmetricCase1() throws Exception {
         int graphSize = 3;
-        HashMap<Long, Double[]> expectedValues = new HashMap<>();
+        HashMap<Long, Double> expectedValues = new HashMap<>();
         String[] graph = new String[] {
             "[0,[-1,5],[[1,-2,-3],[2,3,2]]]",
             "[1,[2,9],[[0,-3,-2],[2,1,-1]]]",
             "[2,[3,-7],[[0,2,3],[1,-1,1]]]"
         };
-        expectedValues.put(0L, new Double[]{-1.0, 5.0,  0.182});
-        expectedValues.put(1L, new Double[]{ 2.0, 9.0,  0.330});
-        expectedValues.put(2L, new Double[]{ 3.0, -7.0, -0.407});
+        expectedValues.put(0L, 0.182);
+        expectedValues.put(1L, 0.330);
+        expectedValues.put(2L, -0.407);
         conf.set("gbp.convergenceThreshold", "0.007");
         conf.set("gbp.outerLoop", "true");
         runTest(conf, graph, graphSize, expectedValues);
@@ -139,57 +139,44 @@ public class GaussianBeliefPropagationComputationTest {
     @Test
     public void testAsymmetricCase2() throws Exception {
         int graphSize = 2;
-        HashMap<Long, Double[]> expectedValues = new HashMap<>();
+        HashMap<Long, Double> expectedValues = new HashMap<>();
         String[] graph = new String[] {
             "[0,[6,7],[[1,-1,1]]]",
             "[1,[-4,-5],[[0,1,-1]]]"
         };
-        expectedValues.put(0L, new Double[]{6.0, 7.0,  1.0});
-        expectedValues.put(1L, new Double[]{-4.0, -5.0,  1.0});
+        expectedValues.put(0L, 1.0);
+        expectedValues.put(1L, 1.0);
         conf.set("gbp.convergenceThreshold", "0.007");
         conf.set("gbp.outerLoop", "true");
         runTest(conf, graph, graphSize, expectedValues);
     }
 
 
-    public void runTest(GiraphConfiguration conf, String[] graph, int graphSize, HashMap<Long, Double[]> expectedValues) throws Exception {
+    public void runTest(GiraphConfiguration conf, String[] graph, int graphSize, HashMap<Long, Double> expectedValues) throws Exception {
         // run internally
         Iterable<String> results = InternalVertexRunner.run(conf, graph);
-        Map<Long, Double[]> vertexValues = parseVertexValues(results);
+        Map<Long, Double> vertexValues = parseVertexValues(results);
 
         // verify results
         assertNotNull(vertexValues);
         assertEquals(graphSize, vertexValues.size());
-        for (Map.Entry<Long, Double[]> entry : vertexValues.entrySet()) {
-            assertEquals(3, entry.getValue().length);
-            for (int j = 0; j < 3; j++) {
-                assertEquals(expectedValues.get(entry.getKey())[j], entry.getValue()[j], 0.03d);
-            }
+        for (Map.Entry<Long, Double> entry : vertexValues.entrySet()) {
+            assertEquals(expectedValues.get(entry.getKey()), entry.getValue(), 0.03d);
         }
     }
 
-    private Map<Long, Double[]> parseVertexValues(Iterable<String> results) {
-        Map<Long, Double[]> vertexValues = Maps.newHashMapWithExpectedSize(Iterables.size(results));
+    private Map<Long, Double> parseVertexValues(Iterable<String> results) {
+        Map<Long, Double> vertexValues = Maps.newHashMapWithExpectedSize(Iterables.size(results));
         for (String line : results) {
             try {
                 JSONArray jsonVertex = new JSONArray(line);
-                if (jsonVertex.length() != 3) {
+                if (jsonVertex.length() != 2) {
                     throw new IllegalArgumentException("Wrong vertex output format!");
                 }
                 // get id
                 long id = jsonVertex.getLong(0);
-                // get prior
-                JSONArray priorArray = jsonVertex.getJSONArray(1);
-                if (priorArray.length() != 2) {
-                    throw new IllegalArgumentException("Wrong vertex prior output value format!");
-                }
-                Double[] values = new Double[3];
-                for (int i = 0; i < 2; i++) {
-                    values[i] = priorArray.getDouble(i);
-                }
                 // get posterior mean
-                values[2] = jsonVertex.getDouble(2);
-                vertexValues.put(id, values);
+                vertexValues.put(id, jsonVertex.getDouble(1));
                 } catch (JSONException e) {
                     throw new IllegalArgumentException("Couldn't get vertex from line " + line, e);
                 }
