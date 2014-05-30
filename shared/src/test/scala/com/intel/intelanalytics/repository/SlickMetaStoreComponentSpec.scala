665ee82050e18fcf9b08592f6253a538f49557be
Todd Lisonbee <todd.lisonbee@intel.com>
2014-05-30 01:13:43 -0700

    TRIB-2764 adding missing dependencies, more tests

--- shared/src/test/scala/com/intel/intelanalytics/repository/SlickMetaStoreComponentSpec.scala
+++ shared/src/test/scala/com/intel/intelanalytics/repository/SlickMetaStoreComponentSpec.scala
@@ -2,6 +2,7 @@ package com.intel.intelanalytics.repository
 
 import org.scalatest.{Matchers, FlatSpec}
 import scala.slick.driver.H2Driver
+import com.intel.intelanalytics.domain.UserTemplate
 
 class SlickMetaStoreComponentSpec extends FlatSpec with Matchers {
 
@@ -9,14 +10,31 @@ class SlickMetaStoreComponentSpec extends FlatSpec with Matchers {
     override lazy val profile = new Profile(H2Driver, connectionString = "jdbc:h2:mem:iatest;DB_CLOSE_DELAY=-1", driver = "org.h2.Driver")
   }
 
-  "MetaStore" should "be able to initialize tables" in {
-    slickMetaStoreComponent.metaStore.create()
+  // initialize tables
+  slickMetaStoreComponent.metaStore.create()
 
+
+  "MetaStore" should "be able to initialize tables" in {
     val results = slickMetaStoreComponent.database.createConnection().getMetaData.getTables(null, null, null, Array("TABLE"))
     var count = 0
     while (results.next()) {
       count += 1
     }
-    count shouldEqual 3
+    count shouldEqual 4
+  }
+
+  "UserRepository" should "be able to create users" in {
+    val userRepo = slickMetaStoreComponent.metaStore.userRepo
+
+    slickMetaStoreComponent.metaStore.withSession("user-test") {
+      implicit session =>
+        val apiKey = "my-api-key-" + System.currentTimeMillis()
+
+        val user = userRepo.insert(new UserTemplate(apiKey))
+        user.get.api_key shouldBe apiKey
+
+        val user2 = userRepo.lookup(user.get.id)
+        user.get shouldBe user2.get
+    }
   }
 }
