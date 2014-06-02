da687285ccd136c07a030c51e399573d5be8731a
schen55 <steven.shih-chi.chen@intel.com>
2014-06-02 10:40:18 -0700

    move resolveSchemaNamingConflicts to SchemaUtil

--- engine-spark/src/main/scala/com/intel/intelanalytics/engine/spark/SparkEngine.scala
+++ engine-spark/src/main/scala/com/intel/intelanalytics/engine/spark/SparkEngine.scala
@@ -125,40 +125,6 @@ import com.intel.intelanalytics.domain.DataTypes.DataType
 //TODO event notification
 //TODO pass current user info
 
-object SparkEngine {
-
-  /**
-   * Resolve naming conflicts when both left and right side of join operation have same column names
-   * @param leftColumns columns for the left side of join operation
-   * @param rightColumns columns for the right side of join operation
-   * @return
-   */
-  def resolveSchemaNamingConflicts(leftColumns: List[(String, DataType)], rightColumns: List[(String, DataType)]): List[(String, DataType)] = {
-
-    val funcAppendLetterForConflictingNames = (left: List[(String, DataType)], right: List[(String, DataType)], appendLetter: String) => {
-
-      var leftColumnNames = left.map(r => r._1)
-
-      left.map(r =>
-        if (right.map(i => i._1).contains(r._1)) {
-          var name = r._1 + "_" + appendLetter
-          while (leftColumnNames.contains(name)) {
-            name = name + "_" + appendLetter
-          }
-          leftColumnNames = leftColumnNames ++ List(name)
-          (name, r._2)
-        }
-        else
-          r
-      )
-    }
-
-    val left = funcAppendLetterForConflictingNames(leftColumns, rightColumns, "L")
-    val right = funcAppendLetterForConflictingNames(rightColumns, leftColumns, "R")
-
-    left ++ right
-  }
-}
 
 
 class SparkComponent extends EngineComponent
@@ -537,7 +503,7 @@ class SparkComponent extends EngineComponent
                   }
               }
 
-              val allColumns = SparkEngine.resolveSchemaNamingConflicts(originalColumns(0), originalColumns(1))
+              val allColumns = SchemaUtil.resolveSchemaNamingConflicts(originalColumns(0), originalColumns(1))
 
               /* create a dataframe should take very little time, much less than 10 minutes */
               val newJoinFrame = Await.result(create(DataFrameTemplate(joinCommand.name)), 10 minutes)
