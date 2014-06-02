5bc52907585493f90904930ca9f7ff1061302044
schen55 <steven.shih-chi.chen@intel.com>
2014-06-02 10:40:18 -0700

    move resolveSchemaNamingConflicts to SchemaUtil

--- interfaces/src/main/scala/com/intel/intelanalytics/domain/Schema.scala
+++ interfaces/src/main/scala/com/intel/intelanalytics/domain/Schema.scala
@@ -176,3 +176,37 @@ import DataTypes._
 case class Schema(columns: List[(String, DataType)] = List[(String, DataType)]()) {
   require(columns != null)
 }
+
+object SchemaUtil {
+  /**
+   * Resolve naming conflicts when both left and right side of join operation have same column names
+   * @param leftColumns columns for the left side of join operation
+   * @param rightColumns columns for the right side of join operation
+   * @return
+   */
+  def resolveSchemaNamingConflicts(leftColumns: List[(String, DataType)], rightColumns: List[(String, DataType)]): List[(String, DataType)] = {
+
+    val funcAppendLetterForConflictingNames = (left: List[(String, DataType)], right: List[(String, DataType)], appendLetter: String) => {
+
+      var leftColumnNames = left.map(r => r._1)
+
+      left.map(r =>
+        if (right.map(i => i._1).contains(r._1)) {
+          var name = r._1 + "_" + appendLetter
+          while (leftColumnNames.contains(name)) {
+            name = name + "_" + appendLetter
+          }
+          leftColumnNames = leftColumnNames ++ List(name)
+          (name, r._2)
+        }
+        else
+          r
+      )
+    }
+
+    val left = funcAppendLetterForConflictingNames(leftColumns, rightColumns, "L")
+    val right = funcAppendLetterForConflictingNames(rightColumns, leftColumns, "R")
+
+    left ++ right
+  }
+}
