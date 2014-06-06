b86cb850e21955e9b15900dcc5a4de15d6c1f95f
nlsegerl <nathan.l.segerlind@intel.com>
2014-06-06 10:29:28 -0700

    TRIB-2506
    Added graphx dependency to structure for the connected components phases.
    
    GraphX should be here only temporarily until the project structure settles down.

--- graphon/src/main/scala/com/intel/spark/graphon/connectedcomponents/ConnectedComponents.scala
+++ graphon/src/main/scala/com/intel/spark/graphon/connectedcomponents/ConnectedComponents.scala
@@ -1,5 +1,8 @@
 package com.intel.spark.graphon.connectedcomponents
 
+import org.apache.spark._
+import org.apache.spark.graphx._
+
 /**
  * The improved connected components algorithm of Rastogi et al:
  * Finding Connected Components in Map-Reduce in Logarithmic Rounds
