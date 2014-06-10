8f3b07b6f0f7e357367383698fc0b526243bf671
nlsegerl <nathan.l.segerlind@intel.com>
2014-06-10 14:51:42 -0700

    TRIB-2786
    Unit test for the ConnectedComponentsByMessagePassing routine improved.

--- graphon/src/test/scala/com/intel/spark/graphon/connectedcomponents/ConnectedComponentsByMessagePassingTest.scala
+++ graphon/src/test/scala/com/intel/spark/graphon/connectedcomponents/ConnectedComponentsByMessagePassingTest.scala
@@ -9,8 +9,10 @@ class ConnectedComponentsByMessagePassingTest extends FlatSpec with Matchers  wi
   trait ConnectedComponentsTest {
     val vertexList: List[Long] = List(1, 2, 3, 5, 6, 7)
 
+    // to save ourselves some pain we enter the directed edge list and then make it bidirectional with a flatmap
+
     val edgeList: List[(Long, Long)] = List((2.toLong, 6.toLong), (2.toLong,4.toLong), (4.toLong,6.toLong),
-      (3.toLong, 5.toLong), (3.toLong, 7.toLong), (5.toLong,7.toLong))
+      (3.toLong, 5.toLong), (3.toLong, 7.toLong), (5.toLong,7.toLong)).flatMap( {case (x,y) => Set((x,y), (y,x))})
 
 
   }
@@ -26,8 +28,8 @@ class ConnectedComponentsByMessagePassingTest extends FlatSpec with Matchers  wi
     vertexIdToComponentMap(2) shouldEqual  vertexIdToComponentMap(6)
     vertexIdToComponentMap(2) shouldEqual  vertexIdToComponentMap(4)
 
-    vertexIdToComponentMap(3) shouldEqual  vertexIdToComponentMap(5)
-    vertexIdToComponentMap(3) shouldEqual  vertexIdToComponentMap(7)
+    vertexIdToComponentMap(7) shouldEqual  vertexIdToComponentMap(5)
+    vertexIdToComponentMap(7) shouldEqual  vertexIdToComponentMap(3)
 
     vertexIdToComponentMap(2) should not be vertexIdToComponentMap(3)
     vertexIdToComponentMap(3) should not be vertexIdToComponentMap(1)
