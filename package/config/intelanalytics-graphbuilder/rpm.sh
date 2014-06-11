b8c14b3874d586a771431a296782c61e243a54ac
rodorad <rene.o.dorado@intel.com>
2014-06-11 11:18:31 -0700

    add sample spec file and the shell script to create graphbuilder jar

---
+++ package/config/intelanalytics-graphbuilder/rpm.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+source common.sh
+
+log "Build rpm package"
+
+packageName=$1
+tarFile=$2
+TAR_FILE=$tarFile
+version=$3
+
+log "package name: $packageName, tar file: $tarFile, version: $version, script path: $SCRIPTPATH"
+
+log "copy and rename: $tarFile"
+cp $tarFile $SCRIPTPATH/rpm/SOURCES/${packageName}-${version}.tar.gz
+
+LICENSE="Confidential"
+SUMMARY="$packageName-$version Build number: $BUILD_NUMBER. TimeStamp $TIMESTAMP"
+DESCRIPTION=$SUMMARY 
+REQUIRES="java >= 1.7, intelanalytics-spark-deps >= 0.8.${BUILD_NUMBER}-${BUILD_NUMBER}"
+
+POST="
+
+"
+
+#delete the sym link only if we are uninstalling not updating
+POSTUN="
+
+"
+
+rpmSpec > $SCRIPTPATH/rpm/SPECS/$packageName.spec
+
+topDir="$SCRIPTPATH/rpm"
+#exit 1
+pushd $SCRIPTPATH/rpm
+
+log "clean up build dirs"
+rm -rf BUILD/*
+rm -rf BUILDROOT/*
+
+
+log $BUILD_NUMBER
+rpmbuild --define "_topdir $topDir"  --define "BUILD_NUMBER $BUILD_NUMBER" --define "VERSION $VERSION" -bb SPECS/$packageName.spec
+
+cleanRpm
+popd
+
