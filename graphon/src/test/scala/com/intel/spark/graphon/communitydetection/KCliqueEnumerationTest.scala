c838ba9c101c7f4cd3b2701e1ebfc77fc2e06cf2
nlsegerl <nathan.l.segerlind@intel.com>
2014-06-11 11:02:12 -0700

    TRIB-2506 Working on unit tests for KCliqueEnumeration (partial work, not building)

--- graphon/src/test/scala/com/intel/spark/graphon/communitydetection/KCliqueEnumerationTest.scala
+++ graphon/src/test/scala/com/intel/spark/graphon/communitydetection/KCliqueEnumerationTest.scala
@@ -6,6 +6,25 @@ import com.intel.spark.graphon.connectedcomponents.TestingSparkContext
 
 class KCliqueEnumerationTest extends FlatSpec with Matchers with TestingSparkContext {
 
-  // createEdgelistFromParsedAdjList(vertices: RDD[VertexInAdjacencyFormat]): RDD[Edge]
+  trait KCliqueEnumTest {
+    val vertexWithAdjacencyList : List[(Long, Array[Long])]= List((1,Array(2,3,4)), (2,Array(1,3,4)), (3, Array(1,2)), (4,Array(1,2))).map(
+    {case (v, nbrs) => (v.toLong, nbrs.map(_.toLong))})
+
+    val vertexWithAdjacencyRDD = sc.parallelize(vertexWithAdjacencyList)
+
+    val edgeList : List[(Long,Long)] = List((1,2),(1,3),(1,4),(2,1),(2,3),(2,4),(3,1),(3,2),(4,1),(4,2)).map({case (x,y) => (x.toLong, y.toLong)})
+
+    val x : List[Long] = List (0,1,2,3)
+  }
+
+
+  "creating edge list from adjacency list" should "create correct edge list" in new KCliqueEnumTest {
+
+
+    val kCliqueEnumeration = new KCliqueEnumeration(vertexWithAdjacencyRDD,3)
+
+    kCliqueEnumeration.createEdgelistFromParsedAdjList(vertices: RDD[VertexInAdjacencyFormat]): RDD[Edge]
+    0 shouldBe 0
+  }
 
 }
