0170f666ccbec1b92b6fe6d0a23eb3ce0d9cece4
Todd Lisonbee <todd.lisonbee@intel.com>
2014-06-12 06:32:33 -0700

    TRIB-2960 adding scaladoc

--- api-server/src/main/scala/com/intel/intelanalytics/service/UrlParser.scala
+++ api-server/src/main/scala/com/intel/intelanalytics/service/UrlParser.scala
@@ -7,6 +7,10 @@ object UrlParser {
 
   private val frameIdRegex = "/dataframes/(\\d+)".r
 
+  /**
+   * Get the frameId out of a URL in the format "../dataframes/id"
+   * @return unique id
+   */
   def getFrameId(url: String): Option[Long] = {
     val id = frameIdRegex.findFirstMatchIn(url).map(m => m.group(1))
     id.map(s => s.toLong)
@@ -14,6 +18,10 @@ object UrlParser {
 
   private val graphIdRegex = "/graphs/(\\d+)".r
 
+  /**
+   * Get the graphId out of a URL in the format "../graphs/id"
+   * @return unique id
+   */
   def getGraphId(url: String): Option[Long] = {
     val id = graphIdRegex.findFirstMatchIn(url).map(m => m.group(1))
     id.map(s => s.toLong)
