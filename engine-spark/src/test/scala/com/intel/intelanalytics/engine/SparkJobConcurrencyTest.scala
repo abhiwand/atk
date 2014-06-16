d475aa14ebc36ec42505644464e5af607152b37b
schen55 <steven.shih-chi.chen@intel.com>
2014-06-16 14:26:46 -0700

    still call saveAsTextFile but save them under temp folder. delete temp folder after the test

--- engine-spark/src/test/scala/com/intel/intelanalytics/engine/SparkJobConcurrencyTest.scala
+++ engine-spark/src/test/scala/com/intel/intelanalytics/engine/SparkJobConcurrencyTest.scala
@@ -5,11 +5,21 @@ import org.apache.spark.engine.{ProgressPrinter, SparkProgressListener}
 import java.util.concurrent.Semaphore
 import com.intel.intelanalytics.engine.spark.SparkOps
 import com.intel.intelanalytics.engine.TestingSparkContext
+import java.io.File
+import org.apache.commons.io.FileUtils
 
 class SparkJobConcurrencyTest  extends TestingSparkContext with Matchers {
   "Running multiple thread" should "keep isolation between threads when setting properties" in {
     val listener = new SparkProgressListener(null)
     sc.addSparkListener(listener)
+
+    val file = File.createTempFile("test", "-tmp")
+    val path = file.getAbsolutePath
+    file.delete()
+    if (!file.mkdirs()) {
+      throw new RuntimeException("Failed to create tmpDir: " + path)
+    }
+
     val sem = new Semaphore(0)
     val num = 100
     val threads = (1 to num).map {
@@ -19,7 +29,7 @@ class SparkJobConcurrencyTest  extends TestingSparkContext with Matchers {
           val carOwnerShips = List(Array[Any]("Bob", "Mustang,Camry"), Array[Any]("Josh", "Neon,CLK"), Array[Any]("Alice", "PT Cruiser,Avalon,F-150"), Array[Any]("Tim", "Beatle"), Array[Any]("Becky", ""))
           val rdd = sc.parallelize(carOwnerShips)
           val flattened = SparkOps.flattenRddByColumnIndex(1, ",", rdd)
-          flattened.collect()
+          flattened.saveAsTextFile(new File(path, "command-" + i.toString).toString)
           sem.release()
         }
       }
@@ -37,8 +47,9 @@ class SparkJobConcurrencyTest  extends TestingSparkContext with Matchers {
     val distinctIds = commandIds.toSet.toList.sorted
 
     distinctIds.size shouldBe num
-    for(i <- distinctIds)
-      println("######## command" + i)
+//    for(i <- distinctIds)
+//      println("######## command" + i)
+    FileUtils.deleteQuietly(file)
   }
 
 
