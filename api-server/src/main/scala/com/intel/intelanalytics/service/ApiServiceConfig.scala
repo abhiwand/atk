949b1f5cad1b4dc54de8127deb6a17bef53a351d
Todd Lisonbee <todd.lisonbee@intel.com>
2014-06-16 03:52:39 -0700

    TRIB-2960 adding type to signature of public methods

--- api-server/src/main/scala/com/intel/intelanalytics/service/ApiServiceConfig.scala
+++ api-server/src/main/scala/com/intel/intelanalytics/service/ApiServiceConfig.scala
@@ -11,20 +11,20 @@ import scala.concurrent.duration._
 object ApiServiceConfig extends SharedConfig {
 
   /** Host or interface that ApiService should listen on */
-  val host = config.getString("intel.analytics.api.host")
+  val host: String = config.getString("intel.analytics.api.host")
 
   /** Port that ApiService should listen on */
-  val port = config.getInt("intel.analytics.api.port")
+  val port: Int = config.getInt("intel.analytics.api.port")
 
   /** A String describing the service, e.g. "ia" */
-  val identifier = config.getString("intel.analytics.api.identifier")
+  val identifier: String = config.getString("intel.analytics.api.identifier")
 
   /** Default timeout for actors */
   val defaultTimeout: FiniteDuration = config.getInt("intel.analytics.api.defaultTimeout").seconds
 
   /** Default number of items to return in service index when not specified. E.g. GET /v1/commands */
-  val defaultCount = config.getInt("intel.analytics.api.defaultCount")
+  val defaultCount: Int = config.getInt("intel.analytics.api.defaultCount")
 
   /** Input file for creating test users for local development */
-  val testUsersFile = config.getString("intel.analytics.test.users.file")
+  val testUsersFile: String = config.getString("intel.analytics.test.users.file")
 }
