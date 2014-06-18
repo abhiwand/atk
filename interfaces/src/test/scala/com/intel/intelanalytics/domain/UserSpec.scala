1c0ca78a9c6fae2f88c817dfa6aa2d182d9b9b82
Todd Lisonbee <todd.lisonbee@intel.com>
2014-06-18 00:46:31 -0700

    TRIB-2493 testing validation on User

--- interfaces/src/test/scala/com/intel/intelanalytics/domain/UserSpec.scala
+++ interfaces/src/test/scala/com/intel/intelanalytics/domain/UserSpec.scala
@@ -8,4 +8,16 @@ class UserSpec extends FlatSpec {
   "User" should "be able to have a none apiKey" in {
     new User(1L, None, None, new DateTime, new DateTime)
   }
+
+  it should "not be able to have an empty string apiKey" in {
+    try {
+      new User(1L, None, Some(""), new DateTime, new DateTime)
+
+      fail("The expected IllegalArgumentException was not thrown")
+    }
+    catch {
+      case e: IllegalArgumentException => // expected, test passes
+    }
+
+  }
 }
