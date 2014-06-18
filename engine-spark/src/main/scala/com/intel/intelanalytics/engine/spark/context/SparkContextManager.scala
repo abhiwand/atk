daf21e60e0cfb3818da845019ad8b61d2fd3b476
Todd Lisonbee <todd.lisonbee@intel.com>
2014-06-18 00:49:04 -0700

    TRIB-2493 giving nice exception message if an authenticated user doesn't have an API key

--- engine-spark/src/main/scala/com/intel/intelanalytics/engine/spark/context/SparkContextManager.scala
+++ engine-spark/src/main/scala/com/intel/intelanalytics/engine/spark/context/SparkContextManager.scala
@@ -10,7 +10,8 @@ class SparkContextManager(conf: Config, factory: SparkContextFactory) extends Sp
   contextManagementStrategy.sparkContextFactory = factory
 
   def getContext(user: String): Context = contextManagementStrategy.getContext(user)
-  def context(implicit user: UserPrincipal): Context = getContext(user.user.apiKey.get)
+  def context(implicit user: UserPrincipal): Context = getContext(user.user.apiKey.getOrElse(
+    throw new RuntimeException("User didn't have an apiKey which shouldn't be possible if they were authenticated")))
   def cleanup(): Unit = contextManagementStrategy.cleanup()
   def removeContext(user: String): Unit = contextManagementStrategy.removeContext(user)
   def getAllContexts(): List[Context] = contextManagementStrategy.getAllContexts()
