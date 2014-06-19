feaaef768110050f148169f593e69abe2986705a
Kushal Datta <kushal.datta@intel.com>
2014-06-19 17:28:08 -0400

    TRIB-2731: After merge from sprint_14 to this branch on 6/19/14

--- api-server/src/main/scala/com/intel/intelanalytics/service/ApiService.scala
+++ api-server/src/main/scala/com/intel/intelanalytics/service/ApiService.scala
@@ -1,35 +1,61 @@
+//////////////////////////////////////////////////////////////////////////////
+// INTEL CONFIDENTIAL
+//
+// Copyright 2014 Intel Corporation All Rights Reserved.
+//
+// The source code contained or described herein and all documents related to
+// the source code (Material) are owned by Intel Corporation or its suppliers
+// or licensors. Title to the Material remains with Intel Corporation or its
+// suppliers and licensors. The Material may contain trade secrets and
+// proprietary and confidential information of Intel Corporation and its
+// suppliers and licensors, and is protected by worldwide copyright and trade
+// secret laws and treaty provisions. No part of the Material may be used,
+// copied, reproduced, modified, published, uploaded, posted, transmitted,
+// distributed, or disclosed in any way without Intel's prior express written
+// permission.
+//
+// No license under any patent, copyright, trade secret or other intellectual
+// property right is granted to or conferred upon you by disclosure or
+// delivery of the Materials, either expressly, by implication, inducement,
+// estoppel or otherwise. Any license under such intellectual property rights
+// must be express and approved by Intel in writing.
+//////////////////////////////////////////////////////////////////////////////
+
 package com.intel.intelanalytics.service
 
 import akka.actor.Actor
-import spray.routing._
-import spray.http._
-import MediaTypes._
 import akka.event.Logging
-import spray.routing.directives.BasicDirectives
-import com.intel.event.{ Severity, EventContext }
-import com.intel.intelanalytics.shared.EventLogging
 import com.intel.intelanalytics.service.v1.ApiV1Service
-import com.intel.intelanalytics.service.v1.viewmodels.RelLink
-import com.typesafe.config.ConfigFactory
-import com.intel.intelanalytics.domain.schema.Schema
+import spray.http.MediaTypes._
+import spray.routing._
 
-// we don't implement our route structure directly in the service actor because
-// we want to be able to test it independently, without having to spin up an actor
-class ApiServiceActor extends Actor with HttpService { this: ApiService =>
+/**
+ * We don't implement our route structure directly in the service actor because
+ * we want to be able to test it independently, without having to spin up an actor
+ *
+ * @param apiService the service to delegate to
+ */
+class ApiServiceActor(val apiService: ApiService) extends Actor with HttpService {
 
-  // the HttpService trait defines only one abstract member, which
-  // connects the services environment to the enclosing actor or test
+  /**
+   * the HttpService trait defines only one abstract member, which
+   * connects the services environment to the enclosing actor or test
+   */
   override def actorRefFactory = context
 
-  // this actor only runs our route, but you could add
-  // other things here, like request stream processing
-  // or timeout handling
-  def receive = runRoute(this.serviceRoute)
+  /**
+   * Delegates to apiService.
+   *
+   * This actor only runs our route, but you could add other things here, like
+   * request stream processing or timeout handling
+   */
+  def receive = runRoute(apiService.serviceRoute)
 }
 
-// this trait defines our service behavior independently from the service actor
-trait ApiService extends Directives
-    with EventLoggingDirectives { this: ApiService with ApiV1Service =>
+/**
+ * Defines our service behavior independently from the service actor
+ */
+class ApiService(val apiV1Service: ApiV1Service) extends Directives {
 
   def homepage = {
     respondWithMediaType(`text/html`) {
@@ -43,25 +69,24 @@ trait ApiService extends Directives
     }
   }
 
-  private val config = ConfigFactory.load()
-
   lazy val description = {
     new ServiceDescription(name = "Intel Analytics",
-      identifier = config.getString("intel.analytics.api.identifier"),
+      identifier = ApiServiceConfig.identifier,
       versions = List("v1"))
   }
 
-  import spray.json._
-  import spray.httpx.SprayJsonSupport._
-  import DefaultJsonProtocol._
+  import spray.json.DefaultJsonProtocol._
   implicit val descFormat = jsonFormat3(ServiceDescription)
 
+  /**
+   * Main Route entry point to the API Server
+   */
   val serviceRoute: Route = logRequest("api service", Logging.InfoLevel) {
     path("") {
       get { homepage }
     } ~
       pathPrefix("v1") {
-        this.apiV1Service
+        apiV1Service.route
       } ~
       path("info") {
         respondWithMediaType(`application/json`) {
@@ -73,14 +98,4 @@ trait ApiService extends Directives
 
 case class ServiceDescription(name: String, identifier: String, versions: List[String])
 
-trait EventLoggingDirectives extends EventLogging {
-  import BasicDirectives._
-  def eventContext(context: String): Directive0 =
-    mapRequestContext { ctx ⇒
-      withContext(context) {
-        ctx.withRouteResponseMapped {
-          response ⇒ response
-        }
-      }
-    }
-}
+
