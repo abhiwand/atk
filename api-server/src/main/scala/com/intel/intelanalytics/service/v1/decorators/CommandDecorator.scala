ec12995907595be0f992e4a92cf56abc633b4374
Kushal Datta <kushal.datta@intel.com>
2014-06-19 17:25:46 -0400

    TRIB-2731: After merge from sprint_14 to this branch on 6/19/14

---
+++ api-server/src/main/scala/com/intel/intelanalytics/service/v1/decorators/CommandDecorator.scala
@@ -0,0 +1,60 @@
+//////////////////////////////////////////////////////////////////////////////
+// INTEL CONFIDENTIAL
+//
+// Copyright 2014 Intel Corporation All Rights Reserved.
+//
+// The source code contained or described herein and all documents related to
+// the source code (Material) are owned by Intel Corporation or its suppliers
+// or licensors. Title to the Material remains with Intel Corporation or its
+// suppliers and licensors. The Material may contain trade secrets and
+// proprietary and confidential information of Intel Corporation and its
+// suppliers and licensors, and is protected by worldwide copyright and trade
+// secret laws and treaty provisions. No part of the Material may be used,
+// copied, reproduced, modified, published, uploaded, posted, transmitted,
+// distributed, or disclosed in any way without Intel's prior express written
+// permission.
+//
+// No license under any patent, copyright, trade secret or other intellectual
+// property right is granted to or conferred upon you by disclosure or
+// delivery of the Materials, either expressly, by implication, inducement,
+// estoppel or otherwise. Any license under such intellectual property rights
+// must be express and approved by Intel in writing.
+//////////////////////////////////////////////////////////////////////////////
+package com.intel.intelanalytics.service.v1.decorators
+
+import com.intel.intelanalytics.domain.command.Command
+import com.intel.intelanalytics.service.v1.viewmodels.{ RelLink, GetCommand, GetCommands }
+
+/**
+ * A decorator that takes an entity from the database and converts it to a View/Model
+ * for delivering via REST services
+ */
+object CommandDecorator extends EntityDecorator[Command, GetCommands, GetCommand] {
+
+  /**
+   * Decorate a single entity (like you would want in "GET /entities/id")
+   *
+   * @param uri UNUSED? DELETE?
+   * @param links related links
+   * @param entity the entity to decorate
+   * @return the View/Model
+   */
+  override def decorateEntity(uri: String, links: Iterable[RelLink], entity: Command): GetCommand = {
+    GetCommand(id = entity.id, name = entity.name,
+      arguments = entity.arguments, error = entity.error, complete = entity.complete,
+      result = entity.result, links = links.toList)
+  }
+
+  /**
+   * Decorate a list of entities (like you would want in "GET /entities")
+   *
+   * @param uri the base URI, for this type of entity "../entities"
+   * @param entities the list of entities to decorate
+   * @return the View/Model
+   */
+  override def decorateForIndex(uri: String, entities: Seq[Command]): List[GetCommands] = {
+    entities.map(frame => new GetCommands(id = frame.id,
+      name = frame.name,
+      url = uri + "/" + frame.id)).toList
+  }
+}
