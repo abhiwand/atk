e86d0c37740cfdf744c1866190ef71439d29f7bc
Andrew Wicker <andrew.w.wicker@intel.com>
2014-06-23 18:18:12 -0700

    added some comments

--- python/binning_examples.py
+++ python/binning_examples.py
@@ -2,29 +2,34 @@ from intelanalytics import *
 
 #loggers.set_http()
 
-csv_file = CsvFile("datasets/test.csv", [('colA', int32), ('colB', int32), ('colC', str)])
+input_file = "datasets/netflix/movie_data_1mb_noheader.csv"
+csv_file = CsvFile(input_file, [('src', int64),
+                                ('vertex_type', str),
+                                ('dest', int64),
+                                ('weight', int32),
+                                ('edge_type', str)])
 frame = BigFrame(csv_file)
 print(frame.inspect())
 
-width_frame = frame.bin_column('colB', 5, bin_type='equalwidth', bin_column_name='EWBinned')
-print(width_frame.inspect())
+width_frame = frame.bin_column('weight', 6, bin_type='equalwidth', bin_column_name='EWBinned')
+print(width_frame.inspect(20))
 
-width_frame2 = frame.bin_column('colB', 3, bin_type='equalwidth', bin_column_name='EWBinned')
-print(width_frame2.inspect())
-
-width_frame3 = frame.bin_column('colA', 2, bin_type='equalwidth', bin_column_name='EWBinned')
-print(width_frame3.inspect())
-
-depth_frame = frame.bin_column('colB', 5, bin_type='equaldepth', bin_column_name='EDBinned')
-print(depth_frame.inspect())
-
-depth_frame2 = frame.bin_column('colB', 3, bin_type='equaldepth', bin_column_name='EDBinned')
-print(depth_frame2.inspect())
-
-# note that identical values are not split across bins
-depth_frame3 = frame.bin_column('colA', 2, bin_type='equaldepth', bin_column_name='EDBinned')
-print(depth_frame3.inspect())
+# width_frame2 = frame.bin_column('src', 3, bin_type='equalwidth', bin_column_name='EWBinned')
+# print(width_frame2.inspect())
+#
+# width_frame3 = frame.bin_column('src', 2, bin_type='equalwidth', bin_column_name='EWBinned')
+# print(width_frame3.inspect())
+#
+# depth_frame = frame.bin_column('src', 5, bin_type='equaldepth', bin_column_name='EDBinned')
+# print(depth_frame.inspect())
+#
+# depth_frame2 = frame.bin_column('src', 3, bin_type='equaldepth', bin_column_name='EDBinned')
+# print(depth_frame2.inspect())
+#
+# # note that identical values are not split across bins
+# depth_frame3 = frame.bin_column('src', 2, bin_type='equaldepth', bin_column_name='EDBinned')
+# print(depth_frame3.inspect())
 
 # should fail in bin_column: unable to bin non-numeric values
-#bad_frame = frame.bin_column('colC', 2, bin_type='equalwidth', bin_column_name='EWBinned')
+#bad_frame = frame.bin_column('vertex_type', 2, bin_type='equalwidth', bin_column_name='EWBinned')
 #print(bad_frame.inspect())
