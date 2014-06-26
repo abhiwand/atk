8d17726e5086bc2b07b94a85c7ff06d9338659fa
Andrew Wicker <andrew.w.wicker@intel.com>
2014-06-26 15:33:01 -0700

    cutoff check

--- python/binning_examples.py
+++ python/binning_examples.py
@@ -2,8 +2,8 @@ from intelanalytics import *
 
 #loggers.set('DEBUG', 'intelanalytics.rest.connection','/home/hadoop/logOutput','True')
 
-#input_file = "datasets/netflix/netflix_2million.csv"
-input_file = "datasets/netflix/movie_data_1mb_noheader.csv"
+input_file = "datasets/netflix/netflix_2million.csv"
+#input_file = "datasets/netflix/movie_data_1mb_noheader.csv"
 #input_file = "datasets/testdata.csv"
 #csv_file = CsvFile(input_file, [('src', str), ('weight', int32)])
 csv_file = CsvFile(input_file, [('src', int64),
