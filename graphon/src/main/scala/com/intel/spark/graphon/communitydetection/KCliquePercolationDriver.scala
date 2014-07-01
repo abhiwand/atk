881b61d99148a5c5fbfed7efeb2404cb182d384e
Amit Bhattacharjee <amit.bhattacharjee@intel.com>
2014-07-01 01:39:00 -0500

    Updated KCliquePercolationDataTypes, KCliquePercolatoinDriver, CreateGraphFromEnumeratedKCliques. Added GetConnectedComponents (to run connected components) and AssignCommunitiesToVertex (to map vertex and community id list)

--- graphon/src/main/scala/com/intel/spark/graphon/communitydetection/KCliquePercolationDriver.scala
+++ graphon/src/main/scala/com/intel/spark/graphon/communitydetection/KCliquePercolationDriver.scala
@@ -5,16 +5,33 @@ import org.apache.spark.rdd.RDD
 import com.intel.graphbuilder.elements.{ Edge => GBEdge, GraphElement }
 import com.intel.graphbuilder.driver.spark.rdd.GraphBuilderRDDImplicits._
 import com.intel.spark.graphon.communitydetection.KCliquePercolationDataTypes._
+import com.intel.spark.graphon.communitydetection.CreateGraphFromEnumeratedKCliques.KCliqueGraphOutput
 
+/**
+ * The driver for running the k-clique percolation algorithm
+ */
 object KCliquePercolationDriver {
 
+  /**
+   * Convert Graph Builder edge to the undirected edge as pair of vertex identifiers
+   * @param gbEdgeList Graph Builder edge list
+   * @return an RDD of Edge set
+   */
   def edgeListFromGBEdgeList(gbEdgeList: RDD[GBEdge]): RDD[Edge] = {
 
     gbEdgeList.filter(e => (e.tailPhysicalId.asInstanceOf[Long] < e.headPhysicalId.asInstanceOf[Long])).
-      map(e => (KCliquePercolationDataTypes.Edge(e.tailPhysicalId.asInstanceOf[Long], e.headPhysicalId.asInstanceOf[Long])))
+      map(e => KCliquePercolationDataTypes.Edge(e.tailPhysicalId.asInstanceOf[Long], e.headPhysicalId.asInstanceOf[Long]))
 
   }
 
+  /**
+   * The main driver to execute k-clique percolation algorithm
+   * @param graphTableName This should be reference to the graph from which we are pulling data
+   * @param titanStorageHostName
+   * @param sparkMaster
+   * @param k Parameter determining clique-size used to determine communities. Must be at least 1. Large values of k
+   *          result in fewer, smaller communities that are more connected
+   */
   def run(graphTableName: String, titanStorageHostName: String, sparkMaster: String, k: Int) = {
 
     /**
@@ -29,7 +46,7 @@ object KCliquePercolationDriver {
     val gbEdgeList = graphElements.filterEdges()
 
     /**
-     * Convert the graph builder edge list to the edge list that can will used in KClique Percolation
+     * Convert the graph builder edge list to the edge list that can be used in KClique Percolation
      */
     val edgeList: RDD[Edge] = edgeListFromGBEdgeList(gbEdgeList)
 
@@ -41,17 +58,17 @@ object KCliquePercolationDriver {
     /**
      * Construct the clique graph that will be input for connected components
      */
-    val kcliqueGraphForComponentAnalysis = CreateGraphFromEnumeratedKCliques.run(enumeratedKCliques)
+    val kcliqueGraphForComponentAnalysis: KCliqueGraphOutput = CreateGraphFromEnumeratedKCliques.run(enumeratedKCliques)
 
     /**
-     * TODO: Call connected component analysis to get the communities
+     *  Run connected component analysis to get the communities
      */
-    //val cliquesAndConnectedComponent = GetConnectedComponentsFrom(kcliqueGraphForComponentAnalysis)
+    val cliquesAndConnectedComponent = GetConnectedComponents.run(kcliqueGraphForComponentAnalysis)
 
     /**
-     * TODO: Associate each vertex with a list of the communities to which it belongs
+     * Associate each vertex with a list of the communities to which it belongs
      */
-    //val vertexAndCommunities = AssignCommunitiesToVertexUsing(cliquesAndConnectedComponent)
+    val vertexAndCommunityList: RDD[(Long, Set[Long])] = AssignCommunitiesToVertex.run(cliquesAndConnectedComponent)
 
     /**
      * TODO: Update the properties for each vertex by adding community property to it
