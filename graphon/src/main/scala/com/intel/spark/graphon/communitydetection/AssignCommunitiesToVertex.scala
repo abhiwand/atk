d0428ac3d99391873981bdf734ab8f5faae307d2
Amit Bhattacharjee <amit.bhattacharjee@intel.com>
2014-07-01 18:36:09 -0500

    Updated AssignCommunitiesToVertex, KCliquePercolationDriver. Added test scripts - KCliqueEnumerationTest, CreateGraphFromEnumeratedKCliquesTest, GetConnectedComponentsTest and AssignCommunitiesToVertexTest

--- graphon/src/main/scala/com/intel/spark/graphon/communitydetection/AssignCommunitiesToVertex.scala
+++ graphon/src/main/scala/com/intel/spark/graphon/communitydetection/AssignCommunitiesToVertex.scala
@@ -15,16 +15,14 @@ object AssignCommunitiesToVertex {
   /**
    * Emit pairs of vertex ID and community ID from k-clique graph vertex (having a new Long ID) which is originally a set of vertices.
    * Group by vertex IDs so that each vertex ID gets a list (possibly empty) of the community IDs to which it belongs
-   * @param cliquesAndConnectedComponent connectedComponentsOutput that has mapping between new Long IDs (corresponding to each k-cliques)
-   *                                     and community ID, and mpping between new Long IDs and corresponding k-cliques
+   *
+   * @param connectedComponents pair of new Long IDs (corresponding to each k-cliques) and community ID
+   * @param cliqueGraphNewIDsToVerticesList pair of new Long IDs and corresponding k-cliques
    * @return an RDD of pair of original vertex ID and list of community IDs to which it belongs
    */
-  def run(cliquesAndConnectedComponent: connectedComponentsOutput): RDD[(Long, Set[Long])] = {
+  def run(connectedComponents: RDD[(Long, Long)], cliqueGraphNewIDsToVerticesList: RDD[(Long, VertexSet)]): RDD[(Long, Set[Long])] = {
 
-    val derivedConnectedComponents: RDD[(Long, Long)] = cliquesAndConnectedComponent.connectedComponents
-    val derivedCliqueGraphNewIDsToVerticesList: RDD[(Long, VertexSet)] = cliquesAndConnectedComponent.cliqueGraphNewIDsToVerticesList
-
-    val communityCoGroupedWithVerticesList: RDD[(Seq[Long], Seq[Set[Long]])] = derivedConnectedComponents.cogroup(derivedCliqueGraphNewIDsToVerticesList).map(_._2)
+    val communityCoGroupedWithVerticesList: RDD[(Seq[Long], Seq[Set[Long]])] = connectedComponents.cogroup(cliqueGraphNewIDsToVerticesList).map(_._2)
     val communityVerticesList: RDD[(Long, Set[Long])] = communityCoGroupedWithVerticesList.flatMap({ case (communityIdList, verticesList) => communityIdList.flatMap(communityID => verticesList.map(vertices => (communityID, vertices))) })
     val communityVertex: RDD[VertexCommunity] = communityVerticesList.flatMap(communityVertices => communityVertices._2.map(vertex => VertexCommunity(vertex, communityVertices._1)))
 
--- graphon/src/test/scala/com/intel/spark/graphon/communitydetection/CreateGraphFromEnumeratedKCliquesTest.scala
+++ graphon/src/test/scala/com/intel/spark/graphon/communitydetection/CreateGraphFromEnumeratedKCliquesTest.scala
@@ -2,42 +2,46 @@ package com.intel.spark.graphon.communitydetection
 
 import org.scalatest.{ Matchers, FlatSpec, FunSuite }
 import com.intel.spark.graphon.connectedcomponents.TestingSparkContext
-import org.apache.spark.SparkContext._
-import org.apache.spark.rdd.RDD
 import com.intel.spark.graphon.communitydetection.KCliquePercolationDataTypes._
 
 class CreateGraphFromEnumeratedKCliquesTest extends FlatSpec with Matchers with TestingSparkContext {
 
   trait KCliqueGraphTest {
+    val fourCliques = List((Array(2, 3, 4), Array(5, 7, 8)), (Array(3, 5, 6), Array(7, 8)))
+      .map({ case (cliques, extenders) => (cliques.map(_.toLong).toSet, extenders.map(_.toLong).toSet) })
 
-    val fourCliques = List((Array(2, 3, 4), Array(5, 7, 8)), (Array(3, 5, 6), Array(7, 8))).map({ case (cliques, extenders) => (cliques.map(_.toLong).toSet, extenders.map(_.toLong).toSet) })
-    val fourCliquesRDD = sc.parallelize(fourCliques).map({ case (x, y) => (CliqueFact(x), y, true) })
+    val vertexListOfFourCliqueGraph = List(Array(2, 3, 4, 5), Array(2, 3, 4, 7), Array(2, 3, 4, 8), Array(3, 5, 6, 7), Array(3, 5, 6, 8))
+      .map(clique => clique.map(_.toLong).toSet)
 
-    val fourCliqueGraph = List((Array(2, 3, 4), Array(Array(2, 3, 4, 5), Array(2, 3, 4, 7), Array(2, 3, 4, 8))), (Array(3, 5, 6), Array(Array(3, 5, 6, 7), Array(3, 5, 6, 8))))
-      .map({ case (clique, cliqueSet) => (clique.map(_.toLong).toSet, cliqueSet.map(_.map(_.toLong).toSet).toSet) })
-    val fourCliqueGraphRDD = sc.parallelize(fourCliqueGraph).map({ case (clique, cliqueSet) => (CliqueFact(clique), cliqueSet.map(x => CliqueFact(x))) })
-
-    val fourCliqueEdgeList = List(Array(Array(2, 3, 4, 5), Array(2, 3, 4, 7)), Array(Array(2, 3, 4, 5), Array(2, 3, 4, 8)), Array(Array(2, 3, 4, 7), Array(2, 3, 4, 8)), Array(Array(3, 5, 6, 7), Array(3, 5, 6, 8))).map(_.map(_.map(_.toLong).toSet).toSet)
-    val fourCliqueEdgeListRDD = sc.parallelize(fourCliqueEdgeList).map(_.map(x => CliqueFact(x)))
+    val edgeListOfFourCliqueGraph = List(
+      Array(Array(2, 3, 4, 5), Array(2, 3, 4, 7)),
+      Array(Array(2, 3, 4, 5), Array(2, 3, 4, 8)),
+      Array(Array(2, 3, 4, 7), Array(2, 3, 4, 8)),
+      Array(Array(3, 5, 6, 7), Array(3, 5, 6, 8))
+    ).map(_.map(_.map(_.toLong).toSet).toSet)
   }
 
-  /*
-  "K-Clique graph" should "create correct pairs of Clique and K-cliques extending it" in new KCliqueGraphTest {
+  "K-Clique graph" should
+    "have each k-cliques as the vertex of the new graph " in new KCliqueGraphTest {
 
-    val cliqueAndExtendedCliqueSet = CreateGraphFromEnumeratedKCliques.createCliqueAndExtendedCliqueSet(fourCliquesRDD)
+      val rddOfFourCliques = sc.parallelize(fourCliques).map({ case (x, y) => ExtendersFact(CliqueFact(x), y, true) })
+      val rddOfVertexListOfFourCliqueGraph = sc.parallelize(vertexListOfFourCliqueGraph).map(vertexSet => CliqueFact(vertexSet))
 
-    cliqueAndExtendedCliqueSet shouldEqual fourCliqueGraphRDD
+      val fourCliqueGraphFromCreateGraphOutput = CreateGraphFromEnumeratedKCliques.run(rddOfFourCliques)
+      val vertexListFromCreateGraphOutput = fourCliqueGraphFromCreateGraphOutput.cliqueFactVertexList
 
-  }
-  */
+      vertexListFromCreateGraphOutput.collect().toSet shouldEqual rddOfVertexListOfFourCliqueGraph.collect().toSet
+    }
 
-  /*
-  "K-Clique graph" should "create correct pairs of Clique edges" in new KCliqueGraphTest {
+  "K-Clique graph" should
+    "produce correct edge list where edges between two k-cliques (which is the vertices of new graph) exists if they share (k-1) elements" in new KCliqueGraphTest {
 
-    val cliqueEdgeList = CreateGraphFromEnumeratedKCliques.run(fourCliquesRDD)
+      val rddOfFourCliques = sc.parallelize(fourCliques).map({ case (x, y) => ExtendersFact(CliqueFact(x), y, true) })
+      val rddOfEdgeListOfFourCliqueGraph = sc.parallelize(edgeListOfFourCliqueGraph).map(subset => (CliqueFact(subset.head), CliqueFact(subset.last)))
 
-    cliqueEdgeList shouldEqual fourCliqueEdgeListRDD
+      val fourCliqueGraphFromCreateGraphOutput = CreateGraphFromEnumeratedKCliques.run(rddOfFourCliques)
+      val edgeListFromCreateGraphOutput = fourCliqueGraphFromCreateGraphOutput.cliqueFactEdgeList
 
-  }
-  */
+      edgeListFromCreateGraphOutput.collect().toSet shouldEqual rddOfEdgeListOfFourCliqueGraph.collect().toSet
+    }
 }
