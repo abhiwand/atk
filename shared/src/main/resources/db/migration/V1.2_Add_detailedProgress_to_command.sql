5d10b8e95726fb8e9621f78f0f70cd65a6d8ba30
schen55 <steven.shih-chi.chen@intel.com>
2014-07-02 15:01:50 -0700

    add script to add detailedProgress column

---
+++ shared/src/main/resources/db/migration/V1.2_Add_detailedProgress_to_command.sql
@@ -0,0 +1,5 @@
+/**
+ * Add the column detailedProgress to command table
+ * This column is used to display extra details of command progress
+ */
+ALTER TABLE command ADD COLUMN detailedProgress text;
