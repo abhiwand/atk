702c243d9749fd9564ed2d971a6687f6eda7bb61
schen55 <steven.shih-chi.chen@intel.com>
2014-07-03 11:04:59 -0700

    set not null and default value

--- shared/src/main/resources/db/migration/V1.2_Add_detailedProgress_to_command.sql
+++ shared/src/main/resources/db/migration/V1.2_Add_detailedProgress_to_command.sql
@@ -2,4 +2,4 @@
  * Add the column detailedProgress to command table
  * This column is used to display extra details of command progress
  */
-ALTER TABLE command ADD COLUMN detailedProgress text;
+ALTER TABLE command ADD COLUMN detailedProgress text NOT NULL DEFAULT '[]';
