8b0ade63b495acb349d92ee8605a081a3021f702
Andrew Wicker <andrew.w.wicker@intel.com>
2014-07-09 10:44:55 -0700

    examples

--- python/sampling_examples.py
+++ python/sampling_examples.py
@@ -7,8 +7,8 @@ frame = BigFrame(csv_file)
 print(frame.inspect())
 
 # vertex rules
-src_vertex_rule = VertexRule('userId', frame.colA)  # ignore any properties for now
-dest_vertex_rule = VertexRule('userId', frame.colB)
+src_vertex_rule = VertexRule('userId', frame['colA'])  # ignore any properties for now
+dest_vertex_rule = VertexRule('userId', frame['colB'])
 
 # edge rule
 edge_rule = EdgeRule('knows', src_vertex_rule, dest_vertex_rule)
