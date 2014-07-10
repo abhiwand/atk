57a9d447a89f4494876afab1c92cb5da88467058
nlsegerl <nathan.l.segerlind@intel.com>
2014-07-10 11:36:47 -0700

    TRIB-2245
    Working on the statistics module. It builds, but it's not really hooked up to the python layer.

--- engine-spark/src/main/scala/com/intel/intelanalytics/engine/spark/DiscreteStatistics.scala
+++ engine-spark/src/main/scala/com/intel/intelanalytics/engine/spark/DiscreteStatistics.scala
@@ -4,11 +4,11 @@ import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.rdd.RDD
 
-class DiscreteStatistics[T](data : RDD[T]) {
+class DiscreteStatistics[T](data: RDD[T]) {
 
-  private lazy val mode : T = generateMode()
+  private lazy val mode: T = generateMode()
 
-  private def generateMode(input: RDD[T]): T = {
+  private def generateMode(): T = {
 
     // the countByValue operation creates (value, count) pairs out the RDD
     // hence the mode is the first component of the component with maximum second component
@@ -16,6 +16,6 @@ class DiscreteStatistics[T](data : RDD[T]) {
     def takeValueWithMaximumCount(p1: (T, Long), p2: (T, Long)): (T, Long) = {
       if (p1._2 > p2._2) p1 else p2
     }
-    input.countByValue().reduce(takeValueWithMaximumCount)._1
+    data.countByValue().reduce(takeValueWithMaximumCount)._1
   }
 }
