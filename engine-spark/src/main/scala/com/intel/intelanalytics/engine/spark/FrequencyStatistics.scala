323ff448c55659ef967b9ea513c3ea81a5b3d280
nlsegerl <nathan.l.segerlind@intel.com>
2014-07-14 11:15:23 -0700

    TRIB-2245  Stats module work... mode is passing unit tests.

--- engine-spark/src/main/scala/com/intel/intelanalytics/engine/spark/FrequencyStatistics.scala
+++ engine-spark/src/main/scala/com/intel/intelanalytics/engine/spark/FrequencyStatistics.scala
@@ -2,20 +2,20 @@ package com.intel.intelanalytics.engine.spark
 
 import org.apache.spark.{ AccumulatorParam, Accumulator, SparkContext }
 import org.apache.spark.SparkContext._
-import org.apache.spark.rdd.RDD
+import org.apache.spark.rdd._
 
 case class FrequencyStatsCounter[T](mode: T, weightOfMode: Double, totalWeight: Double) extends Serializable
 
-class frequencyStatsAccumulatorParam[T](nonValue: T) extends AccumulatorParam[FrequencyStatsCounter] with Serializable {
+class frequencyStatsAccumulatorParam[T](nonValue: T) extends AccumulatorParam[FrequencyStatsCounter[T]] with Serializable {
 
-  override def zero(initialValue: FrequencyStatsCounter) = FrequencyStatsCounter(nonValue, -1, 0)
+  override def zero(initialValue: FrequencyStatsCounter[T]) = FrequencyStatsCounter[T](nonValue, -1, 0)
 
-  override def addInPlace(r1: FrequencyStatsCounter, r2: FrequencyStatsCounter): FrequencyStatsCounter = {
+  override def addInPlace(r1: FrequencyStatsCounter[T], r2: FrequencyStatsCounter[T]): FrequencyStatsCounter[T] = {
     if (r1.weightOfMode > r2.weightOfMode) {
-      FrequencyStatsCounter(r1.mode, r1.weightOfMode, r1.totalWeight + r2.totalWeight)
+      FrequencyStatsCounter[T](r1.mode, r1.weightOfMode, r1.totalWeight + r2.totalWeight)
     }
     else {
-      FrequencyStatsCounter(r2.mode, r2.weightOfMode, r1.totalWeight + r2.totalWeight)
+      FrequencyStatsCounter[T](r2.mode, r2.weightOfMode, r1.totalWeight + r2.totalWeight)
     }
   }
 }
@@ -27,25 +27,23 @@ class FrequencyStatistics[T: ClassManifest](dataWeightPairs: RDD[(T, Double)], n
   private def generateMode(): (T, Double) = {
 
     val acumulatorParam = new frequencyStatsAccumulatorParam[T](nonValue)
-    val initialValue = FrequencyStatsCounter(nonValue, -1, 0)
+    val initialValue = FrequencyStatsCounter[T](nonValue, -1, 0)
     val accumulator =
-      dataWeightPairs.sparkContext.accumulator[FrequencyStatsCounter](initialValue)(acumulatorParam)
+      dataWeightPairs.sparkContext.accumulator[FrequencyStatsCounter[T]](initialValue)(acumulatorParam)
 
-    val groupedByValues = dataWeightPairs.groupBy((x: (T, Double)) => x._1)
-    val valueNetWeightPairs = groupedByValues
-      .map({ case (value: T, weightList: Seq[(T, Double)]) => (value, weightList.map(_._2).reduce(_ + _)) })
+    val weightsGroupedByValues: RDD[(T, Seq[(T,Double)])] = dataWeightPairs.groupBy(_._1)
+
+    val valueNetWeightPairs: RDD[(T, Double)] = weightsGroupedByValues
+      .map({ case (value, weightList) => (value, weightList.map(_._2).reduce(_ + _)) })
 
     valueNetWeightPairs.foreach({
-      case (value, weightAtValue) => accumulator.add(FrequencyStatsCounter(value, weightAtValue, weightAtValue))
+      case (value, weightAtValue) => accumulator.add(FrequencyStatsCounter[T](value, weightAtValue, weightAtValue))
     })
 
     val mode: T = accumulator.value.mode
     val weightOfMode: Double = accumulator.value.weightOfMode
     val totalWeight: Double = accumulator.value.totalWeight
 
-    println("The mode is " + mode)
-    println("The weight of the mode is " + weightOfMode)
-    println("The total weight is " + totalWeight)
-    (mode, totalWeight)
+    (mode, weightOfMode / totalWeight)
   }
 }
