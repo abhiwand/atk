08e265f00146ed98c0e7786e45fbc89c5501a951
nlsegerl <nathan.l.segerlind@intel.com>
2014-07-15 17:57:52 -0700

    TRIB-2245   Changed summary statistics to use an accumulator for the single pass statistics.

--- engine-spark/src/main/scala/com/intel/intelanalytics/engine/spark/NumericalStatistics.scala
+++ engine-spark/src/main/scala/com/intel/intelanalytics/engine/spark/NumericalStatistics.scala
@@ -1,70 +1,58 @@
 package com.intel.intelanalytics.engine.spark
 
-import org.apache.spark.SparkContext
+import org.apache.spark.{ AccumulatorParam, SparkContext }
 import org.apache.spark.SparkContext._
 import org.apache.spark.rdd.RDD
 import com.intel.intelanalytics.domain.frame.ColumnSummaryStatisticsReturn
 
-case class SimpleStatistics(weightedSum: Double, weightedProduct: Double, minimum: Double,
-                            maximum: Double, mode: Double, weightAtMode: Double, totalWeight: Double, count: Long)
-
 class NumericalStatistics(dataWeightPairs: RDD[(Double, Double)]) extends Serializable {
-
-  lazy val simpleStatistics: SimpleStatistics = generateFirstOrderStats(dataWeightPairs)
-
-  lazy val weightedSum: Double = simpleStatistics.weightedSum
-
-  lazy val weightedMean: Double = simpleStatistics.weightedSum / simpleStatistics.totalWeight
-
-  lazy val weightedGeometricMean: Double = Math.pow(simpleStatistics.weightedProduct, 1 / simpleStatistics.totalWeight)
-
-  lazy val weightedVariance: Double = generateVariance()
-
-  lazy val weightedStandardDeviation: Double = Math.sqrt(weightedVariance)
-
-  lazy val weightedMode: Double = simpleStatistics.mode
-
-  lazy val min = simpleStatistics.minimum
-
-  lazy val max = simpleStatistics.maximum
-
-  lazy val count = simpleStatistics.count
 
   lazy val summaryStatistics: ColumnSummaryStatisticsReturn = ColumnSummaryStatisticsReturn(mean = weightedMean,
     geometric_mean = weightedGeometricMean, variance = weightedVariance, standard_deviation = weightedStandardDeviation,
     mode = weightedMode, minimum = min, maximum = max, count = count)
 
-  lazy val weightedSkewness: Double = generateSkewness()
+  private lazy val simpleStatistics: SinglePassStatistics = generateSinglePassStatistics(dataWeightPairs)
 
-  lazy val weightedKurtosis: Double = generateKurtosis()
+  private lazy val weightedMean: Double = simpleStatistics.weightedSum / simpleStatistics.totalWeight
 
-  private def convertWeightedPairToStats(p: (Double, Double)): SimpleStatistics = {
+  private lazy val weightedGeometricMean: Double = Math.pow(simpleStatistics.weightedProduct, 1 / simpleStatistics.totalWeight)
+
+  private lazy val weightedVariance: Double = generateVariance()
+
+  private lazy val weightedStandardDeviation: Double = Math.sqrt(weightedVariance)
+
+  private lazy val weightedMode: Double = simpleStatistics.mode
+
+  private lazy val min: Double = simpleStatistics.minimum
+
+  private lazy val max: Double = simpleStatistics.maximum
+
+  private lazy val count: Long = simpleStatistics.count
+
+  private lazy val weightedSkewness: Double = generateSkewness()
+
+  private lazy val weightedKurtosis: Double = generateKurtosis()
+
+  private def convertWeightedPairToStats(p: (Double, Double)): SinglePassStatistics = {
     val data = p._1
     val weight = p._2
 
-    SimpleStatistics(weightedSum = data * weight,
+    SinglePassStatistics(weightedSum = data * weight,
       weightedProduct = Math.pow(data, weight), minimum = data, maximum = data, mode = data, weightAtMode = weight,
       totalWeight = weight, count = 1.toLong)
   }
 
-  private def combineFirstOrderStats(stats1: SimpleStatistics, stats2: SimpleStatistics) = {
+  private def generateSinglePassStatistics(dataWeightPairs: RDD[(Double, Double)]): SinglePassStatistics = {
 
-    val weightedSum = stats1.weightedSum + stats2.weightedSum
-    val weightedProduct = stats1.weightedProduct * stats2.weightedProduct
-    val weightedMin = Math.min(stats1.minimum, stats2.minimum)
-    val weightedMax = Math.max(stats1.maximum, stats2.maximum)
-    val totalWeight = stats1.totalWeight + stats2.totalWeight
-    val count = stats1.count + stats2.count
-    val (mode, weightAtMode) = if (stats1.weightAtMode > stats2.weightAtMode)
-      (stats1.mode, stats1.weightAtMode)
-    else
-      (stats2.mode, stats2.weightAtMode)
+    val accumulatorParam = new SinglePassStatisticsAccumulatorParam()
+    val initialValue = new SinglePassStatistics(0, weightedProduct = 1.toDouble,
+      minimum = Double.PositiveInfinity,
+      maximum = Double.NegativeInfinity, 0, 0, 0, 0)
+    val accumulator = dataWeightPairs.sparkContext.accumulator[SinglePassStatistics](initialValue)(accumulatorParam)
 
-    SimpleStatistics(weightedSum, weightedProduct, weightedMin, weightedMax, mode, weightAtMode, totalWeight, count)
-  }
+    dataWeightPairs.map(convertWeightedPairToStats).foreach(x => accumulator.add(x))
 
-  private def generateFirstOrderStats(dataWeightPairs: RDD[(Double, Double)]): SimpleStatistics = {
-    dataWeightPairs.map(convertWeightedPairToStats).reduce(combineFirstOrderStats)
+    accumulator.value
   }
 
   private def generateVariance(): Double = {
@@ -106,5 +94,31 @@ class NumericalStatistics(dataWeightPairs: RDD[(Double, Double)]) extends Serial
     val subtrahend = (3 * (n - 1) * (n - 1)).toDouble / ((n - 2) * (n - 3)).toDouble
 
     (leadingCoefficient * theSum) - subtrahend
+  }
+}
+
+case class SinglePassStatistics(weightedSum: Double, weightedProduct: Double, minimum: Double,
+                                maximum: Double, mode: Double, weightAtMode: Double, totalWeight: Double, count: Long)
+    extends Serializable
+
+class SinglePassStatisticsAccumulatorParam extends AccumulatorParam[SinglePassStatistics] with Serializable {
+
+  override def zero(initialValue: SinglePassStatistics) = SinglePassStatistics(0, weightedProduct = 1.toDouble,
+    minimum = Double.PositiveInfinity,
+    maximum = Double.NegativeInfinity, 0, 0, 0, 0)
+
+  override def addInPlace(stats1: SinglePassStatistics, stats2: SinglePassStatistics): SinglePassStatistics = {
+
+    val weightedSum = stats1.weightedSum + stats2.weightedSum
+    val weightedProduct = stats1.weightedProduct * stats2.weightedProduct
+    val weightedMin = Math.min(stats1.minimum, stats2.minimum)
+    val weightedMax = Math.max(stats1.maximum, stats2.maximum)
+    val totalWeight = stats1.totalWeight + stats2.totalWeight
+    val count = stats1.count + stats2.count
+    val (mode, weightAtMode) = if (stats1.weightAtMode > stats2.weightAtMode)
+      (stats1.mode, stats1.weightAtMode)
+    else
+      (stats2.mode, stats2.weightAtMode)
+    SinglePassStatistics(weightedSum, weightedProduct, weightedMin, weightedMax, mode, weightAtMode, totalWeight, count)
   }
 }
