53636616da607b4f67fa25418d6eafb58d3f8037
rodorad <rene.o.dorado@intel.com>
2014-07-16 17:46:55 -0700

    test some packing changes on my cluster

--- package/package-rest-server.sh
+++ package/package-rest-server.sh
@@ -25,8 +25,8 @@ mkdir -p  tarballs/$package/etc/intelanalytics/rest-server
 
 mkdir -p  tarballs/$package/usr/lib/intelanalytics/rest-server/lib
 
-cp -Rv ../api-server/src/main/resources/* tarballs/$package/etc/intelanalytics/rest-server
-cp -Rv ../engine/src/main/resources/* tarballs/$package/etc/intelanalytics/rest-server
+#cp -Rv ../api-server/src/main/resources/* tarballs/$package/etc/intelanalytics/rest-server
+#cp -Rv ../engine/src/main/resources/* tarballs/$package/etc/intelanalytics/rest-server
 cp -Rv config/intelanalytics-rest-server/assets/* tarballs/$package/
 #log "add build number to application conf"
 #sed -i "s/^intel.analytics.api.buildId.*/intel.analytics.api.buildId = \"$BUILD_NUMBER\"/g" tarballs/$package/etc/intelanalytics/rest-server/application.conf
