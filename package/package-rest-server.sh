660017c50c638f522f9c2f0492219b6cc2dfb564
rodorad <rene.o.dorado@intel.com>
2014-07-18 01:49:59 -0700

    update start script to use application.conf file, update upstart script to add all /usr/lib/ia/rest-server/lib/*.jar to the classpath

--- package/package-rest-server.sh
+++ package/package-rest-server.sh
@@ -25,14 +25,15 @@ mkdir -p  tarballs/$package/etc/intelanalytics/rest-server
 
 mkdir -p  tarballs/$package/usr/lib/intelanalytics/rest-server/lib
 
+cp -v ../conf/examples/application.conf.tpl tarballs/$package/etc/intelanalytics/rest-server
 #cp -Rv ../api-server/src/main/resources/* tarballs/$package/etc/intelanalytics/rest-server
 #cp -Rv ../engine/src/main/resources/* tarballs/$package/etc/intelanalytics/rest-server
-cp -Rv config/intelanalytics-rest-server/assets/* tarballs/$package/
+cp -Rv config/intelanalytics-rest-server/assets/* tarballs/$package/ 
 #log "add build number to application conf"
 #sed -i "s/^intel.analytics.api.buildId.*/intel.analytics.api.buildId = \"$BUILD_NUMBER\"/g" tarballs/$package/etc/intelanalytics/rest-server/application.conf
-   
 
-jars="engine-spark.jar api-server.jar engine.jar interfaces.jar igiraph-titan.jar"
+
+jars="engine-spark.jar api-server.jar engine.jar interfaces.jar igiraph-titan.jar graphon.jar"
 
 popd
 
@@ -42,8 +43,10 @@ do
 	jarPath=$(find .  -path ./package -prune -o -name $jar -print )
 	echo $jarPath
 	cp -v $jarPath $SCRIPTPATH/tarballs/$package/usr/lib/intelanalytics/rest-server/lib/
+
 done
 	jarPath=$(find .  -path ./package -prune -o -name launcher.jar -print)
+
 	echo $jarPath
 	cp -v $jarPath $SCRIPTPATH/tarballs/$package/usr/lib/intelanalytics/rest-server/launcher.jar
 	cp -v $jarPath $SCRIPTPATH/tarballs/$package/usr/lib/intelanalytics/rest-server/lib/
