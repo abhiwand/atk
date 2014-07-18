2bd970b176c3ba98aaeae86f8be7d656077ec9ab
Hicke, Ryan <ryan.hicke@intel.com>
2014-07-18 10:02:41 -0700

    Fix Type

--- api-server/src/main/scala/com/intel/intelanalytics/service/v1/viewmodels/GetQueryPages.scala
+++ api-server/src/main/scala/com/intel/intelanalytics/service/v1/viewmodels/GetQueryPages.scala
@@ -26,5 +26,5 @@ package com.intel.intelanalytics.service.v1.viewmodels
  * Case Class for listing the number of pages that a query contains
  * @param id id of page
  * @param url url to request page
-
+ */
 case class GetQueryPages(id: Long, url: String)
