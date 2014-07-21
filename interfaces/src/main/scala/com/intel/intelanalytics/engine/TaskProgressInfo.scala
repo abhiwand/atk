cc4b6ca5500a7e8945036c6c9c9fa13a7a8c37ae
schen55 <steven.shih-chi.chen@intel.com>
2014-07-21 10:01:48 -0700

    update comment

--- interfaces/src/main/scala/com/intel/intelanalytics/engine/TaskProgressInfo.scala
+++ interfaces/src/main/scala/com/intel/intelanalytics/engine/TaskProgressInfo.scala
@@ -2,6 +2,7 @@ package com.intel.intelanalytics.engine
 
 /**
  * represent extra progress information
- * @param retries current number of failed tasks
+ * @param retries current number of failed tasks. This is same as error in Spark.
+ *                Call it retries because it is a better user experience.
  */
 case class TaskProgressInfo(retries: Int)
