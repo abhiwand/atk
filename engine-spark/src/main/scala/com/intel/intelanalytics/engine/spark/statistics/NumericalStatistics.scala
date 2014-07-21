825da631648b7de1af0380d700c9ee448ab4948c
nlsegerl <nathan.l.segerlind@intel.com>
2014-07-21 11:11:23 -0700

    TRIB-2245
    Changed internal calculations of running sums / averages / net weights etc to use BigDecimal instead of Double to  improve numerical stability.

--- engine-spark/src/main/scala/com/intel/intelanalytics/engine/spark/statistics/NumericalStatistics.scala
+++ engine-spark/src/main/scala/com/intel/intelanalytics/engine/spark/statistics/NumericalStatistics.scala
@@ -15,12 +15,21 @@ import com.intel.intelanalytics.domain.frame.{ ColumnFullStatisticsReturn, Colum
 class NumericalStatistics(dataWeightPairs: RDD[(Double, Double)]) extends Serializable {
 
   /*
+   * Incoming weights and data are Doubles, but internal running sums are represented as BigDecimal to improve
+   * numerical stability.
+   *
+   * Values are recast to Doubles before being returned because we do not want to give the false impression that
+   * we are improving precision. The use of BigDecimals is only to reduce accumulated rounding error while combing
+   * values over many, many entries.
+   */
+
+  private lazy val singlePassStatistics: FirstPassStatistics = generateFirstPassStatistics()
+
+  /*
    * TODO: TRIB-3134  Investigate one-pass algorithms for weighted skewness and kurtosis. (Currently these parameters
    *  are handled in the second pass statistics, and this accounts for our separation of summary and full statistics
    *  at the API level.)
    */
-
-  private lazy val singlePassStatistics: FirstPassStatistics = generateSinglePassStatistics()
 
   private lazy val secondPassStatistics: SecondPassStatistics = generateSecondPassStatistics()
 
@@ -29,27 +38,34 @@ class NumericalStatistics(dataWeightPairs: RDD[(Double, Double)]) extends Serial
   /**
    * The weighted mean of the data.
    */
-  lazy val weightedMean: Double = singlePassStatistics.mean
+  lazy val weightedMean: Double = singlePassStatistics.mean.toDouble
 
   /**
    * The weighted geometric mean of the data. NaN when a data element is <= 0,
    * 1 when there are no data elements of positive weight.
    */
-  lazy val weightedGeometricMean: Double = if (singlePassStatistics.totalWeight > 0)
-    Math.exp(singlePassStatistics.weightedSumOfLogs / singlePassStatistics.totalWeight)
-  else if (singlePassStatistics.weightedSumOfLogs equals Double.NaN) {
-    Double.NaN
-  }
-  else {
-    1.toDouble
+  lazy val weightedGeometricMean: Double = {
+
+    val totalWeight: BigDecimal = singlePassStatistics.totalWeight
+    val weightedSumOfLogs: Option[BigDecimal] = singlePassStatistics.weightedSumOfLogs
+
+    if (totalWeight > 0 && weightedSumOfLogs.nonEmpty)
+      Math.exp((weightedSumOfLogs.get / totalWeight).toDouble)
+    else if (totalWeight > 0 && weightedSumOfLogs.isEmpty) {
+      Double.NaN
+    }
+    else {
+      // this is the totalWeight == 0 case
+      1.toDouble
+    }
   }
 
   /**
    * The weighted variance of the data. NaN when there are <=1 data elements.
    */
   lazy val weightedVariance: Double = {
-    val n = singlePassStatistics.count
-    if (n > 1) singlePassStatistics.weightedSumOfSquaredDistancesFromMean / (n - 1) else Double.NaN
+    val n: BigDecimal = BigDecimal(singlePassStatistics.count)
+    if (n > 1) (singlePassStatistics.weightedSumOfSquaredDistancesFromMean / (n - 1)).toDouble else Double.NaN
   }
 
   /**
@@ -96,36 +112,82 @@ class NumericalStatistics(dataWeightPairs: RDD[(Double, Double)]) extends Serial
    * NaN when there are <= 2 data elements of nonzero weight.
    */
   lazy val weightedSkewness: Double = {
-    val n = singlePassStatistics.count
-
-    if (n > 2) (n.toDouble / ((n - 1) * (n - 2)).toDouble) * secondPassStatistics.sumOfThirdWeighted else Double.NaN
+    val n: BigDecimal = BigDecimal(singlePassStatistics.count)
+    val sumOfThirdWeighted: Option[BigDecimal] = secondPassStatistics.sumOfThirdWeighted
+    if ((n > 2) && sumOfThirdWeighted.nonEmpty)
+      ((n / ((n - 1) * (n - 2))) * sumOfThirdWeighted.get).toDouble
+    else Double.NaN
   }
 
   /**
    * The weighted kurtosis of the dataset. NaN when there are <= 3 data elements of nonzero weight.
    */
   lazy val weightedKurtosis: Double = {
-    val n = singlePassStatistics.count
-    if (n > 3) {
-      val leadingCoefficient = (n * (n + 1)).toDouble / ((n - 1) * (n - 2) * (n - 3)).toDouble
+    val n = BigDecimal(singlePassStatistics.count)
+    val sumOfFourthWeighted: Option[BigDecimal] = secondPassStatistics.sumOfFourthWeighted
+    if ((n > 3) && sumOfFourthWeighted.nonEmpty) {
+      val leadingCoefficient: BigDecimal = (n * (n + 1)) / ((n - 1) * (n - 2) * (n - 3))
 
-      val subtrahend = (3 * (n - 1) * (n - 1)).toDouble / ((n - 2) * (n - 3)).toDouble
+      val subtrahend: BigDecimal = (3 * (n - 1) * (n - 1)) / ((n - 2) * (n - 3))
 
-      (leadingCoefficient * secondPassStatistics.sumOfFourthWeighted) - subtrahend
+      ((leadingCoefficient * secondPassStatistics.sumOfFourthWeighted.get) - subtrahend).toDouble
     }
     else {
       Double.NaN
     }
   }
 
-  private def generateSinglePassStatistics(): FirstPassStatistics = {
+  /*
+   * Contains all statistics that are computed in a single pass over the data. All statistics are in their weighted form.
+   *
+   * Floating point values that are running combinations over all of the data are represented as BigDecimal, whereas
+   * minimum, mode and maximum are Doubles since they are simply single data points.
+   * @param mean
+   * @param weightedSumOfSquares
+   * @param weightedSumOfSquaredDistancesFromMean
+   * @param weightedSumOfLogs
+   * @param minimum
+   * @param maximum
+   * @param mode
+   * @param weightAtMode
+   * @param totalWeight
+   * @param count
+   */
+  private case class FirstPassStatistics(mean: BigDecimal,
+                                         weightedSumOfSquares: BigDecimal,
+                                         weightedSumOfSquaredDistancesFromMean: BigDecimal,
+                                         weightedSumOfLogs: Option[BigDecimal],
+                                         minimum: Double,
+                                         maximum: Double,
+                                         mode: Double,
+                                         weightAtMode: Double,
+                                         totalWeight: BigDecimal,
+                                         count: Long)
+      extends Serializable
+
+  /*
+   * Second pass statistics - for computing higher moments from the mean.
+   *
+   * In the notation of:
+   * http://support.sas.com/documentation/cdl/en/procstat/63104/HTML/default/viewer.htm#procstat_univariate_sect026.htm
+   *
+   * xw : the weighted mean of the data
+   * sw: the weighted standard deviation of the data
+   *
+   * @param sumOfThirdWeighted  Sum over data-weight pairs (x, w) of  Math.pow(w, 1.5) * Math.pow((x - xw) / sw, 3)
+   * @param sumOfFourthWeighted Sum over data-weight pairs (x, w) of Math.pow(w, 2) * Math.pow(((x - xw) / sw), 4) })
+   */
+  private case class SecondPassStatistics(sumOfThirdWeighted: Option[BigDecimal],
+                                          sumOfFourthWeighted: Option[BigDecimal]) extends Serializable
+
+  private def generateFirstPassStatistics(): FirstPassStatistics = {
 
     val accumulatorParam = new FirstPassStatisticsAccumulatorParam()
 
     val initialValue = new FirstPassStatistics(mean = 0,
       weightedSumOfSquares = 0,
       weightedSumOfSquaredDistancesFromMean = 0,
-      weightedSumOfLogs = 0,
+      weightedSumOfLogs = Some(BigDecimal(0)),
       minimum = Double.PositiveInfinity,
       maximum = Double.NegativeInfinity,
       mode = Double.NaN,
@@ -141,14 +203,93 @@ class NumericalStatistics(dataWeightPairs: RDD[(Double, Double)]) extends Serial
     accumulator.value
   }
 
+  private def convertDataWeightPairToFirstPassStats(p: (Double, Double)): FirstPassStatistics = {
+    val dataAsDouble: Double = p._1
+    val weightAsDouble: Double = p._2
+
+    val dataAsBigDecimal: BigDecimal = BigDecimal(dataAsDouble)
+    val weightAsBigDecimal: BigDecimal = BigDecimal(weightAsDouble)
+
+    val weightedLog = if (dataAsDouble <= 0) None else Some(weightAsBigDecimal * BigDecimal(Math.log(dataAsDouble)))
+
+    FirstPassStatistics(mean = dataAsBigDecimal,
+      weightedSumOfSquares = weightAsBigDecimal * dataAsBigDecimal * dataAsBigDecimal,
+      weightedSumOfSquaredDistancesFromMean = BigDecimal(0),
+      weightedSumOfLogs = weightedLog,
+      minimum = dataAsDouble,
+      maximum = dataAsDouble,
+      mode = dataAsDouble,
+      weightAtMode = weightAsDouble,
+      totalWeight = weightAsBigDecimal,
+      count = 1.toLong)
+  }
+
+  /*
+   * Accumulator settings for gathering single pass statistics.
+   */
+  private class FirstPassStatisticsAccumulatorParam extends AccumulatorParam[FirstPassStatistics] with Serializable {
+
+    override def zero(initialValue: FirstPassStatistics) =
+      FirstPassStatistics(mean = 0,
+        weightedSumOfSquares = 0,
+        weightedSumOfSquaredDistancesFromMean = 0,
+        weightedSumOfLogs = Some(0),
+        minimum = Double.PositiveInfinity,
+        maximum = Double.NegativeInfinity,
+        mode = Double.NaN,
+        weightAtMode = 0,
+        totalWeight = 0,
+        count = 0)
+
+    override def addInPlace(stats1: FirstPassStatistics, stats2: FirstPassStatistics): FirstPassStatistics = {
+
+      val totalWeight = stats1.totalWeight + stats2.totalWeight
+      val mean =
+        if (totalWeight > 0)
+          (stats1.mean * stats1.totalWeight + stats2.mean * stats2.totalWeight) / totalWeight
+        else BigDecimal(0)
+
+      val weightedSumOfSquares = stats1.weightedSumOfSquares + stats2.weightedSumOfSquares
+
+      val sumOfSquaredDistancesFromMean = weightedSumOfSquares - 2 * mean * mean * totalWeight + mean * mean * totalWeight
+
+      val weightedSumOfLogs: Option[BigDecimal] =
+        if (stats1.weightedSumOfLogs.nonEmpty && stats2.weightedSumOfLogs.nonEmpty) {
+          Some(stats1.weightedSumOfLogs.get + stats2.weightedSumOfLogs.get)
+        }
+        else {
+          None
+        }
+
+      val min = Math.min(stats1.minimum, stats2.minimum)
+      val max = Math.max(stats1.maximum, stats2.maximum)
+
+      val count = stats1.count + stats2.count
+      val (mode, weightAtMode) = if (stats1.weightAtMode > stats2.weightAtMode)
+        (stats1.mode, stats1.weightAtMode)
+      else
+        (stats2.mode, stats2.weightAtMode)
+
+      FirstPassStatistics(mean = mean,
+        weightedSumOfSquares = weightedSumOfSquares,
+        weightedSumOfSquaredDistancesFromMean = sumOfSquaredDistancesFromMean,
+        weightedSumOfLogs = weightedSumOfLogs,
+        minimum = min,
+        maximum = max,
+        mode = mode,
+        weightAtMode = weightAtMode, totalWeight = totalWeight,
+        count = count)
+    }
+  }
+
   private def generateSecondPassStatistics(): SecondPassStatistics = {
 
     val mean = weightedMean
     val stddev = weightedStandardDeviation
 
-    if (stddev != 0) {
+    if (stddev != 0 && !stddev.isNaN()) {
       val accumulatorParam = new SecondPassStatisticsAccumulatorParam()
-      val initialValue = new SecondPassStatistics(0, 0)
+      val initialValue = new SecondPassStatistics(Some(BigDecimal(0)), Some(BigDecimal(0)))
       val accumulator = dataWeightPairs.sparkContext.accumulator[SecondPassStatistics](initialValue)(accumulatorParam)
 
       dataWeightPairs.filter(distributionUtils.hasPositiveWeight).
@@ -157,137 +298,54 @@ class NumericalStatistics(dataWeightPairs: RDD[(Double, Double)]) extends Serial
       accumulator.value
     }
     else {
-      SecondPassStatistics(Double.NaN, Double.NaN)
+      SecondPassStatistics(None, None)
     }
 
   }
 
-  private def convertDataWeightPairToFirstPassStats(p: (Double, Double)): FirstPassStatistics = {
-    val data = p._1
-    val weight = p._2
-
-    val weightedLog = if (data <= 0) Double.NaN else weight * Math.log(data)
-
-    FirstPassStatistics(mean = data,
-      weightedSumOfSquares = weight * data * data,
-      weightedSumOfSquaredDistancesFromMean = 0,
-      weightedSumOfLogs = weightedLog,
-      minimum = data,
-      maximum = data,
-      mode = data,
-      weightAtMode = weight,
-      totalWeight = weight,
-      count = 1.toLong)
-  }
-
   private def convertDataWeightPairToSecondPassStats(p: (Double, Double), mean: Double, stddev: Double): SecondPassStatistics = {
-    val x = p._1
-    val w = p._2
 
-    val thirdWeighted = if (stddev != 0) Math.pow(w, 1.5) * Math.pow((x - mean) / stddev, 3) else Double.NaN
-    val fourthWeighted = if (stddev != 0) Math.pow(w, 2) * Math.pow((x - mean) / stddev, 4) else Double.NaN
+    val dataAsDouble: Double = p._1
+    val weightAsDouble: Double = p._2
+
+    val thirdWeighted: Option[BigDecimal] =
+      if (stddev != 0 && !stddev.isNaN())
+        Some(BigDecimal(Math.pow(weightAsDouble, 1.5) * Math.pow((dataAsDouble - mean) / stddev, 3)))
+      else
+        None
+
+    val fourthWeighted: Option[BigDecimal] =
+      if (stddev != 0 && !stddev.isNaN())
+        Some(BigDecimal(Math.pow(weightAsDouble, 2) * Math.pow((dataAsDouble - mean) / stddev, 4)))
+      else
+        None
+
     SecondPassStatistics(sumOfThirdWeighted = thirdWeighted, sumOfFourthWeighted = fourthWeighted)
   }
-}
 
-/**
- * Contains all statistics that are computed in a single pass over the data. All statistics are in their weighted form.
- * @param mean
- * @param weightedSumOfSquares
- * @param weightedSumOfSquaredDistancesFromMean
- * @param weightedSumOfLogs
- * @param minimum
- * @param maximum
- * @param mode
- * @param weightAtMode
- * @param totalWeight
- * @param count
- */
-private case class FirstPassStatistics(mean: Double,
-                                       weightedSumOfSquares: Double,
-                                       weightedSumOfSquaredDistancesFromMean: Double,
-                                       weightedSumOfLogs: Double,
-                                       minimum: Double,
-                                       maximum: Double,
-                                       mode: Double,
-                                       weightAtMode: Double,
-                                       totalWeight: Double,
-                                       count: Long)
-    extends Serializable
+  private class SecondPassStatisticsAccumulatorParam() extends AccumulatorParam[SecondPassStatistics] with Serializable {
 
-/**
- * Accumulator settings for gathering single pass statistics.
- */
-private class FirstPassStatisticsAccumulatorParam extends AccumulatorParam[FirstPassStatistics] with Serializable {
+    override def zero(initialValue: SecondPassStatistics) = SecondPassStatistics(Some(BigDecimal(0)), Some(BigDecimal(0)))
 
-  override def zero(initialValue: FirstPassStatistics) =
-    FirstPassStatistics(mean = 0,
-      weightedSumOfSquares = 0,
-      weightedSumOfSquaredDistancesFromMean = 0,
-      weightedSumOfLogs = 0,
-      minimum = Double.PositiveInfinity,
-      maximum = Double.NegativeInfinity,
-      mode = Double.NaN,
-      weightAtMode = 0,
-      totalWeight = 0,
-      count = 0)
+    override def addInPlace(stats1: SecondPassStatistics, stats2: SecondPassStatistics): SecondPassStatistics = {
 
-  override def addInPlace(stats1: FirstPassStatistics, stats2: FirstPassStatistics): FirstPassStatistics = {
+      val sumOfThirdWeighted: Option[BigDecimal] = if (stats1.sumOfThirdWeighted.nonEmpty && stats2.sumOfThirdWeighted.nonEmpty) {
+        Some(stats1.sumOfThirdWeighted.get + stats2.sumOfThirdWeighted.get)
+      }
+      else {
+        None
+      }
 
-    val totalWeight = stats1.totalWeight + stats2.totalWeight
-    val mean =
-      if (totalWeight > 0) (stats1.mean * stats1.totalWeight + stats2.mean * stats2.totalWeight) / totalWeight else 0
+      val sumOfFourthWeighted: Option[BigDecimal] = if (stats1.sumOfFourthWeighted.nonEmpty && stats2.sumOfFourthWeighted.nonEmpty) {
+        Some(stats1.sumOfFourthWeighted.get + stats2.sumOfFourthWeighted.get)
+      }
+      else {
+        None
+      }
 
-    val weightedSumOfSquares = stats1.weightedSumOfSquares + stats2.weightedSumOfSquares
-
-    val sumOfSquaredDistancesFromMean = weightedSumOfSquares - 2 * mean * mean * totalWeight + mean * mean * totalWeight
-
-    val weightedSumOfLogs = stats1.weightedSumOfLogs + stats2.weightedSumOfLogs
-    val min = Math.min(stats1.minimum, stats2.minimum)
-    val max = Math.max(stats1.maximum, stats2.maximum)
-
-    val count = stats1.count + stats2.count
-    val (mode, weightAtMode) = if (stats1.weightAtMode > stats2.weightAtMode)
-      (stats1.mode, stats1.weightAtMode)
-    else
-      (stats2.mode, stats2.weightAtMode)
-
-    FirstPassStatistics(mean = mean,
-      weightedSumOfSquares = weightedSumOfSquares,
-      weightedSumOfSquaredDistancesFromMean = sumOfSquaredDistancesFromMean,
-      weightedSumOfLogs = weightedSumOfLogs,
-      minimum = min,
-      maximum = max,
-      mode = mode,
-      weightAtMode = weightAtMode, totalWeight = totalWeight,
-      count = count)
+      SecondPassStatistics(sumOfThirdWeighted = sumOfThirdWeighted, sumOfFourthWeighted = sumOfFourthWeighted)
+    }
   }
+
 }
 
-/**
- * Second pass statistics - for computing higher moments from the mean.
- *
- * In the notation of:
- * http://support.sas.com/documentation/cdl/en/procstat/63104/HTML/default/viewer.htm#procstat_univariate_sect026.htm
- *
- * xw : the weighted mean of the data
- * sw: the weighted standard deviation of the data
- *
- * @param sumOfThirdWeighted  Sum over data-weight pairs (x, w) of  Math.pow(w, 1.5) * Math.pow((x - xw) / sw, 3)
- * @param sumOfFourthWeighted Sum over data-weight pairs (x, w) of Math.pow(w, 2) * Math.pow(((x - xw) / sw), 4) })
- */
-private case class SecondPassStatistics(sumOfThirdWeighted: Double, sumOfFourthWeighted: Double) extends Serializable
-
-private class SecondPassStatisticsAccumulatorParam() extends AccumulatorParam[SecondPassStatistics] with Serializable {
-
-  override def zero(initialValue: SecondPassStatistics) = SecondPassStatistics(0, 0)
-
-  override def addInPlace(stats1: SecondPassStatistics, stats2: SecondPassStatistics): SecondPassStatistics = {
-
-    val sumOfThirdWeighted = stats1.sumOfThirdWeighted + stats2.sumOfThirdWeighted
-
-    val sumOfFourthWeighted = stats1.sumOfFourthWeighted + stats2.sumOfFourthWeighted
-
-    SecondPassStatistics(sumOfThirdWeighted = sumOfThirdWeighted, sumOfFourthWeighted = sumOfFourthWeighted)
-  }
-}
--- engine-spark/src/main/scala/com/intel/intelanalytics/engine/spark/statistics/OrderStatistics.scala
+++ engine-spark/src/main/scala/com/intel/intelanalytics/engine/spark/statistics/OrderStatistics.scala
@@ -33,12 +33,13 @@ class OrderStatistics[T: ClassTag](dataWeightPairs: RDD[(T, Double)])(implicit o
    */
   private def computeMedian: Option[T] = {
 
-    val sortedDataWeightPairs: RDD[(T, Double)] =
-      dataWeightPairs.filter(distributionUtils.hasPositiveWeight).sortByKey(ascending = true)
+    val sortedDataWeightPairs: RDD[(T, BigDecimal)] =
+      dataWeightPairs.filter(distributionUtils.hasPositiveWeight).
+        map({ case (data, weight) => (data, BigDecimal(weight)) }).sortByKey(ascending = true)
 
-    val weightsOfPartitions: Array[Double] = sortedDataWeightPairs.mapPartitions(sumWeightsInPartition).collect()
+    val weightsOfPartitions: Array[BigDecimal] = sortedDataWeightPairs.mapPartitions(sumWeightsInPartition).collect()
 
-    val totalWeight: Double = weightsOfPartitions.reduce(_ + _)
+    val totalWeight: BigDecimal = weightsOfPartitions.reduce(_ + _)
 
     if (totalWeight <= 0) {
       None
@@ -47,13 +48,13 @@ class OrderStatistics[T: ClassTag](dataWeightPairs: RDD[(T, Double)])(implicit o
 
       // the "median partition" is the partition the contains the median
       val (indexOfMedianPartition, weightInPrecedingPartitions) = findMedianPartition(weightsOfPartitions, totalWeight)
-      val medianPartition: Array[(T, Double)] =
+      val medianPartition: Array[(T, BigDecimal)] =
         sortedDataWeightPairs.mapPartitionsWithIndex(partitionSelector(indexOfMedianPartition), true).collect()
 
       // now we find where the median value of the dataset resides inside the median partition
       val weightPrecedingMedian = (totalWeight / 2) - weightInPrecedingPartitions
       var indexOfMedian: Int = 0
-      var weightSoFar: Double = 0
+      var weightSoFar: BigDecimal = 0
 
       while (weightSoFar + medianPartition(indexOfMedian)._2 < weightPrecedingMedian) {
         weightSoFar += medianPartition(indexOfMedian)._2
@@ -64,21 +65,21 @@ class OrderStatistics[T: ClassTag](dataWeightPairs: RDD[(T, Double)])(implicit o
     }
   }
 
-  // Sums the weights in an individual partition of an RDD[(T, Double)] where second coordinate is "weight'
-  private def sumWeightsInPartition(it: Iterator[(T, Double)]): Iterator[Double] =
+  // Sums the weights in an individual partition of an RDD[(T, BigDecimal)] where second coordinate is "weight'
+  private def sumWeightsInPartition(it: Iterator[(T, BigDecimal)]): Iterator[BigDecimal] =
     if (it.nonEmpty) Iterator(it.map({ case (x, w) => w }).reduce(_ + _)) else Iterator(0)
 
   // Given a desired partition, an index of a partition and its iterator, this returns the iterator of the incoming
   // partition if it is the descired partition, and empty iterator if it is not the desired partition.
-  private def partitionSelector(selectedPartition: Int)(index: Int, partitionIterator: Iterator[(T, Double)]): Iterator[(T, Double)] = {
-    if (index == selectedPartition) partitionIterator else Iterator[(T, Double)]()
+  private def partitionSelector(selectedPartition: Int)(index: Int, partitionIterator: Iterator[(T, BigDecimal)]): Iterator[(T, BigDecimal)] = {
+    if (index == selectedPartition) partitionIterator else Iterator[(T, BigDecimal)]()
   }
 
   // Find the index of the partition that contains the median of the of the dataset, as well as the net weight of the
   // partitions that precede it.
-  private def findMedianPartition(weightsOfPartitions: Array[Double], totalWeight: Double): (Int, Double) = {
+  private def findMedianPartition(weightsOfPartitions: Array[BigDecimal], totalWeight: BigDecimal): (Int, BigDecimal) = {
     var currentPartition: Int = 0
-    var weightInPrecedingPartitions: Double = 0
+    var weightInPrecedingPartitions: BigDecimal = 0
     while (weightInPrecedingPartitions + weightsOfPartitions(currentPartition) < totalWeight / 2) {
       currentPartition += 1
       weightInPrecedingPartitions += weightsOfPartitions(currentPartition)
