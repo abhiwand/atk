7237ce50ae622a8abfc3d7afb0890dd4c8115fd7
Amit Bhattacharjee <amit.bhattacharjee@intel.com>
2014-07-22 11:39:26 -0500

    Changed for testing locally

--- graphon/src/main/scala/com/intel/spark/graphon/communitydetection/kclique/GBVertexSetter.scala
+++ graphon/src/main/scala/com/intel/spark/graphon/communitydetection/kclique/GBVertexSetter.scala
@@ -51,37 +51,14 @@ class GBVertexSetter(gbVertexList: RDD[GBVertex], vertexCommunitySet: RDD[(Long,
         ({ case (x, y) => y.union(x) }),
         ({ case (x, y) => y.union(x) }))
 
-    val gbVertexIDOldPropertiesPairs: RDD[(Long, Seq[Property])] = gbVertexList.map({
-      case GBVertex(physicalId, gbId, properties) => (physicalId.asInstanceOf[Long], properties)
-    })
-    //  To create a GB Vertex from the (Long, Set[Long]) pair in vertexAndCommunitySet.
-    //  The ID (the long) is both the physical ID and the GB ID. Since we need a GB ID to
-    //  create the vertex, we need to create some default property (TitanReader.TITAN_READER_DEFAULT_GB_ID)
-    //  and stick the physical ID in there as well (so the property list will have two entries:
-    //  the GB ID and the community list)
-    //  The label of the communitySet property is defined as communityPropertyDefaultLabel
-
-    //  Converted communitySet from scala Set to Java.util.Set using .asJava decorator method of JavaConverters.
-    //  We need to use Java objects while actually storing to Titan using GraphBuilder.
-
-    val gbVertexIDCommunityPropertyPairs: RDD[(Long, Seq[Property])] = gbVertexCombinedWithEmptyCommunity.map({
-      case (vertexId, communitySet) => (vertexId,
+    //    Convert the pairs of vertex and community set into a GB Vertex
+    val newGBVertices: RDD[GBVertex] = gbVertexCombinedWithEmptyCommunity.map({
+      case (vertexId, communitySet) => GBVertex(
+        java.lang.Long.valueOf(vertexId),
+        Property(TitanReader.TITAN_READER_DEFAULT_GB_ID, java.lang.Long.valueOf(vertexId)),
         Seq(Property(communityPropertyDefaultLabel, ScalaToJavaCollectionConverter.convertSet(communitySet))))
     })
-
-    val gbVertexIDOldPropertiesCogroupCommunityProperty: RDD[(Long, (Seq[Seq[Property]], Seq[Seq[Property]]))] = gbVertexIDOldPropertiesPairs.cogroup(gbVertexIDCommunityPropertyPairs)
-
-    val gbVertexIDNewProperties: RDD[(Long, Seq[Property])] = gbVertexIDOldPropertiesCogroupCommunityProperty
-      .map({ case (vertexId, (Seq(seqOfOldProperties), Seq(seqOfCommunityProperty))) => (vertexId, (seqOfOldProperties, seqOfCommunityProperty)) })
-      .map({ case (vertexId, (seqOfOldProperties, seqOfCommunityProperty)) => (vertexId, seqOfOldProperties ++ seqOfCommunityProperty) })
-
-    val updatedGBVertices: RDD[GBVertex] = gbVertexIDNewProperties.map({
-      case (vertexId, newSeqOfProperties) => GBVertex(
-        vertexId,
-        Property(TitanReader.TITAN_READER_DEFAULT_GB_ID, vertexId),
-        newSeqOfProperties)
-    })
-    updatedGBVertices
+    newGBVertices
   }
 
 }
