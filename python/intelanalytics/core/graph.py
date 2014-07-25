23f186be09e9fb73ba60a0096d6d1bfe1b4b0f4d
Andrew Wicker <andrew.w.wicker@intel.com>
2014-07-25 14:54:36 -0700

    added python documentation

--- python/intelanalytics/core/graph.py
+++ python/intelanalytics/core/graph.py
@@ -512,6 +512,7 @@ class BigGraph(CommandSupport):
 
 class GraphSampling(object):
     """
+    Functionality for creating a graph sample
 
     .. versionadded:: 0.8
 
@@ -524,15 +525,21 @@ class GraphSampling(object):
 
     def vertex_sample(self, size, sample_type, **kwargs):
         """
-        Create a vertex induced subgraph obtained by vertex sampling.
+        Create a vertex induced subgraph obtained by vertex sampling
+
+        Three types of vertex sampling are provided: 'uniform', 'degree', and 'degreedist'.  A 'uniform' vertex sample
+        is obtained by sampling vertices uniformly at random.  For 'degree' vertex sampling, each vertex is weighted by
+        its out-degree.  For 'degreedist' vertex sampling, each vertex is weighted by the total number of vertices that
+        have the same out-degree as it.  That is, the weight applied to each vertex for 'degreedist' vertex sampling is
+        given by the out-degree histogram bin size.
 
         Parameters
         ----------
         size : int
-            number of vertices to sample from the graph
-        sample_type : 'uniform', 'degree', or 'degreedist'
-            the type of vertex sample
-        seed : (optional) int
+            the number of vertices to sample from the graph
+        sample_type : str
+            the type of vertex sample among: ['uniform', 'degree', 'degreedist']
+        seed : (optional keyword argument) int
             random seed value
 
         Returns
@@ -542,6 +549,11 @@ class GraphSampling(object):
 
         Examples
         --------
+        Assume a set of rules created on a BigFrame that specifies 'user' and 'product' vertices as well as an edge
+        rule.  The BigGraph created from this data can be vertex sampled to obtained a vertex induced subgraph:
+
+            graph = BigGraph([user_vertex_rule, product_vertex_rule, edge_rule])
+            subgraph = graph.sampling.vertex_sample(1000, 'uniform')
 
         """
         if 'seed' in kwargs:
