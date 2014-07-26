252c71582ea35f1175b36ad1b18cc0011aba7900
spkavuly <soila.p.kavulya@intel.com>
2014-07-25 20:07:50 -0700

    TRIB-2729 Integration test for Gremlin query

--- engine-spark/src/test/scala/com/intel/intelanalytics/engine/spark/graph/query/GremlinQueryITest.scala
+++ engine-spark/src/test/scala/com/intel/intelanalytics/engine/spark/graph/query/GremlinQueryITest.scala
@@ -1,28 +1,52 @@
 package com.intel.intelanalytics.engine.spark.graph.query
 
-import com.intel.intelanalytics.engine.plugin.Invocation
 import com.intel.intelanalytics.engine.spark.graph.TestingTitan
-import com.intel.intelanalytics.security.UserPrincipal
-import org.scalatest.{Matchers, FlatSpec}
+import com.intel.testutils.MatcherUtils._
+import org.scalatest.{FlatSpec, Matchers}
 import org.specs2.mock.Mockito
-
-import scala.concurrent.ExecutionContext
+import spray.json.JsNumber
 
 class GremlinQueryITest extends FlatSpec with Matchers with TestingTitan with Mockito {
 
-  "GremlinQuery" should "execute valid Gremlin queries" in {
-    val vertex = graph.addVertex(1)
-    vertex.setProperty("name", "marko")
-    vertex.setProperty("age", 29)
+  "executeGremlinQuery" should "execute valid Gremlin queries" in {
+    val vertex1 = titanGraph.addVertex(null)
+    val vertex2 = titanGraph.addVertex(null)
+    val edge = titanGraph.addEdge(null, vertex1, vertex2, "knows")
 
-    val invocation = mock[Invocation]
-    implicit val user = mock[UserPrincipal]
-    implicit val executionContext = mock[ExecutionContext]
-    val gremlinPlugin = new GremlinQuery
-    gremlinPlugin.configuration
-    val queryArgs = new QueryArgs("g.V")
-    val queryResults = gremlinPlugin.execute(invocation, queryArgs)
+    vertex1.setProperty("name", "alice")
+    vertex1.setProperty("age", 23)
+    vertex2.setProperty("name", "bob")
+    vertex2.setProperty("age", 27)
 
+    val gremlinQuery = new GremlinQuery()
+    val gremlinScript = """g.V("name", "alice").out("knows")"""
+
+    val bindings = gremlinQuery.gremlinExecutor.createBindings()
+    bindings.put("g", titanGraph)
+
+    val results = gremlinQuery.executeGremlinQuery(titanGraph, gremlinScript, bindings).toArray
+    val vertexCount = gremlinQuery.executeGremlinQuery(titanGraph, "g.V.count()", bindings).toArray
+    val edgeCount = gremlinQuery.executeGremlinQuery(titanGraph, "g.E.count()", bindings).toArray
+
+    results.size should equal(1)
+    vertexCount.size should equal(1)
+    edgeCount.size should equal(1)
+
+    vertex2 should equalsGraphSONVertex(results(0))
+    vertexCount(0) should equal(JsNumber(2))
+    edgeCount(0) should equal(JsNumber(1))
   }
+  "executeGremlinQuery" should "throw a Runtime exception when executing invalid Gremlin" in {
+    intercept[java.lang.RuntimeException] {
+      val gremlinQuery = new GremlinQuery()
+      val gremlinScript = """InvalidGremlin"""
+
+      val bindings = gremlinQuery.gremlinExecutor.createBindings()
+      bindings.put("g", titanGraph)
+
+      gremlinQuery.executeGremlinQuery(titanGraph, gremlinScript, bindings).toArray
+    }
+  }
+
 
 }
