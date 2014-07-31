b90117f9c8ab74f1a8906071e10dcc7d9ad9696a
spkavuly <soila.p.kavulya@intel.com>
2014-07-31 16:47:40 -0700

    TRIB-2246 Added topk and entropy calculations

--- engine-spark/src/test/scala/com/intel/intelanalytics/engine/spark/EntropyTest.scala
+++ engine-spark/src/test/scala/com/intel/intelanalytics/engine/spark/EntropyTest.scala
@@ -5,11 +5,30 @@ import org.scalatest.Matchers
 
 class EntropyTest extends TestingSparkContextFlatSpec with Matchers {
   val inputList = List(
-    Array[Any](0, "a"),
-    Array[Any](0, "a"),
-    Array[Any](0, "b"),
-    Array[Any](1, "b"),
-    Array[Any](1, "b"),
-    Array[Any](2, "c"))
+    Array[Any](-1, "a", 0),
+    Array[Any](0, "a", 0),
+    Array[Any](0, "b", 0),
+    Array[Any](1, "b", 0),
+    Array[Any](1, "b", 0),
+    Array[Any](2, "c", 0))
 
+  val emptyList = List.empty[Array[Any]]
+
+  val epsilon = 0.000001
+  "entropy" should "compute the correct entropy" in {
+    val frameRdd = sparkContext.parallelize(inputList, 2)
+    val entropy1 = SparkOps.entropy(frameRdd, 0)
+    val entropy2 = SparkOps.entropy(frameRdd, 1)
+    val entropy3 = SparkOps.entropy(frameRdd, 2)
+
+    entropy1 should equal(1.329661 +- epsilon)
+    entropy2 should equal(1.011404 +- epsilon)
+    entropy3 should equal(0)
+  }
+  "entropy" should "should throw a runtime exception if the frame is empty" in {
+    intercept[java.lang.RuntimeException] {
+      val frameRdd = sparkContext.parallelize(emptyList, 2)
+      SparkOps.entropy(frameRdd, 0)
+    }
+  }
 }
