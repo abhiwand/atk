03414dd78ab40988c6fa617203bd10e92c309acd
Xia Zhu <xia.zhu@intel.com>
2014-08-06 10:18:02 -0700

    remove netflix data in package directory. remove dev example

---
+++ examples/netflix/netflix.py
@@ -0,0 +1,50 @@
+#
+# Netflix example - create a frame and load a graph
+#
+# Depends on a netflix.csv file
+#
+# Usage:
+#
+#   Copy data to HDFS
+#       hadoop fs -copyFromLocal netflix.csv {fsRoot in HDFS}
+#
+#   At Python prompt
+#       import os
+#       execfile('/path/to/netflix.py')
+#
+
+from intelanalytics import *
+
+#loggers.set_http()
+
+print("server ping")
+server.ping()
+
+print("define csv file")
+csv = CsvFile("/netflix.csv", schema= [('user', int32),
+                                              ('vertexType', str),
+                                              ('movie', int32),
+                                              ('rating', str),
+                                              ('splits', str)])
+
+print("create big frame")
+frame = BigFrame(csv)
+
+errors = frame.get_error_frame()
+
+print("inspect frame")
+print frame.inspect(10)
+print("frame row count " + str(frame.row_count))
+
+print("inspect frame errors")
+print errors.inspect(10)
+print("frame row count " + str(errors.row_count))
+
+print("define graph parsing rules")
+movie = VertexRule("movie", frame.movie)
+user = VertexRule("user", frame.user, {"vertexType": frame.vertexType})
+rates = EdgeRule("rating", user, movie, { "splits": frame.splits }, is_directed = True)
+
+print("create graph")
+graph = BigGraph([user, movie, rates])
+print("created graph " + graph.name)
