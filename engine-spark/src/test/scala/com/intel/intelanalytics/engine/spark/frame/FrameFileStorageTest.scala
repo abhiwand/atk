08635ebefade9435f2ed99672e2adddfafbbffa4
Todd Lisonbee <todd.lisonbee@intel.com>
2014-08-14 09:46:05 -0700

    TRIB-3431 test cleanup

--- engine-spark/src/test/scala/com/intel/intelanalytics/engine/spark/frame/FrameFileStorageTest.scala
+++ engine-spark/src/test/scala/com/intel/intelanalytics/engine/spark/frame/FrameFileStorageTest.scala
@@ -1,17 +1,16 @@
 package com.intel.intelanalytics.engine.spark.frame
 
 import org.scalatest.FlatSpec
-import com.intel.intelanalytics.engine.spark.SparkEngineConfig
 
 class FrameFileStorageTest extends FlatSpec {
 
-  val frameFileStorage = new FrameFileStorage(SparkEngineConfig.fsRoot, null)
+  val frameFileStorage = new FrameFileStorage("hdfs://hostname/user/iauser", null)
 
   "FrameFileStorage" should "determine the correct data frames base directory" in {
-    assert(frameFileStorage.frameBaseDirectory(1L).toString == "hdfs:/invalid-fsroot-host/user/iauser/intelanalytics/dataframes/1")
+    assert(frameFileStorage.frameBaseDirectory(1L).toString == "hdfs://hostname/user/iauser/intelanalytics/dataframes/1")
   }
 
   it should "determine the correct data frame revision directory" in {
-    assert(frameFileStorage.frameRevisionDirectory(1L, 1).toString == "hdfs:/invalid-fsroot-host/user/iauser/intelanalytics/dataframes/1/rev1")
+    assert(frameFileStorage.frameRevisionDirectory(1L, 1).toString == "hdfs://hostname/user/iauser/intelanalytics/dataframes/1/rev1")
   }
 }
