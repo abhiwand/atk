9e7ba85d8675a523da8649f278a5066647130a12
Todd Lisonbee <todd.lisonbee@intel.com>
2014-08-18 09:04:02 -0700

    TRIB-3449 fixing test - adding skip option

--- graphbuilder-3/src/main/scala/com/intel/graphbuilder/driver/spark/rdd/EnvironmentValidator.scala
+++ graphbuilder-3/src/main/scala/com/intel/graphbuilder/driver/spark/rdd/EnvironmentValidator.scala
@@ -7,32 +7,36 @@ import java.io.File
  */
 object EnvironmentValidator extends Serializable {
 
+  var skipEnvironmentValidation = false
+
   /**
    * Validate that ispark-deps.jar is available to Spark.
    *
    * This jar is needed to connect to Titan from executors.
    */
   lazy val validateISparkDepsAvailable: Unit = {
-    print("validating that ispark-deps.jar is available on the SPARK_CLASSPATH")
-    val sparkClasspath = System.getenv("SPARK_CLASSPATH")
-    if (sparkClasspath == null) {
-      throw new RuntimeException("SPARK_CLASSPATH is NOT set.  Please set SPARK_CLASSPATH, include ispark-deps.jar in the classpath, and restart Spark")
-    }
-    else {
-      if (!sparkClasspath.contains("ispark-deps.jar")) {
-        throw new RuntimeException("SPARK_CLASSPATH does NOT contain ispark-deps.jar, please add, and restart Spark")
+    if (!skipEnvironmentValidation) {
+      print("validating that ispark-deps.jar is available on the SPARK_CLASSPATH")
+      val sparkClasspath = System.getenv("SPARK_CLASSPATH")
+      if (sparkClasspath == null) {
+        throw new RuntimeException("SPARK_CLASSPATH is NOT set.  Please set SPARK_CLASSPATH, include ispark-deps.jar in the classpath, and restart Spark")
       }
       else {
-        val parts = sparkClasspath.split("[: ]+")
-        parts.foreach(part => {
-          if (part.contains("ispark-deps.jar")) {
-            val jar = new File(part)
-            if (!jar.exists() || !jar.canRead) {
-              throw new RuntimeException("ispark-deps.jar does NOT exist or permissions problem with: " + part + " please install jar or update SPARK_CLASSPATH and restart Spark")
+        if (!sparkClasspath.contains("ispark-deps.jar")) {
+          throw new RuntimeException("SPARK_CLASSPATH does NOT contain ispark-deps.jar, please add, and restart Spark")
+        }
+        else {
+          val parts = sparkClasspath.split("[: ]+")
+          parts.foreach(part => {
+            if (part.contains("ispark-deps.jar")) {
+              val jar = new File(part)
+              if (!jar.exists() || !jar.canRead) {
+                throw new RuntimeException("ispark-deps.jar does NOT exist or permissions problem with: " + part + " please install jar or update SPARK_CLASSPATH and restart Spark")
+              }
+              print("ispark-deps.jar was found: " + part)
             }
-            print("ispark-deps.jar was found: " + part)
-          }
-        })
+          })
+        }
       }
     }
   }
