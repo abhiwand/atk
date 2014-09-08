6b9a3fd04b169d417fa57fe13686780354dff49c
spkavuly <soila.p.kavulya@intel.com>
2014-09-08 14:46:56 -0700

    TRIB-2246 Using priority queue instead of tree map for topK

--- engine-spark/src/main/scala/com/intel/intelanalytics/engine/spark/statistics/TopKRDDFunctions.scala
+++ engine-spark/src/main/scala/com/intel/intelanalytics/engine/spark/statistics/TopKRDDFunctions.scala
@@ -1,11 +1,17 @@
 package com.intel.intelanalytics.engine.spark.statistics
 
 import com.intel.intelanalytics.engine.Rows._
+import org.apache.spark.SparkContext._
 import org.apache.spark.rdd.RDD
 
-import scala.collection.immutable.TreeMap
+import scala.collection.mutable.PriorityQueue
 
 private[spark] object TopKRDDFunctions extends Serializable {
+
+  case class CountPair(key: Any, value: Long) extends Ordered[CountPair] {
+    def compare(that: CountPair) = this.value compare that.value
+  }
+
   /**
    * Returns the top (or bottom) K distinct values by count for specified data column.
    *
@@ -17,23 +23,24 @@ private[spark] object TopKRDDFunctions extends Serializable {
    */
   def topK(frameRdd: RDD[Row], columnIndex: Int, k: Int, reverse: Boolean = false): RDD[Row] = {
     require(columnIndex >= 0, "label column index must be greater than or equal to zero")
-    val groupedRDD = frameRdd.groupBy(row => row(columnIndex))
-    val distinctCountRDD = groupedRDD.map({ case (distinctValue, rows) => (distinctValue, rows.size.toLong) })
+    val distinctCountRDD = frameRdd.map(row => (row(columnIndex), 1l)).reduceByKey((a, b) => a + b)
 
     //Sort by descending order to get top K
     val isDescendingSort = !reverse
 
     // Efficiently get the top (or bottom) K entries by first sorting the top (or bottom) K entries in each partition
+    // This function uses a tree map instead of a bounded priority queue (despite the added overhead)
+    // because we need to keep key-value pairs
     val topKByPartition = distinctCountRDD.mapPartitions(countIterator => {
-      sortTopKByValue(countIterator, k, isDescendingSort)
+      Iterator.single(sortTopKByValue(countIterator, k, isDescendingSort))
+    }).reduce({ (topPartition1, topPartition2) =>
+      mergeSortedSeqs(topPartition1, topPartition2, Seq(), k, isDescendingSort)
     })
 
     // Get the overall top (or bottom) K entries from partitions
     // Works when K*num_partitions fits in memory of single machine.
-    // TODO: Figure out more efficient way to sort by value in Spark
-    val topIterator = sortTopKByValue(topKByPartition.collect().toIterator, k, isDescendingSort)
-    val topRows = for ((distinctValue, count) <- topIterator) yield Array(distinctValue, count)
-    frameRdd.sparkContext.parallelize(topRows.toSeq)
+    val topRows = topKByPartition.take(k).map(f => Array(f.key, f.value))
+    frameRdd.sparkContext.parallelize(topRows)
   }
 
   /**
@@ -46,18 +53,45 @@ private[spark] object TopKRDDFunctions extends Serializable {
    * @param descending Sort in descending order if true, else sort in ascending order
    * @return Top K sorted entries
    */
-  def sortTopKByValue[K, V: Ordering](inputIterator: Iterator[(K, V)],
-                                      k: Int, descending: Boolean = false): Iterator[(K, V)] = {
-    val ordering = if (descending) implicitly[Ordering[V]].reverse else implicitly[Ordering[V]]
-    var treeMap = new TreeMap[V, K]()(ordering)
+  def sortTopKByValue(inputIterator: Iterator[(Any, Long)],
+                      k: Int, descending: Boolean = false): Seq[CountPair] = {
+    val ordering = if (descending) Ordering[CountPair].reverse else Ordering[CountPair]
+    val priorityQueue = new PriorityQueue[CountPair]()(ordering)
 
-    inputIterator.foreach({
-      case (key, value) =>
-        treeMap += (value -> key)
-        if (treeMap.size > k) treeMap = treeMap.dropRight(1)
+    inputIterator.foreach(element => {
+      priorityQueue.enqueue(CountPair(element._1, element._2))
+      if (priorityQueue.size > k)  priorityQueue.dequeue()
     })
 
-    val sortedK = for ((value, key) <- treeMap.toSeq) yield (key, value)
-    sortedK.toIterator
+    priorityQueue.reverse.dequeueAll.toSeq
   }
+
+  private def mergeSortedSeqs(topKSeq1: Seq[CountPair], topKSeq2: Seq[CountPair], accumulator: Seq[CountPair],
+                        k: Int, descending: Boolean = false): Seq[CountPair] = {
+    val ordering = if (descending) implicitly[Ordering[CountPair]].reverse else implicitly[Ordering[CountPair]]
+
+    (topKSeq1, topKSeq2) match {
+      case (seq1, Nil) => accumulator ++ seq1
+      case (Nil, seq2) => accumulator ++ seq2
+      case (seq1, seq2) => {
+        val (newTopKSeq1, newTopKSeq2, newAccumulator) = getNextMergeSeqs(seq1, seq2, accumulator, ordering)
+        mergeSortedSeqs(newTopKSeq1, newTopKSeq2, newAccumulator, k, descending)
+      }
+    }
+  }
+
+  private def getNextMergeSeqs(seq1: Seq[CountPair],
+                               seq2: Seq[CountPair],
+                               accumulator: Seq[CountPair],
+                               ordering: Ordering[CountPair]): (Seq[CountPair], Seq[CountPair], Seq[CountPair]) = {
+    val (head1, tailSeq1) = (seq1.head, seq1.tail)
+    val (head2, tailSeq2) = (seq2.head, seq2.tail)
+    val (newSeq1, newSeq2, newAccumulator) = if (ordering.lteq(head1, head2))
+      (tailSeq1, seq2, accumulator :+ head1)
+    else
+      (seq1, tailSeq2, accumulator :+ head2)
+    (newSeq1, newSeq2, newAccumulator)
+  }
+
+
 }
