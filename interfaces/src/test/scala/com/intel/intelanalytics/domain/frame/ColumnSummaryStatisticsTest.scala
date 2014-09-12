6661cd8f02d4e983f941aa7b70c9b4bf0383966a
Todd Lisonbee <todd.lisonbee@intel.com>
2014-09-12 07:11:27 -0700

    Adding tests

--- interfaces/src/test/scala/com/intel/intelanalytics/domain/frame/ColumnSummaryStatisticsTest.scala
+++ interfaces/src/test/scala/com/intel/intelanalytics/domain/frame/ColumnSummaryStatisticsTest.scala
@@ -5,12 +5,12 @@ import org.scalatest.mock.MockitoSugar
 
 class ColumnSummaryStatisticsTest extends WordSpec with MockitoSugar {
 
-   "ColumnSummaryStatistics" should {
-     "not allow null FrameReference" in {
-       intercept[IllegalArgumentException] { ColumnSummaryStatistics(null, "dataColumn", None, None) }
-     }
-     "not allow null dataColumn" in {
-       intercept[IllegalArgumentException] { ColumnSummaryStatistics(mock[FrameReference], null, None, None) }
-     }
-   }
- }
+  "ColumnSummaryStatistics" should {
+    "not allow null FrameReference" in {
+      intercept[IllegalArgumentException] { ColumnSummaryStatistics(null, "dataColumn", None, None) }
+    }
+    "not allow null dataColumn" in {
+      intercept[IllegalArgumentException] { ColumnSummaryStatistics(mock[FrameReference], null, None, None) }
+    }
+  }
+}
