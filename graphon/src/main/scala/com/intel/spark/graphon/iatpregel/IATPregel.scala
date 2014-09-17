9bd414cc6ac49c9e370c1a0513ad00f6c8533838
nlsegerl <nathan.l.segerlind@intel.com>
2014-09-17 16:58:48 -0700

    TRIB-3475
    Cleaned up the IATPregel logging infrastructure.

--- graphon/src/main/scala/com/intel/spark/graphon/iatpregel/IATPregel.scala
+++ graphon/src/main/scala/com/intel/spark/graphon/iatpregel/IATPregel.scala
@@ -4,99 +4,35 @@ import scala.reflect.ClassTag
 
 import org.apache.spark.graphx._
 
-/**
- * Implements a Pregel-like bulk-synchronous message-passing API.
- *
- * Unlike the original Pregel API, the GraphX Pregel API factors the sendMessage computation over
- * edges, enables the message sending computation to read both vertex attributes, and constrains
- * messages to the graph structure.  These changes allow for substantially more efficient
- * distributed execution while also exposing greater flexibility for graph-based computation.
- *
- * @example We can use the Pregel abstraction to implement PageRank:
- *          {{{
- *           val pagerankGraph: Graph[Double, Double] = graph
- *             // Associate the degree with each vertex
- *             .outerJoinVertices(graph.outDegrees) {
- *               (vid, vdata, deg) => deg.getOrElse(0)
- *             }
- *             // Set the weight on the edges based on the degree
- *             .mapTriplets(e => 1.0 / e.srcAttr)
- *             // Set the vertex attributes to the initial pagerank values
- *             .mapVertices((id, attr) => 1.0)
- *
- *           def vertexProgram(id: VertexId, attr: Double, msgSum: Double): Double =
- *             resetProb + (1.0 - resetProb) * msgSum
- *           def sendMessage(id: VertexId, edge: EdgeTriplet[Double, Double]): Iterator[(VertexId, Double)] =
- *             Iterator((edge.dstId, edge.srcAttr * edge.attr))
- *           def messageCombiner(a: Double, b: Double): Double = a + b
- *           val initialMessage = 0.0
- *           // Execute Pregel for a fixed number of iterations.
- *           Pregel(pagerankGraph, initialMessage, numIter)(
- *             vertexProgram, sendMessage, messageCombiner)
- *          }}}
- *
- */
 object IATPregel {
 
   /**
-   * Execute a Pregel-like iterative vertex-parallel abstraction.  The
-   * user-defined vertex-program `vprog` is executed in parallel on
-   * each vertex receiving any inbound messages and computing a new
-   * value for the vertex.  The `sendMsg` function is then invoked on
-   * all out-edges and is used to compute an optional message to the
-   * destination vertex. The `mergeMsg` function is a commutative
-   * associative function used to combine messages destined to the
-   * same vertex.
+   * Implements Pregel-like BSP message passing.
+   * Based on the GraphX implementation of Pregel but with richer logging.
    *
-   * On the first iteration all vertices receive the `initialMsg` and
-   * on subsequent iterations if a vertex does not receive a message
-   * then the vertex-program is not invoked.
-   *
-   * This function iterates until there are no remaining messages, or
-   * for `maxIterations` iterations.
-   *
-   * @tparam VD the vertex data type
-   * @tparam ED the edge data type
-   * @tparam A the Pregel message type
-   *
-   * @param graph the input graph.
-   *
-   * @param initialMsg the message each vertex will receive at the on
-   *                   the first iteration
-   *
-   * @param maxIterations the maximum number of iterations to run for
-   *
-   * @param activeDirection the direction of edges incident to a vertex that received a message in
-   *                        the previous round on which to run `sendMsg`. For example, if this is `EdgeDirection.Out`, only
-   *                        out-edges of vertices that received a message in the previous round will run. The default is
-   *                        `EdgeDirection.Either`, which will run `sendMsg` on edges where either side received a message
-   *                        in the previous round. If this is `EdgeDirection.Both`, `sendMsg` will only run on edges where
-   *                        *both* vertices received a message.
-   *
-   * @param vprog the user-defined vertex program which runs on each
-   *              vertex and receives the inbound message and computes a new vertex
-   *              value.  On the first iteration the vertex program is invoked on
-   *              all vertices and is passed the default message.  On subsequent
-   *              iterations the vertex program is only invoked on those vertices
-   *              that receive messages.
-   *
-   * @param sendMsg a user supplied function that is applied to out
-   *                edges of vertices that received messages in the current
-   *                iteration
-   *
-   * @param mergeMsg a user supplied function that takes two incoming
-   *                 messages of type A and merges them into a single message of type
-   *                 A.  ''This function must be commutative and associative and
-   *                 ideally the size of A should not increase.''
-   *
-   * @return the resulting graph at the end of the computation
-   *
+   * @param graph
+   * @param initialMsg
+   * @param pregelLogger
+   * @param maxIterations
+   * @param activeDirection
+   * @param vprog
+   * @param sendMsg
+   * @param mergeMsg
+   * @tparam VertexData
+   * @tparam EdgeData
+   * @tparam Message
+   * @tparam VertexInitialState
+   * @tparam EdgeInitialState
+   * @tparam SuperStepState
+   * @return
    */
-  def apply[VD: ClassTag, ED: ClassTag, A: ClassTag, VI: ClassTag, EI: ClassTag, S: ClassTag]
-  (graph: Graph[VD, ED], initialMsg: A, pregelLogger: IATPregelLogger[VD, VI, ED, EI, S],
-   maxIterations: Int = Int.MaxValue,  activeDirection: EdgeDirection = EdgeDirection.Either)(vprog: (VertexId, VD, A) => VD,
-                                                          sendMsg: EdgeTriplet[VD, ED] => Iterator[(VertexId, A)],
-                                                          mergeMsg: (A, A) => A): (Graph[VD, ED], String) = {
+  def apply[VertexData: ClassTag, EdgeData: ClassTag, Message: ClassTag, VertexInitialState: ClassTag, EdgeInitialState: ClassTag, SuperStepState: ClassTag](graph: Graph[VertexData, EdgeData],
+                                                                                                                                                             initialMsg: Message,
+                                                                                                                                                             pregelLogger: IATPregelLogger[VertexData, VertexInitialState, EdgeData, EdgeInitialState, SuperStepState],
+                                                                                                                                                             maxIterations: Int = Int.MaxValue,
+                                                                                                                                                             activeDirection: EdgeDirection = EdgeDirection.Either)(vprog: (VertexId, VertexData, Message) => VertexData,
+                                                                                                                                                                                                                    sendMsg: EdgeTriplet[VertexData, EdgeData] => Iterator[(VertexId, Message)],
+                                                                                                                                                                                                                    mergeMsg: (Message, Message) => Message): (Graph[VertexData, EdgeData], String) = {
 
     val vertexDataToInitialStatus = pregelLogger.vertexDataToInitialStatus
     val vertexInitialStatusCombiner = pregelLogger.vertexInitialStatusCombiner
@@ -107,10 +43,9 @@ object IATPregel {
     val convertStateToStatus = pregelLogger.convertStateToStatus
     val generatePerStepReport = pregelLogger.generatePerStepReport
 
+    val vInitial = graph.vertices.map({ case (vid, vdata) => vertexDataToInitialStatus(vdata) }).reduce(vertexInitialStatusCombiner)
 
-    val vInitial = graph.vertices.map({ case (vid, vdata) => vertexDataToInitialStatus(vdata)}).reduce(vertexInitialStatusCombiner)
-
-    val eInitial = graph.edges.map({ case e: Edge[ED] => edgeDataToInitialStatus(e.attr)}).reduce(edgeInitialStatusCombiner)
+    val eInitial = graph.edges.map({ case e: Edge[EdgeData] => edgeDataToInitialStatus(e.attr) }).reduce(edgeInitialStatusCombiner)
 
     var log = new StringBuilder(generateInitialReport(vInitial, eInitial))
 
@@ -121,7 +56,7 @@ object IATPregel {
     var activeMessages = messages.count()
 
     // Loop
-    var prevG: Graph[VD, ED] = null
+    var prevG: Graph[VertexData, EdgeData] = null
     var i = 0
 
     while (activeMessages > 0 && i < maxIterations) {
@@ -129,7 +64,7 @@ object IATPregel {
       val newVerts = g.vertices.innerJoin(messages)(vprog).cache()
       // Update the graph with the new vertices.
       prevG = g
-      g = g.outerJoinVertices(newVerts) { (vid, old, newOpt) => newOpt.getOrElse(old)}
+      g = g.outerJoinVertices(newVerts) { (vid, old, newOpt) => newOpt.getOrElse(old) }
       g.cache()
 
       val oldMessages = messages
@@ -142,7 +77,7 @@ object IATPregel {
       // vertices of prevG (depended on by newVerts, oldMessages, and the vertices of g).
       activeMessages = messages.count()
 
-      log.++=(generatePerStepReport(g.vertices.map({ case (id, data) => convertStateToStatus(data)}).reduce(accumulateStepStatus), i))
+      log.++=(generatePerStepReport(g.vertices.map({ case (id, data) => convertStateToStatus(data) }).reduce(accumulateStepStatus), i))
 
       // Unpersist the RDDs hidden by newly-materialized RDDs
       oldMessages.unpersist(blocking = false)
