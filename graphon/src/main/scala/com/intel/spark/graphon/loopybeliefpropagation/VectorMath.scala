5b71b701f8b61d3f5d31f51a1a7ce003aa9a883e
nlsegerl <nathan.l.segerlind@intel.com>
2014-09-24 16:49:56 -0700

    TRIB-3475
    Adding comments.

--- graphon/src/main/scala/com/intel/spark/graphon/loopybeliefpropagation/VectorMath.scala
+++ graphon/src/main/scala/com/intel/spark/graphon/loopybeliefpropagation/VectorMath.scala
@@ -1,19 +1,51 @@
 package com.intel.spark.graphon.loopybeliefpropagation
 
+/**
+ * A library of routines for performing normed linear algebra with vectors represented as Vector[Double]
+ *
+
+ *
+ */
 object VectorMath {
 
+  /**
+   * Takes the natural logarithm of each component in the vector.
+   * @param v Incoming vector.
+   * @return Vector whose ith entry is the natural logarithm of the ith entry of v.
+   */
   def componentwiseLog(v: Vector[Double]): Vector[Double] = {
     v.map(Math.log(_))
   }
 
+  /**
+   * Creates a new vector whose ith component is e raised to the the ith component of the input.
+   *
+   * If  a component of the vector is Double.NegativeInfinity, the value in that component in the new vector is 0d
+   *
+   * @param v The input vector
+   * @return A new vector whose ith component is e raised to the the ith component of the input.
+   */
   def recoverFromComponentwiseLog(v: Vector[Double]) = {
     v.map({ case x: Double => if (x.isNegInfinity) 0 else Math.exp(x) })
   }
 
+  /**
+   * Take the per-component sum of two vectors.
+   * @param v1 First input vector.
+   * @param v2 Second input vector.
+   * @return The componentwise sum of the two vectors. If their lengths differ, the result is truncated to the length
+   *         of the shorter.
+   */
   def sum(v1: Vector[Double], v2: Vector[Double]): Vector[Double] = {
     v1.zip(v2).map({ case (x, y) => x + y })
   }
 
+  /**
+   * TODO - is this the right name for this thing???
+   * @param v1
+   * @param v2
+   * @return
+   */
   def overflowProtectedProduct(v1: Vector[Double], v2: Vector[Double]): Vector[Double] = {
     overflowProtectedProduct(List(v1, v2))
   }
