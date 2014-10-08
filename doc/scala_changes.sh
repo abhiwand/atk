24744e2760df4c85ee9114c8eb451c387ab7ee1d
rmaldrix <rmaldrix@intel.com>
2014-10-08 09:36:26 -0700

    changed to create subroutine

--- doc/scala_changes.sh
+++ doc/scala_changes.sh
@@ -11,25 +11,29 @@ w=~/commanddoc
 if test ! -d $w; then mkdir $w; fi
 
 # Now create a (hopefully) new file name
-export d=$w/$(date +%Y%m%d-%H%M).lst
+d=$w/$(date +%Y%m%d-%H%M).lst
+echo "echo $d" > scala_changes
+chmod +x scala_changes
 
 # If the file already exists, then it is too soon to check again.
 if test ! -e $d; then
-
-    echo $d
 
     # Get the name of the most recent listing file.
     ls -1 $w/*.lst 2>/dev/null > $d
     f=$(tail -n 1 $d)
 
     # Create a new listing file with the date of any scala file with python docstrings.
-    for g in $(grep -ril --include=*.scala $s ../); do echo $(date -r $g) $g; done > $d
+    echo "Current Listing;" > $d
+    for g in $(grep -ril --include=*.scala $s ../)
+        do
+            echo $(date -r $g) $g >> $d
+        done
 
     # Make sure there is a file to compare to
     if test "$f" != ""; then
 
         # If the current listing is different than the previous listing, we need to deal with it
-        diff $d $f
+        diff $d $f > /dev/null
         if test "$?" = "1"; then
 
             # Show the differences between the current listing and the previous one.
