6547b954e36d60d2ff888b83a6c85cbab8c56036
rmaldrix <rmaldrix@intel.com>
2014-10-15 16:54:26 -0700

    Small cleaning.

--- doc/scala_changes.sh
+++ doc/scala_changes.sh
@@ -8,15 +8,15 @@ s=CommandDoc\(oneLineSummary
 
 # First make sure the comparison directory exists. The data files will go here.
 w=~/commanddoc
-if test ! -d $w; then mkdir $w; fi
+if [ ! -d $w ]; then mkdir $w; fi
 
-# Now create a (hopefully) new file name
+# Now create a (hopefully) new file name and make it accessable as a variable
 d=$w/$(date +%Y%m%d-%H%M).lst
 echo "echo $d" > scala_changes
 chmod +x scala_changes
 
 # If the file already exists, then it is too soon to check again.
-if test ! -e $d; then
+if [ ! -e $d ]; then
 
     # Get the name of the most recent listing file.
     ls -1 $w/*.lst 2>/dev/null > $d
@@ -30,11 +30,11 @@ if test ! -e $d; then
         done
 
     # Make sure there is a file to compare to
-    if test "$f" != ""; then
+    if [ "$f" != "" ]; then
 
         # If the current listing is different than the previous listing, we need to deal with it
         diff $d $f > /dev/null
-        if test "$?" = "1"; then
+        if [ "$?" = "1" ]; then
 
             # Show the differences between the current listing and the previous one.
             vimdiff $d $f
