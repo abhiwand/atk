61c71231fd2b94c313676d1047520297081ad4c8
Bryn Keller <xoltar@xoltar.org>
2014-10-19 04:24:26 -0700

    checkpoint

--- engine-spark/src/main/scala/com/intel/intelanalytics/engine/spark/command/Dependencies.scala
+++ engine-spark/src/main/scala/com/intel/intelanalytics/engine/spark/command/Dependencies.scala
@@ -28,13 +28,12 @@ import com.intel.intelanalytics.domain._
 import com.intel.intelanalytics.engine.Reflection
 import com.intel.intelanalytics.engine.plugin.CommandPlugin
 import com.intel.intelanalytics.engine.spark.command.Typeful.Searchable
-import spray.json.{JsNumber, JsValue, JsString, JsObject}
+import spray.json.{ JsNumber, JsValue, JsString, JsObject }
 
-import scala.reflect.runtime.{universe => ru}
+import scala.reflect.runtime.{ universe => ru }
 import ru._
 import scalax.collection.Graph
 import scalax.collection.GraphEdge.DiEdge
-
 
 /**
  * Tools for retrieving dependencies of system entities such as frames and graphs,
@@ -50,7 +49,6 @@ object Dependencies {
    */
   type Computable = UriReference with OnDemand
 
-
   /**
    * Returns all the [[UriReference]] elements from a JsObject, which really means
    * extracting all the string values that are recognized as valid entity URIs.
@@ -63,7 +61,7 @@ object Dependencies {
   /**
    * Returns all the [[Computable]] objects from a case class
    */
-  def getComputables[A <: Product](arguments: A)(implicit s: Searchable[A, UriReference]) : Seq[Computable] = {
+  def getComputables[A <: Product](arguments: A)(implicit s: Searchable[A, UriReference]): Seq[Computable] = {
     val refs = arguments.deepFind((_: UriReference) => true)
     refs.flatMap {
       case ref: OnDemand => List(ref.asInstanceOf[Computable])
@@ -74,17 +72,16 @@ object Dependencies {
   /**
    * Helper method for the {{build}} method
    */
-  private def buildHelper(startWith: Seq[Computable], resolver: ReferenceResolver, accumulator : List[Set[Command]])
-  : List[Set[Command]] = {
+  private def buildHelper(startWith: Seq[Computable], resolver: ReferenceResolver, accumulator: List[Set[Command]]): List[Set[Command]] = {
     val unfulfilled = startWith.filter(obj => obj.computeStatus != Complete)
     val commands = unfulfilled.map(obj => obj.command()).toSet
     val deps = commands.map(c => c.arguments.get)
       .flatMap(getUriReferencesFromJsObject)
       .toSet
       .map((ref: UriReference) => {
-      val management: EntityManagement = ref.entity.asInstanceOf[EntityManagement]
-      management.getMetaData(ref.asInstanceOf[management.type#Reference]).asInstanceOf[Computable]
-    })
+        val management: EntityManagement = ref.entity.asInstanceOf[EntityManagement]
+        management.getMetaData(ref.asInstanceOf[management.type#Reference]).asInstanceOf[Computable]
+      })
     buildHelper(deps.toSeq, resolver, commands :: accumulator)
   }
 
