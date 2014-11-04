03c58b00d189bb283b195fb049a0b4a8a063050e
Todd Lisonbee <todd.lisonbee@intel.com>
2014-11-04 06:10:54 -0800

    TRIB-4021 Fixing bug with create_missing_vertices=True

--- engine-spark/src/main/scala/com/intel/intelanalytics/engine/spark/graph/plugins/AddEdgesPlugin.scala
+++ engine-spark/src/main/scala/com/intel/intelanalytics/engine/spark/graph/plugins/AddEdgesPlugin.scala
@@ -146,7 +146,7 @@ class AddEdgesPlugin(addVerticesPlugin: AddVerticesPlugin) extends SparkCommandP
       val sourceVertexData = edgesWithoutVids.selectColumns(List(arguments.columnNameForSourceVertexId))
       val destVertexData = edgesWithoutVids.selectColumns(List(arguments.columnNameForDestVertexId))
       addVerticesPlugin.addVertices(ctx, AddVertices(graph.vertexMeta(srcLabel).frameReference, null, arguments.columnNameForSourceVertexId), sourceVertexData, preferNewVertexData = false)
-      addVerticesPlugin.addVertices(ctx, AddVertices(graph.vertexMeta(destLabel).frameReference, null, arguments.columnNameForSourceVertexId), destVertexData, preferNewVertexData = false)
+      addVerticesPlugin.addVertices(ctx, AddVertices(graph.vertexMeta(destLabel).frameReference, null, arguments.columnNameForDestVertexId), destVertexData, preferNewVertexData = false)
     }
 
     // load src and dest vertex ids
