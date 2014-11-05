2552ae83ec120167e92da281fa31b3429ad738bb
spkavuly <soila.p.kavulya@intel.com>
2014-11-04 16:14:42 -0800

    TRIB-3997 Updated tests and splitting criteria for Titan/HBase splitter

--- graphbuilder-3/src/main/java/com/intel/graphbuilder/io/HBaseUniformSplitter.java
+++ graphbuilder-3/src/main/java/com/intel/graphbuilder/io/HBaseUniformSplitter.java
@@ -6,13 +6,12 @@ import org.apache.hadoop.hbase.mapreduce.TableSplit;
 import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.hadoop.mapreduce.InputSplit;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Split HBase regions uniformly.
- *
+ * <p/>
  * HBase's default TableInputFormat class assigns a single mapper per region. This class uniformly
  * splits HBase regions based on a user-defined split count.
  */
@@ -21,11 +20,11 @@ public class HBaseUniformSplitter {
     //Splitting the last region is not straight-forward because the last row key in a HBase table is null
     //Assumes that the same maximum row key as the uniform splitter in org.apache.hadoop.hbase.util.RegionSplitter
     public static final byte xFF = (byte) 0xFF;
-    public static final byte[] maxRowKeyBytes = new byte[] {xFF, xFF, xFF, xFF, xFF, xFF, xFF, xFF};
+    public static final byte[] maxRowKeyBytes = new byte[]{xFF, xFF, xFF, xFF, xFF, xFF, xFF, xFF};
 
     private final List<InputSplit> initialSplits;
 
-    public HBaseUniformSplitter (List<InputSplit> initialSplits) {
+    public HBaseUniformSplitter(List<InputSplit> initialSplits) {
         this.initialSplits = initialSplits;
     }
 
@@ -33,10 +32,10 @@ public class HBaseUniformSplitter {
     /**
      * Create uniform input splits.
      *
-     * @param requestedRegionCount  Requested region count.
+     * @param requestedRegionCount Requested region count.
      * @return List of uniformly split regions
      */
-    public List<InputSplit> createInputSplits(int requestedRegionCount)  {
+    public List<InputSplit> createInputSplits(int requestedRegionCount) {
         int initialRegionCount = initialSplits.size();
         int splitsPerRegion = calculateSplitsPerRegion(requestedRegionCount, initialRegionCount);
 
@@ -53,11 +52,11 @@ public class HBaseUniformSplitter {
     /**
      * Split a single region into multiple input splits that server as inputs to mappers
      *
-     * @param inputSplit    Input region split
+     * @param inputSplit      Input region split
      * @param splitsPerRegion Number of splits per region
      * @return New region split
-    */
-    private List<InputSplit> splitRegion(TableSplit inputSplit, int splitsPerRegion)  {
+     */
+    private List<InputSplit> splitRegion(TableSplit inputSplit, int splitsPerRegion) {
 
         byte[] startKey = inputSplit.getStartRow();
         byte[] endKey = inputSplit.getEndRow();
@@ -67,23 +66,32 @@ public class HBaseUniformSplitter {
         if (!Bytes.equals(endKey, HConstants.EMPTY_BYTE_ARRAY)) {
             regionSplits = createUniformSplits(inputSplit, startKey, endKey, splitsPerRegion);
         } else {
-            regionSplits = createUniformSplits(inputSplit, startKey, maxRowKeyBytes, splitsPerRegion);
+            regionSplits = createUniformSplits(inputSplit, startKey, getLastRegionKey(startKey), splitsPerRegion);
+
+            if (regionSplits.size() > 0) {
+                int lastSplitIdx = regionSplits.size() - 1;
+                TableSplit lastSplit = (TableSplit) regionSplits.get(lastSplitIdx);
+                TableSplit newLastSplit = new TableSplit(lastSplit.getTable(), lastSplit.getStartRow(),
+                        HConstants.EMPTY_BYTE_ARRAY, lastSplit.getRegionLocation());
+                regionSplits.set(lastSplitIdx, newLastSplit);
+            }
+
         }
 
         return regionSplits;
     }
 
     /**
-     *  Uniformly splits region based on start and end keys.
+     * Uniformly splits region based on start and end keys.
      *
-     * @param initialSplit  Initial region split
-     * @param startKey Start key of split
-     * @param endKey  End key of splits
+     * @param initialSplit    Initial region split
+     * @param startKey        Start key of split
+     * @param endKey          End key of splits
      * @param splitsPerRegion Number of splits to create
      * @return Uniform splits for region
      */
     private List<InputSplit> createUniformSplits(TableSplit initialSplit, byte[] startKey, byte[] endKey,
-                                                 int splitsPerRegion)  {
+                                                 int splitsPerRegion) {
         List<InputSplit> splits = new ArrayList<>();
 
         byte[][] splitKeys = getSplitKeys(startKey, endKey, splitsPerRegion);
@@ -105,12 +113,11 @@ public class HBaseUniformSplitter {
     }
 
 
-
     /**
      * Calculates the number of splits per region based on the requested number of region splits.
      *
-     * @param  requestedRegionCount Requested region count
-     * @param  initialRegionCount Initial region count
+     * @param requestedRegionCount Requested region count
+     * @param initialRegionCount   Initial region count
      * @return Number of splits/region = max(requestedRegionCount/initialRegionCount, 1)
      */
     private int calculateSplitsPerRegion(int requestedRegionCount, int initialRegionCount) {
@@ -118,7 +125,7 @@ public class HBaseUniformSplitter {
         int splitsPerRegion = 1;
 
         if (initialRegionCount > 0 && requestedRegionCount > initialRegionCount) {
-            splitsPerRegion = (int) Math.ceil(requestedRegionCount/initialRegionCount);
+            splitsPerRegion = (int) Math.ceil(requestedRegionCount / initialRegionCount);
         }
 
         return splitsPerRegion;
@@ -127,8 +134,8 @@ public class HBaseUniformSplitter {
     /**
      * Get split keys based on start and end keys, and requested number of splits.
      *
-     * @param startKey Start key of split
-     * @param endKey End key of splits
+     * @param startKey        Start key of split
+     * @param endKey          End key of splits
      * @param splitsPerRegion Number of splits per region
      * @return Split keys for range
      */
@@ -138,11 +145,42 @@ public class HBaseUniformSplitter {
             try {
                 //Bytes.split() creates X+1 splits. If you want to split the range into Y, specify Y-1.
                 splitKeys = Bytes.split(startKey, endKey, true, (splitsPerRegion - 1));
-            }
-            catch (IllegalArgumentException e) {
+            } catch (IllegalArgumentException e) {
                 Log.warn("Exception while getting split keys:" + e);
             }
         }
         return splitKeys;
     }
+
+    /**
+     * @param startKey
+     * @return
+     * @see com.thinkaurelius.titan.diskstorage.hbase.HBaseStoreManager
+     */
+    public byte[] getLastRegionKey(byte[] startKey) {
+
+        // Compute end key using pre-split formula in
+        byte[] endKey = Bytes.toBytes(((1L << 32) - 1L));
+        long maxRange = 0;
+
+        for (InputSplit split : initialSplits) {
+            TableSplit tableSplit = (TableSplit) split;
+            try {
+                long regionRange = Bytes.toLong(tableSplit.getEndRow()) - Bytes.toLong(tableSplit.getStartRow());
+                maxRange = java.lang.Math.max(maxRange, regionRange);
+            }
+            catch (IllegalArgumentException e) {}
+        }
+
+        if (maxRange > 0) {
+            endKey = Bytes.toBytes(Bytes.toLong(startKey) + maxRange);
+        }
+        else if (!Bytes.equals(startKey, HConstants.EMPTY_BYTE_ARRAY)) {
+            if (Bytes.compareTo(startKey, endKey) > 0) { //Start key > end key
+                endKey = maxRowKeyBytes;
+            }
+        }
+        return (endKey);
+    }
+
 }
---
+++ graphbuilder-3/src/test/scala/com/intel/graphbuilder/graph/titan/TitanAutoPartitionerITest.scala
@@ -0,0 +1,102 @@
+package com.intel.graphbuilder.graph.titan
+
+import com.intel.graphbuilder.io.HBaseTableInputFormat
+import org.apache.commons.configuration.BaseConfiguration
+import org.apache.hadoop.hbase.client.HBaseAdmin
+import org.apache.hadoop.hbase.{ HRegionInfo, TableName, ClusterStatus, HBaseConfiguration }
+import org.apache.spark.{ SparkConf, SparkContext }
+import org.mockito.Mockito._
+import org.scalatest.mock.MockitoSugar
+import org.scalatest._
+
+class TitanAutoPartitionerITest extends FlatSpec with Matchers with MockitoSugar with BeforeAndAfterEach {
+  val hBaseTableName = "testtable"
+  val hBaseRegionServers = 3
+  val hBaseTableRegions = 5
+
+  val hBaseAdminMock = mock[HBaseAdmin]
+  val clusterStatusMock = mock[ClusterStatus]
+  val tableRegionsMock = mock[java.util.List[HRegionInfo]]
+
+  override def beforeEach() = {
+    val hBaseConfig = HBaseConfiguration.create()
+
+    when(hBaseAdminMock.getConfiguration).thenReturn(hBaseConfig)
+
+    //Mock number of HBase region servers
+    when(hBaseAdminMock.getClusterStatus()).thenReturn(clusterStatusMock)
+    when(hBaseAdminMock.getClusterStatus.getServersSize).thenReturn(hBaseRegionServers)
+
+    //Mock number of regions in HBase table
+    when(hBaseAdminMock.getTableRegions(TableName.valueOf(hBaseTableName))).thenReturn(tableRegionsMock)
+    when(hBaseAdminMock.getTableRegions(TableName.valueOf(hBaseTableName)).size()).thenReturn(hBaseTableRegions)
+  }
+
+  "enableAutoPartition" should "return true when auto-partitioner is enabled" in {
+    val titanConfig = new BaseConfiguration()
+    titanConfig.setProperty(TitanAutoPartitioner.ENABLE_AUTO_PARTITION, "true")
+
+    val titanAutoPartitioner = new TitanAutoPartitioner(titanConfig)
+    titanAutoPartitioner.enableAutoPartition shouldBe (true)
+  }
+
+  "enableAutoPartition" should "return false when auto-partitioner is disabled" in {
+    val titanConfig = new BaseConfiguration()
+    titanConfig.setProperty(TitanAutoPartitioner.ENABLE_AUTO_PARTITION, "false")
+
+    val titanAutoPartitioner = new TitanAutoPartitioner(titanConfig)
+    titanAutoPartitioner.enableAutoPartition shouldBe (false)
+  }
+
+  "setHBasePreSplits" should "set HBase pre-splits for graph construction based on available region servers" in {
+    val titanConfig = new BaseConfiguration()
+    titanConfig.setProperty(TitanAutoPartitioner.ENABLE_AUTO_PARTITION, "true")
+    titanConfig.setProperty(TitanAutoPartitioner.HBASE_REGIONS_PER_SERVER, "6")
+
+    val titanAutoPartitioner = TitanAutoPartitioner(titanConfig)
+    val newTitanConfig = titanAutoPartitioner.setHBasePreSplits(hBaseAdminMock)
+
+    val expectedRegionCount = hBaseRegionServers * 6
+    newTitanConfig.getProperty(TitanAutoPartitioner.TITAN_HBASE_REGION_COUNT) shouldBe (expectedRegionCount)
+  }
+  "setHBasePreSplits" should "not set HBase pre-splits if regions are less than Titan's minimum region count" in {
+    val titanConfig = new BaseConfiguration()
+    titanConfig.setProperty(TitanAutoPartitioner.ENABLE_AUTO_PARTITION, "true")
+    titanConfig.setProperty(TitanAutoPartitioner.HBASE_REGIONS_PER_SERVER, "0")
+
+    val titanAutoPartitioner = TitanAutoPartitioner(titanConfig)
+    val newTitanConfig = titanAutoPartitioner.setHBasePreSplits(hBaseAdminMock)
+    newTitanConfig.getProperty(TitanAutoPartitioner.TITAN_HBASE_REGION_COUNT) shouldBe (null)
+  }
+  "setHBaseInputSplits" should "set input splits for Titan/HBase reader to spark.cores.max" in {
+    val sparkConfig = new SparkConf()
+    val sparkContextMock = mock[SparkContext]
+    sparkConfig.set(TitanAutoPartitioner.SPARK_MAX_CORES, "20")
+    when(sparkContextMock.getConf).thenReturn(sparkConfig)
+
+    val titanConfig = new BaseConfiguration()
+    titanConfig.setProperty(TitanAutoPartitioner.ENABLE_AUTO_PARTITION, "true")
+
+    val titanAutoPartitioner = TitanAutoPartitioner(titanConfig)
+    titanAutoPartitioner.setHBaseInputSplits(sparkContextMock, hBaseAdminMock, hBaseTableName)
+
+    hBaseAdminMock.getConfiguration.getInt(HBaseTableInputFormat.NUM_REGION_SPLITS, -1) shouldBe (20)
+  }
+  "setHBaseInputSplits" should "set input splits for Titan/HBase reader to estimated Spark cores when spark.cores.max not specified" in {
+    val sparkContext = mock[SparkContext]
+    val sparkConfig = new SparkConf()
+    when(sparkContext.getConf).thenReturn(sparkConfig)
+
+    val titanConfig = new BaseConfiguration()
+    titanConfig.setProperty(TitanAutoPartitioner.ENABLE_AUTO_PARTITION, "true")
+    titanConfig.setProperty(TitanAutoPartitioner.HBASE_INPUT_SPLITS_PER_CORE, "4")
+
+    val titanAutoPartitioner = TitanAutoPartitioner(titanConfig)
+    titanAutoPartitioner.setHBaseInputSplits(sparkContext, hBaseAdminMock, hBaseTableName)
+
+    val numSparkWorkers = hBaseRegionServers //HBase region servers used to estimate Spark workers
+    val expectedHBaseSplits = Runtime.getRuntime.availableProcessors() * numSparkWorkers * 4
+    hBaseAdminMock.getConfiguration.getInt(HBaseTableInputFormat.NUM_REGION_SPLITS, -1) shouldBe (expectedHBaseSplits)
+  }
+
+}
