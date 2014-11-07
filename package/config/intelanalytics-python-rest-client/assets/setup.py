d80f9ed3dec142770f4c776993b6c952e71d7f32
rene.o.dorado@intel.com <rene.o.dorado@intel.com>
2014-11-06 16:11:37 -0800

    TRIB-3507 Add pypi package boilerplate files

---
+++ package/config/intelanalytics-python-rest-client/assets/setup.py
@@ -0,0 +1,38 @@
+from distutils.core import setup
+
+setup(
+    # Application name:
+    name="intelanalytics",
+
+    # Version number (initial):
+    version=u"0.8.8",
+
+    # Application author details:
+    author="Intel",
+    author_email="bleh@intel.com",
+
+    # Packages
+    packages=["intelanalytics","intelanalytics/core","intelanalytics/rest","intelanalytics/tests"],
+
+    # Include additional files into the package
+    include_package_data=True,
+
+    # Details
+    url="https://analyticstoolkit.intel.com",
+
+    #
+    # license="LICENSE.txt",
+    description="Useful towel-related stuff.",
+
+    long_description=open("README").read(),
+
+    # Dependent packages (distributions)
+    install_requires=[
+        'bottle>=0.12',
+        'numpy>=1.8.1',
+        'requests>=2.2.1',
+        'ordereddict>=1.1',
+        'decorator>=3.4.0',
+        'pandas>=0.15.0',
+    ],
+)
