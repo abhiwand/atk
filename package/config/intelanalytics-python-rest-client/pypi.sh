7c343382bf497cc39ae66440dbe54ec53255c57d
rene.o.dorado@intel.com <rene.o.dorado@intel.com>
2014-11-06 16:11:37 -0800

    TRIB-3507 Add pypi package boilerplate files

---
+++ package/config/intelanalytics-python-rest-client/pypi.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+source common.sh
+
+log "Build pypi package"
+
+packageName=$1
+tarFile=$2
+TAR_FILE=$tarFile
+version=$3
+
+log "package name: $packageName, tar file: $tarFile, version: $version, script path: $SCRIPTPATH"
+
