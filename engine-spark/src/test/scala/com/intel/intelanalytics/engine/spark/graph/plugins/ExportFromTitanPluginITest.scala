26a18f7b4de2fee6617513a9ad056c0291d7f43a
schen55 <steven.shih-chi.chen@intel.com>
2014-11-14 17:05:44 -0800

    fix test error

--- engine-spark/src/test/scala/com/intel/intelanalytics/engine/spark/graph/plugins/ExportFromTitanPluginITest.scala
+++ engine-spark/src/test/scala/com/intel/intelanalytics/engine/spark/graph/plugins/ExportFromTitanPluginITest.scala
@@ -5,7 +5,8 @@ import com.intel.intelanalytics.engine.spark.graph.SparkGraphStorage
 
 import org.scalatest.mock.MockitoSugar
 import org.mockito.Mockito._
-import com.intel.intelanalytics.domain.schema.VertexSchema
+import com.intel.intelanalytics.domain.schema.{Column, VertexSchema}
+import com.intel.intelanalytics.domain.schema.DataTypes.{string, int64}
 
 class ExportFromTitanPluginITest extends FlatSpec with Matchers with MockitoSugar {
   "createVertexFrames" should "vertex frame by label" in {
@@ -13,7 +14,7 @@ class ExportFromTitanPluginITest extends FlatSpec with Matchers with MockitoSuga
     val graphId = 1
     ExportFromTitanPlugin.createVertexFrames(graphs, graphId, List("user", "movie"))
 
-    verify(graphs).defineVertexType(graphId, VertexSchema(label = "user", idColumnName = None))
-    verify(graphs).defineVertexType(graphId, VertexSchema(label = "movie", idColumnName = None))
+    verify(graphs).defineVertexType(graphId, VertexSchema(List(Column("_vid", int64), Column("_label", string)), label = "user", idColumnName = None))
+    verify(graphs).defineVertexType(graphId, VertexSchema(List(Column("_vid", int64), Column("_label", string)), label = "movie", idColumnName = None))
   }
 }
