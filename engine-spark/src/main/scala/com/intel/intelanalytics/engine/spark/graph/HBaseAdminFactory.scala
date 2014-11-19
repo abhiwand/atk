224076462122e810c3cc9566480d9dbb1b7cab92
spkavuly <soila.p.kavulya@intel.com>
2014-11-18 17:20:05 -0800

    TRIB-4086 Attempt at fixing HBase drop graph error by adding hbase-client jar to pom and using HBaseConfiguration.create to create HBase config

--- engine-spark/src/main/scala/com/intel/intelanalytics/engine/spark/graph/HBaseAdminFactory.scala
+++ engine-spark/src/main/scala/com/intel/intelanalytics/engine/spark/graph/HBaseAdminFactory.scala
@@ -15,12 +15,7 @@ class HBaseAdminFactory {
    * HBaseAdmin should not be re-used forever: you should create, use, throw away - and then get another one next time
    */
   def createHBaseAdmin(): HBaseAdmin = {
-    val config = new Configuration()
-    // for some reason HBaseConfiguration wasn't picking up hbase-default.xml automatically, so manually adding here
-    val hBaseDefault = this.getClass.getClassLoader.getResourceAsStream("hbase-default.xml")
-    val hBaseSite = this.getClass.getClassLoader.getResourceAsStream("hbase-site.xml")
-    config.addResource(hBaseDefault)
-    config.addResource(hBaseSite)
+    val hBaseConfig = HBaseConfiguration.create()
     new HBaseAdmin(HBaseConfiguration.addHbaseResources(config))
   }
 
