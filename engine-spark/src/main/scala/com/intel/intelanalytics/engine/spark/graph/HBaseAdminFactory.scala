d9dd228016f4c36b29f8fbd1bd13f0b1f2e465e4
spkavuly <soila.p.kavulya@intel.com>
2014-11-18 18:08:02 -0800

    TRIB-4086 Reverting back changes in HBaseAdminFactory

--- engine-spark/src/main/scala/com/intel/intelanalytics/engine/spark/graph/HBaseAdminFactory.scala
+++ engine-spark/src/main/scala/com/intel/intelanalytics/engine/spark/graph/HBaseAdminFactory.scala
@@ -15,8 +15,13 @@ class HBaseAdminFactory {
    * HBaseAdmin should not be re-used forever: you should create, use, throw away - and then get another one next time
    */
   def createHBaseAdmin(): HBaseAdmin = {
-    val hBaseConfig = HBaseConfiguration.create()
-    new HBaseAdmin(HBaseConfiguration.addHbaseResources(hBaseConfig))
+    val config = new Configuration()
+    // for some reason HBaseConfiguration wasn't picking up hbase-default.xml automatically, so manually adding here
+    val hBaseDefault = this.getClass.getClassLoader.getResourceAsStream("hbase-default.xml")
+    val hBaseSite = this.getClass.getClassLoader.getResourceAsStream("hbase-site.xml")
+    config.addResource(hBaseDefault)
+    config.addResource(hBaseSite)
+    new HBaseAdmin(HBaseConfiguration.addHbaseResources(config))
   }
 
 }
