0c6ad4c2baefd60477386dfb87d5f66a7ab65b3e
Suraj Bathija <suraj.p.bathija@intel.com>
2014-11-19 15:18:05 -0800

    covariance

--- engine-spark/src/main/scala/com/intel/intelanalytics/engine/spark/frame/plugins/statistics/covariance/CovarianceFunctions.scala
+++ engine-spark/src/main/scala/com/intel/intelanalytics/engine/spark/frame/plugins/statistics/covariance/CovarianceFunctions.scala
@@ -1,10 +1,14 @@
 package com.intel.intelanalytics.engine.spark.frame.plugins.statistics.covariance
 
 
+import com.intel.intelanalytics.domain.schema.DataTypes
+import com.intel.intelanalytics.domain.schema.DataTypes.DataType
 import com.intel.intelanalytics.domain.schema.DataTypes.DataType
 import com.intel.intelanalytics.engine.Rows.Row
 import org.apache.spark.mllib.linalg.Vector
+import org.apache.spark.mllib.linalg.distributed.RowMatrix
 import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.catalyst.types.DataType
 
 object Covariance extends Serializable {
 
@@ -25,7 +29,7 @@ object Covariance extends Serializable {
     10000 / 5
   }
 
-   /*def covarianceMatrix(rdd: RDD[Row],
+   def covarianceMatrix(rdd: RDD[Row],
                        dataColumnIndicesAndTypes: Seq[(Int, DataType)],
                        weightColumnIndicesAndTypes: Option[Seq[(Int, DataType)]],
                        usePopulationVariance: Boolean,
@@ -33,13 +37,18 @@ object Covariance extends Serializable {
 
     val n = rdd.first().size
 
+     val dataRDD: RDD[Vector] = rdd.map(_).to[Vector]
+
+
     /*val (m, mean) = rdd.aggregate[(Long, Array)]((0L, Array(n) = 0.0)
-      ( (s: (Long, Array), v: RDD[Row]) => (s._1 + 1L, s._2 += v.toArray()),
-      (s1: (Long, Array), s2: (Long, Array)) =>  (s1._1 + s2._1, s1._2 += s2._2)
+      ( (s: (Long, Array), v: RDD[Row]) => (s._1 + 1L, s._2[0] += v[]),
+        (s1: (Long, Array), s2: (Long, Array)) =>  (s1._1 + s2._1, s1._2 += s2._2)
     ))*/
+
+
 
 
    // rdd.sparkContext.parallelize(covMatrix)
   }
-*/
+
 }
