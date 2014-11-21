4d6928d49ed37df6e587d7428dec9d445b20f236
rene.o.dorado@intel.com <rene.o.dorado@intel.com>
2014-11-21 14:01:12 -0800

    TRIB-3507 add service sdl for parcel and update control shell

--- package/config/intelanalytics/csd/scripts/control.sh
+++ package/config/intelanalytics/csd/scripts/control.sh
@@ -1,26 +1,34 @@
 #!/bin/bash
 
+env
+
+function log {
+timestamp=$(date)
+echo "$timestamp: $1" #stdout
+echo "$timestamp: $1" 1>&2; #stderr
+}
 
 
+#exec >>/var/log/intelanalytics/rest-server/output.log 2>&1
 case "$1" in
   start)
-    echo "restart intelanalytics start"
-	service intelanalytics start
+    log "restart intelanalytics start"
+    exec su -c "java $IA_JVM_OPT -cp $CLASSPATH com.intel.intelanalytics.component.Boot api-server com.intel.intelanalytics.service.ApiServiceApplication" $IAUSER
+
 	;;
 #  stop)
 #    echo "restart intelanalytics stop"
 #	service intelanalytics stop
 # ;;
-  restart)
-    echo "restart intelanalytics server"
-	service intelanalytics restart
-	;;
+#  restart)
+#    echo "restart intelanalytics server"
+#	service intelanalytics restart
+#	;;
   *)
 	log "Don't understand [$1]"
 	exit 2
 esac
 
 
-exec >>/var/log/intelanalytics/rest-server/output.log 2>&1
+
   #or log by piping to logger
-exec su -c "java $IA_JVM_OPT -cp $CLASSPATH com.intel.intelanalytics.component.Boot api-server com.intel.intelanalytics.service.ApiServiceApplication" $IAUSER
