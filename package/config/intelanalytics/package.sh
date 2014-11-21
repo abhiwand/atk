d5abd9fbb884d9903e400c0386ed5f31656d7739
rene.o.dorado@intel.com <rene.o.dorado@intel.com>
2014-11-21 10:52:13 -0800

    TRIB-3019 start creating parcel and csd packages.

---
+++ package/config/intelanalytics/package.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+workDir=$(pwd)
+baseDir=${workDir##*/}
+gitRoot="."
+pwd
+$baseDir
+
+if [ "$baseDir" == "package" ]; then
+	source common.sh
+	gitRoot=".."
+	else
+	source package/common.sh
+	gitRoot="."
+fi
+echo $gitRoot
+
+packageName=$1
+VERSION=$VERSION
+BUILD_DIR=$BUILD_DIR
+
+echo $packageName
+echo $VERSION
+echo $BUILD_DIR
+
+pwd
+echo $SCRIPTPATH
+
+log "packageName: $packageName"
+#call package.sh for rest-server
+package intelanalytics-rest-server
+packageName=$1
+
+log "packageName: $packageName"
+#call package.sh for rest server
+package intelanalytics-python-rest-client
+packageName=$1
+
+log "packageName: $packageName"
+#call package.sh for spark-deps
+package intelanalytics-spark-deps
+packageName=$1
+
+
+#call package.sh for client
+log "packageName: $packageName"
+createArchive $packageName
---
+++ package/config/intelanalytics/parcel/meta/IA_ENV.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "IA env sh"
+
+export IA_ENV="BLEH"
