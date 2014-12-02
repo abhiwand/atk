2e62a0add312f4db16b93f2b27cd5a1cd17bc404
Todd Lisonbee <todd.lisonbee@intel.com>
2014-12-02 01:20:27 -0800

    TRIB-4133 switching from setJars() to addJar()

--- engine-spark/src/main/scala/com/intel/intelanalytics/engine/spark/context/SparkContextFactory.scala
+++ engine-spark/src/main/scala/com/intel/intelanalytics/engine/spark/context/SparkContextFactory.scala
@@ -44,7 +44,6 @@ trait SparkContextFactory extends EventLogging {
       .setMaster(SparkEngineConfig.sparkMaster)
       .setSparkHome(SparkEngineConfig.sparkHome)
       .setAppName(s"intel-analytics:$user:$description")
-      .setJars(Seq(jarPath("engine-spark")))
 
     sparkConf.setAll(SparkEngineConfig.sparkConfProperties)
 
@@ -55,8 +54,11 @@ trait SparkContextFactory extends EventLogging {
 
     info("SparkConf settings: " + sparkConf.toDebugString)
 
-    new SparkContext(sparkConf)
+    val sparkContext = new SparkContext(sparkConf)
+    sparkContext.addJar(jarPath("engine-spark"))
+    sparkContext
   }
+
   /**
    * Creates a new sparkContext
    */
