ba5c37334f4fe190f47ba470b92d2c89b6745ccb
Todd Lisonbee <todd.lisonbee@intel.com>
2014-12-02 06:16:26 -0800

    Adding tests

--- interfaces/src/test/scala/com/intel/intelanalytics/domain/SchemaTest.scala
+++ interfaces/src/test/scala/com/intel/intelanalytics/domain/SchemaTest.scala
@@ -2,14 +2,14 @@ package com.intel.intelanalytics.domain
 
 import org.scalatest.{ WordSpec, Matchers, FlatSpec }
 import com.intel.intelanalytics.domain.schema.DataTypes._
-import com.intel.intelanalytics.domain.schema.{ FrameSchema, Column, Schema }
+import com.intel.intelanalytics.domain.schema._
 
 class SchemaTest extends WordSpec with Matchers {
 
   val columns = List(Column("a", int64), Column("b", float32), Column("c", string))
   val abcSchema = new FrameSchema(columns)
 
-  "Schema" should {
+  "FrameSchema" should {
     "find correct column index for single column" in {
       abcSchema.columnIndex("a") shouldBe 0
       abcSchema.columnIndex("b") shouldBe 1
@@ -138,6 +138,306 @@ class SchemaTest extends WordSpec with Matchers {
       assert(schema.column("a").index == 0)
       assert(schema.column(1).name == "c")
     }
+
+    "not allow renaming to duplicate column name" in {
+      intercept[IllegalArgumentException] {
+        abcSchema.renameColumn("c", "a")
+      }
+    }
+  }
+
+  val vertexColumns = List(Column("_vid", int64), Column("_label", str), Column("movie_id", int64), Column("name", str))
+  val vertexSchema = VertexSchema(vertexColumns, "movies", Some("movie_id"))
+
+  "VertexSchema" should {
+
+    "find correct column index for single column" in {
+      vertexSchema.columnIndex("_vid") shouldBe 0
+      vertexSchema.columnIndex("_label") shouldBe 1
+      vertexSchema.columnIndex("movie_id") shouldBe 2
+    }
+
+    "find correct column index for two column" in {
+      vertexSchema.columnIndices(Seq("_vid", "_label")) shouldBe List(0, 1)
+      vertexSchema.columnIndices(Seq("_vid", "movie_id")) shouldBe List(0, 2)
+    }
+
+    "find correct column index for all columns when input columns is empty" in {
+      vertexSchema.columnIndices(Seq()) shouldBe List(0, 1, 2, 3)
+    }
+
+    "be able to report column data types for first column" in {
+      vertexSchema.columnDataType("_vid") shouldBe int64
+    }
+
+    "be able to report column data types for last column" in {
+      vertexSchema.columnDataType("name") shouldBe string
+    }
+
+    "be able to add columns" in {
+      val added = vertexSchema.addColumn("str", string)
+      added.columns.length shouldBe 5
+      added.column("str").dataType shouldBe string
+      added.column("str").index shouldBe 4
+    }
+
+    "be able to validate a column has a given type" in {
+      vertexSchema.hasColumnWithType("_vid", int64) shouldBe true
+      vertexSchema.hasColumnWithType("_vid", string) shouldBe false
+
+    }
+
+    def testDropColumn(columnName: String): Unit = {
+      val result = vertexSchema.dropColumn(columnName)
+      assert(result.columns.length == 2, "length was not 2: " + result)
+      assert(!result.hasColumn(columnName), "column was still present: " + result)
+    }
+
+    "not be able to drop _vid column" in {
+      intercept[IllegalArgumentException] {
+        testDropColumn("_vid")
+      }
+    }
+
+    "not be able to drop _label column" in {
+      intercept[IllegalArgumentException] {
+        testDropColumn("_label")
+      }
+    }
+
+    "not be able to drop movie_id column" in {
+      intercept[IllegalArgumentException] {
+        testDropColumn("movie_id")
+      }
+    }
+
+    "be able to drop multiple columns" in {
+      val result = vertexSchema.dropColumns(List("name"))
+      assert(result.columns.length == 3)
+      assert(result.hasColumn("_vid"))
+      assert(result.hasColumn("_label"))
+      assert(result.hasColumn("movie_id"))
+    }
+
+    "be able to copy a subset of columns" in {
+      vertexSchema.copySubset(Seq("_vid", "_label", "movie_id")).columns.length shouldBe 3
+    }
+
+    "be able to rename columns" in {
+      val renamed = vertexSchema.renameColumn("name", "title")
+      renamed.hasColumn("name") shouldBe false
+      renamed.hasColumn("title") shouldBe true
+      renamed.columnNames shouldBe List("_vid", "_label", "movie_id", "title")
+    }
+
+    // TODO: fix this in Schema
+    "be able to rename movie_id column" ignore {
+      val renamed = vertexSchema.renameColumn("movie_id", "m_id")
+      renamed.hasColumn("movie_id") shouldBe false
+      renamed.hasColumn("m_id") shouldBe true
+      renamed.asInstanceOf[VertexSchema].idColumnName.get shouldBe "m_id"
+    }
+
+    "not be able to rename _vid column" in {
+      intercept[IllegalArgumentException] {
+        vertexSchema.renameColumn("_vid", "other")
+      }
+    }
+
+    "not be able to rename _label column" in {
+      intercept[IllegalArgumentException] {
+        vertexSchema.renameColumn("_label", "other")
+      }
+    }
+
+    "return a subset of the columns as a List[Column]" in {
+      val excluded = vertexSchema.columnsExcept(List("name"))
+      excluded.length should be(3)
+    }
+
+    "be able to select a subset and rename" in {
+      val schema = vertexSchema.copySubsetWithRename(Map(("_vid", "_vid"), ("_label", "_label"), ("movie_id", "m_id")))
+      assert(schema.columns.length == 3)
+      assert(schema.column("_vid").index == 0)
+      assert(schema.column("_label").index == 1)
+      assert(schema.column(2).name == "m_id")
+    }
+
+    "be convertible to a FrameSchema" in {
+      val schema = vertexSchema.toFrameSchema
+      assert(schema.isInstanceOf[FrameSchema])
+    }
+
+    "should require a _vid and _label column" in {
+      intercept[IllegalArgumentException] {
+        new VertexSchema(columns, "movies", None)
+      }
+    }
+
+    "should require a _vid column" in {
+      intercept[IllegalArgumentException] {
+        new VertexSchema(List(Column("_label", str)), "movies", None)
+      }
+    }
+
+    "should require a _label column" in {
+      intercept[IllegalArgumentException] {
+        new VertexSchema(List(Column("_vid", int64)), "movies", None)
+      }
+    }
+
+    "should require a _vid column to be int64" in {
+      intercept[IllegalArgumentException] {
+        new VertexSchema(List(Column("_vid", str), Column("_label", str)), "movies", None)
+      }
+    }
+  }
+
+  val edgeColumns = List(Column("_eid", int64), Column("_label", str), Column("_src_vid", int64), Column("_dest_vid", int64), Column("rating", str))
+  val edgeSchema = EdgeSchema(edgeColumns, "ratings", "users", "movies", directed = true)
+
+  "EdgeSchema" should {
+
+    "find correct column index for single column" in {
+      edgeSchema.columnIndex("_eid") shouldBe 0
+      edgeSchema.columnIndex("_label") shouldBe 1
+      edgeSchema.columnIndex("rating") shouldBe 4
+    }
+
+    "find correct column index for two column" in {
+      edgeSchema.columnIndices(Seq("_eid", "_label")) shouldBe List(0, 1)
+      edgeSchema.columnIndices(Seq("_eid", "rating")) shouldBe List(0, 4)
+    }
+
+    "find correct column index for all columns when input columns is empty" in {
+      edgeSchema.columnIndices(Seq()) shouldBe List(0, 1, 2, 3, 4)
+    }
+
+    "be able to report column data types for first column" in {
+      edgeSchema.columnDataType("_eid") shouldBe int64
+    }
+
+    "be able to report column data types for last column" in {
+      edgeSchema.columnDataType("rating") shouldBe string
+    }
+
+    "be able to add columns" in {
+      val added = edgeSchema.addColumn("str", string)
+      added.columns.length shouldBe 6
+      added.column("str").dataType shouldBe string
+      added.column("str").index shouldBe 5
+    }
+
+    "be able to validate a column has a given type" in {
+      edgeSchema.hasColumnWithType("_eid", int64) shouldBe true
+      edgeSchema.hasColumnWithType("_eid", string) shouldBe false
+
+    }
+
+    def testDropColumn(columnName: String): Unit = {
+      val result = edgeSchema.dropColumn(columnName)
+      assert(result.columns.length == 2, "length was not 2: " + result)
+      assert(!result.hasColumn(columnName), "column was still present: " + result)
+    }
+
+    "not be able to drop _eid column" in {
+      intercept[IllegalArgumentException] {
+        testDropColumn("_eid")
+      }
+    }
+
+    "not be able to drop _label column" in {
+      intercept[IllegalArgumentException] {
+        testDropColumn("_label")
+      }
+    }
+
+    "not be able to drop _src_vid column" in {
+      intercept[IllegalArgumentException] {
+        testDropColumn("_src_vid")
+      }
+    }
+
+    "not be able to drop _dest_vid column" in {
+      intercept[IllegalArgumentException] {
+        testDropColumn("_dest_vid")
+      }
+    }
+
+    "be able to drop multiple columns" in {
+      val result = edgeSchema.dropColumns(List("rating"))
+      assert(result.columns.length == 4)
+      assert(result.hasColumn("_eid"))
+      assert(result.hasColumn("_label"))
+    }
+
+    "be able to rename columns" in {
+      val renamed = edgeSchema.renameColumn("rating", "user_rating")
+      renamed.hasColumn("rating") shouldBe false
+      renamed.hasColumn("user_rating") shouldBe true
+    }
+
+    "not be able to rename _eid column" in {
+      intercept[IllegalArgumentException] {
+        edgeSchema.renameColumn("_eid", "other")
+      }
+    }
+
+    "not be able to rename _label column" in {
+      intercept[IllegalArgumentException] {
+        edgeSchema.renameColumn("_label", "other")
+      }
+    }
+
+    "return a subset of the columns as a List[Column]" in {
+      val excluded = edgeSchema.columnsExcept(List("rating"))
+      excluded.length should be(4)
+    }
+
+    "be able to select a subset and rename" in {
+      val schema = edgeSchema.copySubsetWithRename(Map(("_eid", "_eid"), ("_label", "_label"), ("_src_vid", "_src_vid"), ("_dest_vid", "_dest_vid"), ("rating", "stars")))
+      assert(schema.columns.length == 5)
+      assert(schema.hasColumn("_eid"))
+      assert(schema.hasColumn("_label"))
+      assert(schema.hasColumn("stars"))
+      assert(!schema.hasColumn("rating"))
+    }
+
+    "be convertible to a FrameSchema" in {
+      val schema = edgeSchema.toFrameSchema
+      assert(schema.isInstanceOf[FrameSchema])
+    }
+
+    "should require a _eid column" in {
+      intercept[IllegalArgumentException] {
+        new EdgeSchema(List(Column("_label", str), Column("_src_vid", int64), Column("_dest_vid", int64), Column("rating", str)), "ratings", "users", "movies", directed = true)
+      }
+    }
+
+    "should require a _label column" in {
+      intercept[IllegalArgumentException] {
+        new EdgeSchema(List(Column("_eid", int64), Column("_src_vid", int64), Column("_dest_vid", int64), Column("rating", str)), "ratings", "users", "movies", directed = true)
+      }
+    }
+
+    "should require a _src_vid column" in {
+      intercept[IllegalArgumentException] {
+        new EdgeSchema(List(Column("_eid", int64), Column("_label", str), Column("_dest_vid", int64), Column("rating", str)), "ratings", "users", "movies", directed = true)
+      }
+    }
+
+    "should require a _dest_vid column" in {
+      intercept[IllegalArgumentException] {
+        new EdgeSchema(List(Column("_eid", int64), Column("_label", str), Column("_src_vid", int64), Column("rating", str)), "ratings", "users", "movies", directed = true)
+      }
+    }
+
+    "should require a _eid column to be int64" in {
+      intercept[IllegalArgumentException] {
+        new EdgeSchema(List(Column("_eid", str), Column("_label", str), Column("_src_vid", int64), Column("_dest_vid", int64), Column("rating", str)), "ratings", "users", "movies", directed = true)
+      }
+    }
+
   }
 
 }
