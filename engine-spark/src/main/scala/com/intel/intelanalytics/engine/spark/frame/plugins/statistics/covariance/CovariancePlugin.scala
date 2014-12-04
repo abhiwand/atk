76f739c1d5fb51a368cab27b3e5221c219e47883
Suraj Bathija <suraj.p.bathija@intel.com>
2014-12-04 10:18:21 -0800

    code review comments

--- engine-spark/src/main/scala/com/intel/intelanalytics/engine/spark/frame/plugins/statistics/covariance/CovariancePlugin.scala
+++ engine-spark/src/main/scala/com/intel/intelanalytics/engine/spark/frame/plugins/statistics/covariance/CovariancePlugin.scala
@@ -1,3 +1,26 @@
+//////////////////////////////////////////////////////////////////////////////
+// INTEL CONFIDENTIAL
+//
+// Copyright 2014 Intel Corporation All Rights Reserved.
+//
+// The source code contained or described herein and all documents related to
+// the source code (Material) are owned by Intel Corporation or its suppliers
+// or licensors. Title to the Material remains with Intel Corporation or its
+// suppliers and licensors. The Material may contain trade secrets and
+// proprietary and confidential information of Intel Corporation and its
+// suppliers and licensors, and is protected by worldwide copyright and trade
+// secret laws and treaty provisions. No part of the Material may be used,
+// copied, reproduced, modified, published, uploaded, posted, transmitted,
+// distributed, or disclosed in any way without Intel's prior express written
+// permission.
+//
+// No license under any patent, copyright, trade secret or other intellectual
+// property right is granted to or conferred upon you by disclosure or
+// delivery of the Materials, either expressly, by implication, inducement,
+// estoppel or otherwise. Any license under such intellectual property rights
+// must be express and approved by Intel in writing.
+//////////////////////////////////////////////////////////////////////////////
+
 package com.intel.intelanalytics.engine.spark.frame.plugins.statistics.multivariatestatistics
 
 import com.intel.intelanalytics.domain.command.CommandDoc
@@ -30,10 +53,30 @@ class CovariancePlugin extends SparkCommandPlugin[CovarianceArguments, Covarianc
    */
   override def doc: Option[CommandDoc] = Some(CommandDoc(oneLineSummary = "Calculate covariance for exactly two columns",
     extendedSummary = Some("""
-                             |
-                             |        .. versionadded:: 0.8
-                             | """.stripMargin)))
 
+        Compute the covariance for two columns.
+
+        Parameters
+        ----------
+        columns: [ str | list of str ]
+            The names 2 columns from which to compute the covariance
+
+        Returns
+        -------
+        Covariance of the two columns
+
+        Notes
+        -----
+        This function applies only to columns containing numerical data.
+
+        Examples
+        --------
+        Consider Frame *my_frame*, which accesses a frame that contains a single column named *obs*::
+
+            cov = my_frame.covariance(['col_0', 'col_1'])
+
+            print(cov)
+         """)))
   /**
    * Number of Spark jobs that get created by running this command
    * (this configuration is used to prevent multiple progress bars in Python client)
