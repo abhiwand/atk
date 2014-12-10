f86cfecf34b97ac3aad61719ae23332ea9004170
rene.o.dorado@intel.com <rene.o.dorado@intel.com>
2014-12-10 14:12:48 -0800

    TRIB-3019 comment refs to my host

--- package/config/intelanalytics/csd.sh
+++ package/config/intelanalytics/csd.sh
@@ -22,11 +22,11 @@ pushd $pkgFolder
     jar -cvf $pkgFolder.jar *
 popd
 
-cp $pkgFolder/$pkgFolder.jar /home/rodorad/Documents/parcels/csd
-scp $pkgFolder/$pkgFolder.jar wolverine:~
-scp $pkgFolder/$pkgFolder.jar tungsten:~
-ssh wolverine sudo cp /home/hadoop/$pkgFolder.jar /opt/cloudera/csd
-ssh tungsten sudo cp /home/hadoop/$pkgFolder.jar /opt/cloudera/csd
+#cp $pkgFolder/$pkgFolder.jar /home/rodorad/Documents/parcels/csd
+#scp $pkgFolder/$pkgFolder.jar wolverine:~
+#scp $pkgFolder/$pkgFolder.jar tungsten:~
+#ssh wolverine sudo cp /home/hadoop/$pkgFolder.jar /opt/cloudera/csd
+#ssh tungsten sudo cp /home/hadoop/$pkgFolder.jar /opt/cloudera/csd
 popd
 
 rm -rf $pkgFolder
