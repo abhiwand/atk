989852e64ae3c6db2fadc0110909b8f852c36fdb
rene.o.dorado@intel.com <rene.o.dorado@intel.com>
2014-12-10 14:12:48 -0800

    TRIB-3019 comment refs to my host

--- package/config/intelanalytics/parcel.sh
+++ package/config/intelanalytics/parcel.sh
@@ -35,8 +35,8 @@ mv $parcelDir/usr/lib/intelanalytics/rest-client/python $parcelDir/usr/lib/pytho
 
 tar zcvf $parcelDir-el6.parcel $parcelDir/ --owner=root --group=root
 
-cp $parcelDir-el6.parcel /home/rodorad/Documents/parcels/parcels/
-python /home/rodorad/Documents/parcels/cm_ext/make_manifest/make_manifest.py /home/rodorad/Documents/parcels/parcels/
+#cp $parcelDir-el6.parcel /home/rodorad/Documents/parcels/parcels/
+#python /home/rodorad/Documents/parcels/cm_ext/make_manifest/make_manifest.py /home/rodorad/Documents/parcels/parcels/
 popd
 
 rm -rf $parcelDir
