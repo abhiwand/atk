1922aa2f240f0e1539ee02b25bda39a42e1900f3
rene.o.dorado@intel.com <rene.o.dorado@intel.com>
2014-12-11 14:54:30 -0800

    TRIB-3019 fix broken path for addon jars to spark deps

--- package/config/intelanalytics-spark-deps/package.sh
+++ package/config/intelanalytics-spark-deps/package.sh
@@ -38,8 +38,7 @@ for jar in $jars
 do
 	jarPath=$(find ..  -path ../package -prune -o -name $jar -print)
 	echo $jarPath
-    cp -v $jarPath tarballs/$package/usr/lib/intelanalytics/rest-server/lib
-    cp -v $jarPath ${BUILD_DIR}/usr/lib/intelanalytics/graphbuilder/lib/
+    cp -v $jarPath ${BUILD_DIR}/usr/lib/intelanalytics/rest-server/lib
 done
 
 
