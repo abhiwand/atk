1b25eaaf5fb7067053f964446c19e48322565f0c
hicke, ryan <ryan.hicke@intel.com>
2014-12-16 14:11:46 -0800

    Fix for TRIB-4234. XML files need to handle quotes in Text Nodes.

--- engine-spark/src/main/scala/com/intel/intelanalytics/engine/spark/frame/plugins/load/LoadRDDFunctions.scala
+++ engine-spark/src/main/scala/com/intel/intelanalytics/engine/spark/frame/plugins/load/LoadRDDFunctions.scala
@@ -1,3 +1,25 @@
+//////////////////////////////////////////////////////////////////////////////
+// INTEL CONFIDENTIAL
+//
+// Copyright 2014 Intel Corporation All Rights Reserved.
+//
+// The source code contained or described herein and all documents related to
+// the source code (Material) are owned by Intel Corporation or its suppliers
+// or licensors. Title to the Material remains with Intel Corporation or its
+// suppliers and licensors. The Material may contain trade secrets and
+// proprietary and confidential information of Intel Corporation and its
+// suppliers and licensors, and is protected by worldwide copyright and trade
+// secret laws and treaty provisions. No part of the Material may be used,
+// copied, reproduced, modified, published, uploaded, posted, transmitted,
+// distributed, or disclosed in any way without Intel's prior express written
+// permission.
+//
+// No license under any patent, copyright, trade secret or other intellectual
+// property right is granted to or conferred upon you by disclosure or
+// delivery of the Materials, either expressly, by implication, inducement,
+// estoppel or otherwise. Any license under such intellectual property rights
+// must be express and approved by Intel in writing.
+//////////////////////////////////////////////////////////////////////////////
 package com.intel.intelanalytics.engine.spark.frame.plugins.load
 
 import com.intel.intelanalytics.domain.frame.load.{ LineParser, LineParserArguments }
@@ -9,6 +31,7 @@ import org.apache.spark.{ sql, SparkContext }
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SchemaRDD
 import org.apache.spark.sql.catalyst.expressions.GenericRow
+import org.apache.hadoop.io.Text
 
 import scala.reflect.ClassTag
 
@@ -36,11 +59,11 @@ object LoadRDDFunctions extends Serializable {
       startTag match {
         case Some(s) =>
           val conf = new org.apache.hadoop.conf.Configuration()
-          conf.setStrings(MultiLineHeaderFooterInputFormat.START_TAG_KEY, s: _*) //Treat s as a Varargs parameter
+          conf.setStrings(MultiLineTaggedInputFormat.START_TAG_KEY, s: _*) //Treat s as a Varargs parameter
           val e = endTag.get
-          conf.setStrings(MultiLineHeaderFooterInputFormat.END_TAG_KEY, e: _*)
-          conf.setBoolean(MultiLineHeaderFooterInputFormat.IS_XML_KEY, isXml)
-          sc.newAPIHadoopFile[LongWritable, Text, MultiLineHeaderFooterInputFormat](fileName, classOf[MultiLineHeaderFooterInputFormat], classOf[LongWritable], classOf[Text], conf)
+          conf.setStrings(MultiLineTaggedInputFormat.END_TAG_KEY, e: _*)
+          conf.setBoolean(MultiLineTaggedInputFormat.IS_XML_KEY, isXml)
+          sc.newAPIHadoopFile[LongWritable, Text, MultiLineTaggedInputFormat](fileName, classOf[MultiLineTaggedInputFormat], classOf[LongWritable], classOf[Text], conf)
             .map(row => row._2.toString).filter(_.trim() != "")
         case None => sc.textFile(fileName, partitions).filter(_.trim() != "")
       }
