1fe2e76e3bd0b9d068a2bb99ef1c79deefa8f6f3
Todd Lisonbee <todd.lisonbee@intel.com>
2015-01-21 21:53:01 -0800

    Undoing potential fix because it didn't work

--- engine-spark/src/main/scala/com/intel/intelanalytics/engine/spark/graph/HBaseAdminFactory.scala
+++ engine-spark/src/main/scala/com/intel/intelanalytics/engine/spark/graph/HBaseAdminFactory.scala
@@ -27,27 +27,7 @@ class HBaseAdminFactory {
     // This error shows up despite setting the correct classpath in bin/api-server.sh and packaging the correct cdh hbase jars
     config.setBoolean("hbase.defaults.for.version.skip", true)
 
-    // To help with debugging
-    println("HbaseAdmin Configuration")
-    config.writeXml(System.out)
-
-    // validation from HBaseConfiguration that we might want
-    checkForClusterFreeMemoryLimit(config)
-
-    // HBaseConfiguration.addHbaseResources(config) doesn't seem to work with this version of HBase, maybe because of how we load classes using Boot.scala
-    new HBaseAdmin(config)
-  }
-
-  // TODO: This method was copied from Open Source HBaseConfiguration.java, include copyright / license info
-  private def checkForClusterFreeMemoryLimit(conf: Configuration): Unit = {
-    val globalMemstoreLimit = conf.getFloat("hbase.regionserver.global.memstore.upperLimit", 0.4F)
-    val gml = (globalMemstoreLimit * 100.0F).toInt
-    val blockCacheUpperLimit = conf.getFloat("hfile.block.cache.size", 0.25F)
-
-    val bcul = (blockCacheUpperLimit * 100.0F).toInt
-    if (100 - (gml + bcul) < 20) {
-      throw new RuntimeException("Current heap configuration for MemStore and BlockCache exceeds the threshold required for successful cluster operation. The combined value cannot exceed 0.8. Please check the settings for hbase.regionserver.global.memstore.upperLimit and hfile.block.cache.size in your configuration. hbase.regionserver.global.memstore.upperLimit is " + globalMemstoreLimit + " hfile.block.cache.size is " + blockCacheUpperLimit);
-    }
+    new HBaseAdmin(HBaseConfiguration.addHbaseResources(config))
   }
 
 }
