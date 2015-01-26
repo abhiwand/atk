c03ad7cab8842a6e870dbfb985a5adee25c18f5e
Suraj Bathija <suraj.p.bathija@intel.com>
2015-01-26 13:35:44 -0800

    parameter fix

--- interfaces/src/main/scala/com/intel/intelanalytics/domain/model/ClassificationWithSGDArgs.scala
+++ interfaces/src/main/scala/com/intel/intelanalytics/domain/model/ClassificationWithSGDArgs.scala
@@ -29,7 +29,7 @@ import com.intel.intelanalytics.domain.frame.FrameReference
  * Command for loading model data into existing model in the model database.
  * @param model Handle to the model to be written to.
  * @param frame Handle to the data frame
- * @param observationColumn Handle to the observation column of the data frame
+ * @param observationColumns Handle to the observation column of the data frame
  * @param labelColumn Handle to the label column of the data frame
  */
 case class ClassificationWithSGDArgs(model: ModelReference, frame: FrameReference, observationColumns: List[String], labelColumn: String) {
