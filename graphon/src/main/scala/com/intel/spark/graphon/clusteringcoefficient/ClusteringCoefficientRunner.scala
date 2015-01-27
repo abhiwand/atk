23628fb4daaad8e5eed2765e157a1f9a090ab423
nlsegerl <nathan.l.segerlind@intel.com>
2015-01-26 16:15:34 -0800

    TRIB-4355 Clustering coefficient calculation.
    Initial implementation.

--- graphon/src/main/scala/com/intel/spark/graphon/clusteringcoefficient/ClusteringCoefficientRunner.scala
+++ graphon/src/main/scala/com/intel/spark/graphon/clusteringcoefficient/ClusteringCoefficientRunner.scala
@@ -1,6 +1,7 @@
 package com.intel.spark.graphon.clusteringcoefficient
 
 import com.intel.graphbuilder.elements.{ Property, GBVertex, GBEdge }
+import com.intel.spark.graphon.graphconversions.GraphConversions
 import org.apache.spark.graphx.{ Edge => GraphXEdge, PartitionStrategy, Graph }
 import org.apache.spark.graphx.lib.{ TriangleCount => GraphXTriangleCount }
 import org.apache.spark.rdd.RDD
@@ -8,16 +9,15 @@ import org.apache.spark.SparkContext._
 import org.apache.spark.storage.StorageLevel
 
 /**
- * Arguments for the TriangleCountRunnerArgs
- * @param outputEdgeLabel (optional) Name of the vertex property for storing local clustering coefficients.
- * @param inputEdgeSet List of edge labels to consider for clustering coefficient computation
+ * Return value for the clustering coefficient runner
+ * @param vertices RDD of vertices with the local clustering coefficient placed in the specified property.
+ * @param globalClusteringCoefficient The global clustering coefficient of the input graph.
  */
-case class ClusteringCoefficientRunnerArgs(outputEdgeLabel: String,
-                                           inputEdgeSet: Option[Set[String]])
+case class ClusteringCoefficientRunnerReturn(vertices: RDD[GBVertex], globalClusteringCoefficient: Double)
 
 /**
- * Provides a method for running triangle count on a graph using graphx. The result is a new graph with the
- * posterior value placed as a vertex property.
+ * Provides a method for running clustering coefficient on a graph using graphx. The result is a new graph with the
+ * local clustering coefficient placed as a vertex property.
  */
 object ClusteringCoefficientRunner extends Serializable {
 
@@ -29,68 +29,52 @@ object ClusteringCoefficientRunner extends Serializable {
    * Run clustering coefficient analysis of a graph.
    * @param inVertices Vertices of the incoming graph.
    * @param inEdges Edges of the incoming graph.
-   * @param args Parameters controlling the execution of pagerank.
+   * @param outputPropertyLabel Optional name of the vertex property for storing local clustering coefficients.
+   * @param inputEdgeLabels Optional list of edge labels to consider for clustering coefficient computation.
    * @return Vertices and edges for the output graph.
    */
 
-  def run(inVertices: RDD[GBVertex], inEdges: RDD[GBEdge], args: ClusteringCoefficientRunnerArgs): (RDD[GBVertex], RDD[GBEdge]) = {
+  def run(inVertices: RDD[GBVertex], inEdges: RDD[GBEdge], outputPropertyLabel: Option[String], inputEdgeLabels: Option[Set[String]]): ClusteringCoefficientRunnerReturn = {
 
-    val outputPropertyLabel = args.outputEdgeLabel
-    val inputEdgeLabels = args.inputEdgeSet
-
-    // Only select edges as specified in inputEdgeLabels
-    val filteredEdges: RDD[GBEdge] = inputEdgeLabels match {
-      case None => inEdges
-      case _ => inEdges.filter(edge => inputEdgeLabels.get.contains(edge.label))
+    val filteredEdges: RDD[GBEdge] = if (inputEdgeLabels.isEmpty) {
+      inEdges
+    }
+    else {
+      inEdges.filter(edge => inputEdgeLabels.get.contains(edge.label))
     }
 
     // convert to graphX vertices
     val graphXVertices: RDD[(Long, Null)] =
       inVertices.map(gbVertex => (gbVertex.physicalId.asInstanceOf[Long], null))
 
-    val graphXEdges: RDD[GraphXEdge[Long]] = filteredEdges.map(edge => createGraphXEdgeFromGBEdge(edge))
+    val graphXEdges: RDD[GraphXEdge[Long]] = filteredEdges.map(edge => GraphConversions.createGraphXEdgeFromGBEdge(edge))
 
     // create graphx Graph instance from graphx vertices and edges
     val graph = Graph[Null, Long](graphXVertices, graphXEdges)
       .partitionBy(PartitionStrategy.RandomVertexCut)
 
-    // run graphx trianglecount implementation
-    val newGraph: Graph[Int, Long] = GraphXTriangleCount.run(graph)
+    // run graphx clustering coefficient implementation
 
-    // extract vertices and edges from graphx graph instance
-    val intermediateVertices: RDD[(Long, Property)] = newGraph.vertices.map({
-      case (physicalId, triangleCount) => (physicalId, Property(outputPropertyLabel, triangleCount))
-    })
+    val (newGraph, globalClusteringCoefficient) = GraphXClusteringCoefficient.run(graph)
 
-    // Join the intermediate vertex/edge rdds with input vertex/edge rdd's to append the triangleCount attribute
-    val outVertices: RDD[GBVertex] = inVertices
-      .map(gbVertex => (gbVertex.physicalId.asInstanceOf[Long], gbVertex))
-      .join(intermediateVertices)
-      .map(vertex => generateGBVertex(vertex))
+    val outVertices = if (outputPropertyLabel.nonEmpty) {
+      val outputProperty = outputPropertyLabel.get
+      // extract vertices and edges from graphx graph instance
+      val intermediateVertices: RDD[(Long, Property)] = newGraph.vertices.map({
+        case (physicalId, clusteringCoefficient) => (physicalId, Property(outputProperty, clusteringCoefficient))
+      })
 
-    (outVertices, inEdges)
-  }
-
-  // converts GBEdge to a GraphXEdge to be consumed by the triangle count computation
-  // Note: GraphX Triangle count expects the edge to be in canonical orientation i.e. srcId < destId
-  // Triangle Count makes sense on a undirected graph, hence we can change the edges to the canonical orientation
-  // before passing it to graphx. We do not need the edges henceforth for join purposes.
-  // Refer: https://spark.apache.org/docs/1.0.2/api/scala/index.html#org.apache.spark.graphx.lib.TriangleCount$
-  private def createGraphXEdgeFromGBEdge(gbEdge: GBEdge, canonicalOrientation: Boolean = true): GraphXEdge[Long] = {
-    val srcId = gbEdge.tailPhysicalId.asInstanceOf[Long]
-    val destId = gbEdge.headPhysicalId.asInstanceOf[Long]
-    if (canonicalOrientation && srcId > destId)
-      GraphXEdge[Long](destId, srcId)
-    else
-      GraphXEdge[Long](srcId, destId)
-  }
-
-  // generates GBVertex from value pair obtained as a result of join and appends the pagerank property to the GBVertex
-  private def generateGBVertex(joinValuePair: (Long, GBVertexPropertyPair)): GBVertex = {
-    val (gbVertex, pagerankProperty) = joinValuePair._2 match {
-      case value: GBVertexPropertyPair => (value._1, value._2)
+      // Join the intermediate vertex/edge rdds with input vertex/edge rdd's to append the triangleCount attribute
+      inVertices
+        .map(gbVertex => (gbVertex.physicalId.asInstanceOf[Long], gbVertex))
+        .join(intermediateVertices)
+        .map(vertex => GraphConversions.generateGBVertex(vertex))
     }
-    gbVertex.copy(properties = gbVertex.properties + pagerankProperty)
+    else {
+      inVertices
+    }
+
+    ClusteringCoefficientRunnerReturn(outVertices, globalClusteringCoefficient)
   }
 
 }
