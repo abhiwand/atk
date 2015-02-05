cb56ffae425bf48f16d2c048a12f70e4d5e69ead
abolfazl.shahbazi@intel.com <abolfazl.e.shahbazi@intel.com>
2015-02-04 19:45:31 -0800

    add support for frame size

--- interfaces/src/main/scala/com/intel/intelanalytics/domain/StorageFormats.scala
+++ interfaces/src/main/scala/com/intel/intelanalytics/domain/StorageFormats.scala
@@ -28,11 +28,11 @@ package com.intel.intelanalytics.domain
  */
 object StorageFormats {
 
-  val CassandraTitan  = "cassandra/titan"
-  val HBaseTitan      = "hbase/titan"
-  val SeamlessGraph   = "ia/frame"
-  val FileParquet     = "file/parquet"
-  val FileSequence    = "file/sequence"
+  val CassandraTitan = "cassandra/titan"
+  val HBaseTitan = "hbase/titan"
+  val SeamlessGraph = "ia/frame"
+  val FileParquet = "file/parquet"
+  val FileSequence = "file/sequence"
 
   private val graphFormats = Set(SeamlessGraph, CassandraTitan, HBaseTitan)
   private val frameFormats = Set(FileSequence, FileParquet)
