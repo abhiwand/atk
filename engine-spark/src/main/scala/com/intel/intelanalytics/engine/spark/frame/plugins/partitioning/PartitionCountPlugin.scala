14b00c3a514e370922e7994d40026c5dd4d42ce3
Todd Lisonbee <todd.lisonbee@intel.com>
2015-02-09 19:53:46 -0800

    Moving partition related plugins under frame._partitions/

--- engine-spark/src/main/scala/com/intel/intelanalytics/engine/spark/frame/plugins/partitioning/PartitionCountPlugin.scala
+++ engine-spark/src/main/scala/com/intel/intelanalytics/engine/spark/frame/plugins/partitioning/PartitionCountPlugin.scala
@@ -46,7 +46,7 @@ class PartitionCountPlugin extends SparkCommandPlugin[FrameNoArgs, IntValue] {
    * The format of the name determines how the plugin gets "installed" in the client layer
    * e.g Python client via code generation.
    */
-  override def name: String = "frame/_partition_count"
+  override def name: String = "frame/_partitions/count"
 
   /**
    * User documentation exposed in Python.
