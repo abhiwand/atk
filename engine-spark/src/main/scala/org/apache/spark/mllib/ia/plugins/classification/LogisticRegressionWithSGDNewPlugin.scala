36fc884f4fc1a1c8e7b85a747ab39ade91883c18
Todd Lisonbee <todd.lisonbee@intel.com>
2015-02-25 00:54:57 -0800

    Import clean-up to fix some compiler warnings

--- engine-spark/src/main/scala/org/apache/spark/mllib/ia/plugins/classification/LogisticRegressionWithSGDNewPlugin.scala
+++ engine-spark/src/main/scala/org/apache/spark/mllib/ia/plugins/classification/LogisticRegressionWithSGDNewPlugin.scala
@@ -23,26 +23,12 @@
 
 package org.apache.spark.mllib.ia.plugins.classification
 
-import com.intel.intelanalytics.domain.{ CreateEntityArgs, Naming }
-import com.intel.intelanalytics.domain.command.CommandDoc
-import com.intel.intelanalytics.domain.frame.{ FrameEntity, FrameMeta }
+import com.intel.intelanalytics.domain.CreateEntityArgs
 import com.intel.intelanalytics.domain.model.{ GenericNewModelArgs, ModelEntity }
-import com.intel.intelanalytics.domain.schema.DataTypes
-import com.intel.intelanalytics.engine.Rows.Row
 import com.intel.intelanalytics.engine.plugin.Invocation
-import com.intel.intelanalytics.engine.spark.frame.{ SparkFrameData }
-import org.apache.spark.frame.FrameRDD
+import com.intel.intelanalytics.engine.spark.frame.SparkFrameData
 import com.intel.intelanalytics.engine.spark.plugin.SparkCommandPlugin
-import org.apache.spark.mllib.classification.LogisticRegressionModel
-import org.apache.spark.rdd.RDD
 import org.apache.spark.SparkContext._
-import org.apache.spark.mllib.classification.LogisticRegressionModel
-import org.apache.spark.mllib.ia.plugins.MLLibJsonProtocol
-import org.apache.spark.rdd.RDD
-import org.apache.spark.sql
-import org.apache.spark.sql
-import org.apache.spark.sql.catalyst.expressions.GenericRow
-import org.apache.spark.sql.catalyst.expressions.GenericRow
 import spray.json._
 import com.intel.intelanalytics.domain.DomainJsonProtocol._
 import org.apache.spark.mllib.ia.plugins.MLLibJsonProtocol._
