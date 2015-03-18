ae9788afb4ab90a25d3502b81def2abcff841204
Bryn Keller <bryn.keller@intel.com>
2015-03-18 09:29:39 -0700

    Don't need application.conf templates in CF environment

--- package/config/intelanalytics-rest-server-tar/package.sh
+++ package/config/intelanalytics-rest-server-tar/package.sh
@@ -24,8 +24,6 @@ mkdir -p  tarballs/$package/conf
 mkdir -p  tarballs/$package/lib
 
 
-cp -v ../conf/examples/application.conf.tpl tarballs/$package/conf
-cp -v ../conf/examples/application.conf.single-system.tpl tarballs/$package/conf
 cp -v ../conf/examples/application.conf tarballs/$package/conf
 cp -v ../conf/examples/logback.xml tarballs/$package/conf
 cp -Rv config/intelanalytics-rest-server/assets/etc/intelanalytics/rest-server/* tarballs/$package/conf
