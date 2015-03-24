5fdc71beb23c2b12f149deb88638900a3009a347
hicke, ryan <ryan.hicke@intel.com>
2015-03-24 16:08:22 -0700

    pull out common code between SparkEngineComponent and CommandDriver into a new Abstract Class

--- engine-spark/src/main/scala/com/intel/intelanalytics/engine/spark/command/CommandDriver.scala
+++ engine-spark/src/main/scala/com/intel/intelanalytics/engine/spark/command/CommandDriver.scala
@@ -1,77 +1,22 @@
 package com.intel.intelanalytics.engine.spark.command
-
-import com.intel.event.adapter.ConsoleEventLog
-import com.intel.event.{ EventLogger, EventLogging }
-import com.intel.intelanalytics.EventLoggingImplicits
-import com.intel.intelanalytics.component.Archive
+import com.intel.event.{ EventLogging }
 import com.intel.intelanalytics.domain.User
-import com.intel.intelanalytics.engine._
 import com.intel.intelanalytics.engine.plugin.{ Invocation, Call }
-import com.intel.intelanalytics.engine.spark.context.SparkContextFactory
-import com.intel.intelanalytics.engine.spark.frame.{ SparkFrameStorage, FrameFileStorage }
-import com.intel.intelanalytics.engine.spark.graph.{ SparkGraphStorage, HBaseAdminFactory, SparkGraphHBaseBackend }
-import com.intel.intelanalytics.engine.spark.model.SparkModelStorage
-import com.intel.intelanalytics.engine.spark.user.UserStorage
 import com.intel.intelanalytics.engine.spark._
-import com.intel.intelanalytics.repository.{ Profile, SlickMetaStoreComponent, DbProfileComponent }
 import com.typesafe.config.ConfigFactory
 import org.apache.commons.lang.exception.ExceptionUtils
-
-import scala.collection.mutable
 import scala.reflect.io.Directory
 
-class CommandDriver extends EngineComponent
-    with FrameComponent
-    with GraphComponent
-    with ModelComponent
-    with CommandComponent
-    with DbProfileComponent
-    with SlickMetaStoreComponent
-    with EventLogging
-    with EventLoggingImplicits {
+/**
+ * Executes
+ */
+class CommandDriver extends AbstractEngineComponent(new PluginCommandLoader) {
 
-  implicit lazy val startupCall = Call(null)
-
-  lazy val commandPluginRegistry = new CommandPluginRegistry(new PluginCommandLoader)
-
-  val sparkContextFactory = SparkContextFactory
-
-  val fileStorage = new HdfsFileStorage(SparkEngineConfig.fsRoot)
-
-  val sparkAutoPartitioner = new SparkAutoPartitioner(fileStorage)
-
-  val frameFileStorage = new FrameFileStorage(SparkEngineConfig.fsRoot, fileStorage)
-
-  val frameStorage = new SparkFrameStorage(frameFileStorage, SparkEngineConfig.pageSize, metaStore.asInstanceOf[SlickMetaStore], sparkAutoPartitioner)
-
-  private val backendGraphStorage: SparkGraphHBaseBackend = new SparkGraphHBaseBackend(hbaseAdminFactory = new HBaseAdminFactory)
-  val graphStorage: SparkGraphStorage = new SparkGraphStorage(metaStore, backendGraphStorage, frameStorage)
-
-  val modelStorage: SparkModelStorage = new SparkModelStorage(metaStore.asInstanceOf[SlickMetaStore])
-
-  val userStorage = new UserStorage(metaStore.asInstanceOf[SlickMetaStore])
-
-  val commands = new SparkCommandStorage(metaStore.asInstanceOf[SlickMetaStore])
-
-  lazy val commandExecutor: CommandExecutor = new CommandExecutor(engine, commands)
-
-  override lazy val profile = withContext("engine connecting to metastore") {
-
-    // Initialize a Profile from settings in the config
-    val driver = SparkEngineConfig.metaStoreConnectionDriver
-    new Profile(Profile.jdbcProfileForDriver(driver),
-      connectionString = SparkEngineConfig.metaStoreConnectionUrl,
-      driver,
-      username = SparkEngineConfig.metaStoreConnectionUsername,
-      password = SparkEngineConfig.metaStoreConnectionPassword)
-  }(startupCall.eventContext)
-
-  val engine = new SparkEngine(sparkContextFactory,
-    commandExecutor, commands, frameStorage, graphStorage, modelStorage, userStorage, null,
-    sparkAutoPartitioner, commandPluginRegistry) {}
-
+  /**
+   * Execute Command
+   * @param commandId id of command to execute
+   */
   def execute(commandId: Long): Unit = {
-
     commands.lookup(commandId) match {
       case None => info(s"Command $commandId not found")
       case Some(command) => {
@@ -92,15 +37,29 @@ class CommandDriver extends EngineComponent
   }
 }
 
+/**
+ * Static methods for CommandDriver. Includes main method for use by SparkSubmit
+ */
 object CommandDriver {
 
+  /**
+   * Usage string if this was being executed from the command line
+   */
   def usage() = println("Usage: java -cp engine-spark.jar com.intel.intelanalytics.component.CommandDriver <command_id>")
 
+  /**
+   * Instantiate an instance of the driver and then executing the requested command.
+   * @param commandId
+   */
   def executeCommand(commandId: Long): Unit = {
     val driver = new CommandDriver
     driver.execute(commandId)
   }
 
+  /**
+   * Entry point of CommandDriver for use by SparkSubmit.
+   * @param args command line arguments. Requires command id
+   */
   def main(args: Array[String]) = {
     if (args.length < 1) {
       usage()
