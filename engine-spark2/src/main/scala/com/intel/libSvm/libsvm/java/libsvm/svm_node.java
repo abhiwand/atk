27e1b52d741a4e9d2263eaf55b88ef5914beef13
Anjali Sood <anjali.sood@intel.com>
2015-03-31 16:25:58 -0700

    wip

--- engine-spark2/src/main/scala/com/intel/libSvm/libsvm/java/libsvm/svm_node.java
+++
@@ -1,6 +0,0 @@
-package com.intel.libSvm.libsvm.java.libsvm;
-public class svm_node implements java.io.Serializable
-{
-	public int index;
-	public double value;
-}
--- engine-spark2/src/main/scala/com/intel/libSvm/libsvm/java/libsvm/svm_print_interface.java
+++
@@ -1,5 +0,0 @@
-package com.intel.libSvm.libsvm.java.libsvm;
-public interface svm_print_interface
-{
-	public void print(String s);
-}
