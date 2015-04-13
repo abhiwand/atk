901ca5c0d41a0b16452b838a525808bef9d4859b
scatrava <serban.catrava@intel.com>
2015-04-13 16:21:03 -0700

    added

--- graphon/src/test/scala/com/intel/spark/graphon/hierarchicalclustering/EdgeDistanceTest.scala
+++ graphon/src/test/scala/com/intel/spark/graphon/hierarchicalclustering/EdgeDistanceTest.scala
@@ -1,5 +1,14 @@
 package com.intel.spark.graphon.hierarchicalclustering
 
-class EdgeDistanceTest {
+import org.scalatest.{ Matchers, FlatSpec }
 
+class EdgeDistanceTest extends FlatSpec with Matchers {
+
+  "edgeDistance::min" should "bal1" in {
+    assert(1 == 1)
+  }
+
+  "edgeDistance::min" should "bla2" in {
+
+  }
 }
