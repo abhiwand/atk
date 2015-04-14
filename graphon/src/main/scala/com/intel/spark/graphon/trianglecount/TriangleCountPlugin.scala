ce97bad157d3f5fe60f993765aee3f5b79c00553
Anahita Bhiwandiwalla <anahita.bhiwandiwalla@intel.com>
2015-04-13 18:27:44 -0700

    Fix TRIB-4712

--- graphon/src/main/scala/com/intel/spark/graphon/trianglecount/TriangleCountPlugin.scala
+++ graphon/src/main/scala/com/intel/spark/graphon/trianglecount/TriangleCountPlugin.scala
@@ -25,15 +25,18 @@ package com.intel.spark.graphon.trianglecount
 
 import com.intel.graphbuilder.driver.spark.titan.GraphBuilder
 import com.intel.graphbuilder.util.SerializableBaseConfiguration
-import com.intel.intelanalytics.domain.{ StorageFormats, DomainJsonProtocol }
+import com.intel.intelanalytics.domain.frame.{ FrameMeta, FrameEntity }
+import com.intel.intelanalytics.domain.{ CreateEntityArgs, StorageFormats, DomainJsonProtocol }
 import com.intel.intelanalytics.domain.DomainJsonProtocol._
 import com.intel.intelanalytics.domain.graph.GraphReference
 import com.intel.intelanalytics.domain.graph.{ GraphTemplate, GraphReference }
 import com.intel.intelanalytics.engine.plugin.Invocation
 import com.intel.intelanalytics.engine.spark.context.SparkContextFactory
+import com.intel.intelanalytics.engine.spark.frame.SparkFrameData
 import com.intel.intelanalytics.engine.spark.plugin.SparkCommandPlugin
 import com.intel.intelanalytics.engine.spark.plugin.{ SparkInvocation, SparkCommandPlugin }
 import com.intel.intelanalytics.security.UserPrincipal
+import org.apache.spark.frame.FrameRdd
 import org.apache.spark.storage.StorageLevel
 import scala.concurrent.{ Await, ExecutionContext }
 import com.intel.intelanalytics.component.Boot
@@ -53,27 +56,24 @@ import java.util.UUID
  * Parameters for executing triangle count.
  * @param graph Reference to the graph object on which to compute triangle count.
  * @param output_property Name of the property to which triangle count value will be stored on vertex.
- * @param output_graph_name Name of output graph.
  * @param input_edge_labels List of edge labels to consider for computation. If None, all edges are considered.
  */
 case class TriangleCountArgs(graph: GraphReference,
                              output_property: String,
-                             output_graph_name: String,
                              input_edge_labels: Option[List[String]] = None) {
   require(!output_property.isEmpty, "Output property label must be provided")
-  require(!output_graph_name.isEmpty, "Output graph name must be provided")
 }
 
 /**
  * The result object
- * @param graph Name of the output graph
+ * @param frameDictionaryOutput Name of the output graph
  */
-case class TriangleCountResult(graph: String)
+case class TriangleCountResult(frameDictionaryOutput: Map[String, FrameEntity])
 
 /** Json conversion for arguments and return value case classes */
 object TriangleCountJsonFormat {
   import DomainJsonProtocol._
-  implicit val TCFormat = jsonFormat4(TriangleCountArgs)
+  implicit val TCFormat = jsonFormat3(TriangleCountArgs)
   implicit val TCResultFormat = jsonFormat1(TriangleCountResult)
 }
 
@@ -87,9 +87,9 @@ import TriangleCountJsonFormat._
  *
  * Right now it is using only Titan for graph storage. Other backends including Parquet will be supported later.
  */
-class TriangleCount extends SparkCommandPlugin[TriangleCountArgs, TriangleCountResult] {
+class TriangleCountPlugin extends SparkCommandPlugin[TriangleCountArgs, TriangleCountResult] {
 
-  override def name: String = "graph:titan/graphx_triangle_count"
+  override def name: String = "graph/graphx_triangle_count"
 
   //TODO remove when we move to the next version of spark
   override def kryoRegistrator: Option[String] = None
@@ -109,20 +109,16 @@ class TriangleCount extends SparkCommandPlugin[TriangleCountArgs, TriangleCountR
     // Call TriangleCountRunner to kick off Triangle Count computation on RDDs
     val (outVertices, outEdges) = TriangleCountRunner.run(gbVertices, gbEdges, tcRunnerArgs)
 
-    val newGraphName = arguments.output_graph_name
-    val newGraph = engine.graphs.createGraph(GraphTemplate(Some(newGraphName), StorageFormats.HBaseTitan))
+    val frameRddMap = FrameRdd.toFrameRddMap(outVertices)
 
-    // create titan config copy for newGraph write-back
-    val newTitanConfig = GraphBuilderConfigFactory.getTitanConfiguration(newGraph)
-    writeToTitan(newTitanConfig, outVertices, outEdges)
+    new TriangleCountResult(frameRddMap.keys.map(label => {
+      val result = tryNew(CreateEntityArgs(description = Some("created by connected components operation"))) { newOutputFrame: FrameMeta =>
+        val frameRdd = frameRddMap(label)
+        save(new SparkFrameData(newOutputFrame.meta, frameRdd))
+      }.meta
+      (label, result)
+    }).toMap)
 
-    TriangleCountResult(newGraphName)
-  }
-
-  // Helper function to write rdds back to Titan
-  private def writeToTitan(titanConfig: SerializableBaseConfiguration, gbVertices: RDD[GBVertex], gbEdges: RDD[GBEdge], append: Boolean = false) = {
-    val gb = new GraphBuilder(new GraphBuilderConfig(new InputSchema(Seq.empty), List.empty, List.empty, titanConfig, append))
-    gb.buildGraphWithSpark(gbVertices, gbEdges)
   }
 
 }
