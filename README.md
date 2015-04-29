2e07d2ea4734c6ed489f607528412f420c021e99
Todd Lisonbee <todd.lisonbee@intel.com>
2015-04-28 18:07:16 -0700

    TRIB-4876 renaming api-server to rest-server, moving graphbuilder and meta-store code, and other re-org

--- README.md
+++ README.md
@@ -3,27 +3,26 @@ Intel Analytics Toolkit (ATK)
 
 # source_code folders / modules overview
 * api-doc/ - customer facing plugin docs
-* api-server/ - the rest server
-* bin/ - script for starting api server, gremlin shell
+* bin/ - script for starting REST server, gremlin shell
 * conf/ - configuration templates for setting up a system, put your application.conf here for running out of source_code
 * doc/ - customer facing docs
 * engine/ - engine interface (seems like we should just delete this module)
 * engine-spark - majority of the engine code
 * event/ - logging framework
 * event-api/ - logging framework
-* graphbuilder-3 - Titan graph construction and reading
+* graphbuilder - Titan graph construction and reading
 * graphon/ - some graph related plugins that run on Spark and GraphX
 * igiraph/ - a few graph algorithms that run on Giraph
 * integration-tests/ - developer written, build time integration tests in python, these run against a minimal version of our product
-* interfaces/ - interfaces shared between api-server and engine-spark (we should move plugin args/return values out of here)
+* interfaces/ - interfaces shared between rest-server and engine-spark (we should move plugin args/return values out of here)
 * ititan/ - repackaging of Titan to get around classloader issues
 * launcher/ - starts up our application, launches parts of our app
 * misc/ - miscellaneous items that aren't really part of our product
 * package/ - packaging for VM's, RPM's
 * python/ - python client code
 * python-examples/ - internal example code, add examples here so we don't have to email scripts around
-* scripts/ - RPM packaging
-* shared/ - mostly code for our meta-data repository, SQL scripts for our meta-data repository
+* rest-server/ - the rest server
+* meta-store/ - mostly code for our meta-data repository, SQL scripts for our meta-data repository
 * testutils/ - some test utility code that get re-used between tests in different modules
 
 
---
+++ rest-server/src/main/scala/com/intel/intelanalytics/rest/v1/CommandService.scala
@@ -0,0 +1,158 @@
+//////////////////////////////////////////////////////////////////////////////
+// INTEL CONFIDENTIAL
+//
+// Copyright 2015 Intel Corporation All Rights Reserved.
+//
+// The source code contained or described herein and all documents related to
+// the source code (Material) are owned by Intel Corporation or its suppliers
+// or licensors. Title to the Material remains with Intel Corporation or its
+// suppliers and licensors. The Material may contain trade secrets and
+// proprietary and confidential information of Intel Corporation and its
+// suppliers and licensors, and is protected by worldwide copyright and trade
+// secret laws and treaty provisions. No part of the Material may be used,
+// copied, reproduced, modified, published, uploaded, posted, transmitted,
+// distributed, or disclosed in any way without Intel's prior express written
+// permission.
+//
+// No license under any patent, copyright, trade secret or other intellectual
+// property right is granted to or conferred upon you by disclosure or
+// delivery of the Materials, either expressly, by implication, inducement,
+// estoppel or otherwise. Any license under such intellectual property rights
+// must be express and approved by Intel in writing.
+//////////////////////////////////////////////////////////////////////////////
+
+package com.intel.intelanalytics.rest.v1
+
+import com.intel.intelanalytics.engine.plugin.Invocation
+
+import scala.util.Try
+import com.intel.intelanalytics.domain._
+import com.intel.intelanalytics.engine.Engine
+import scala.concurrent._
+import spray.http.{ StatusCodes, Uri }
+import spray.routing.{ ValidationRejection, Directives, Route }
+import scala.util.Failure
+import scala.util.Success
+import com.intel.intelanalytics.security.UserPrincipal
+import spray.json._
+import com.intel.intelanalytics.domain.DomainJsonProtocol._
+import com.intel.intelanalytics.rest.v1.viewmodels._
+import com.intel.intelanalytics.rest.v1.viewmodels.ViewModelJsonImplicits._
+import com.intel.intelanalytics.domain.command.{ CommandPost, Execution, CommandTemplate, Command }
+import com.intel.intelanalytics.rest.{ UrlParser, RestServerConfig, CommonDirectives }
+import com.intel.intelanalytics.rest.v1.decorators.CommandDecorator
+import com.intel.intelanalytics.spray.json.IADefaultJsonProtocol
+import com.intel.event.EventLogging
+
+//TODO: Is this right execution context for us?
+import ExecutionContext.Implicits.global
+
+/**
+ * REST API Command Service
+ *
+ * Always use onComplete( Future { operationsGoHere() } ) to prevent "server disconnected" messages in client.
+ */
+class CommandService(commonDirectives: CommonDirectives, engine: Engine) extends Directives with EventLogging {
+
+  /**
+   * Creates a view model for return through the HTTP protocol
+   *
+   * @param uri The link representing the command.
+   * @param command The command being decorated
+   * @return View model of the command.
+   */
+  def decorate(uri: Uri, command: Command): GetCommand = {
+    //TODO: add other relevant links
+    val links = List(Rel.self(uri.toString()))
+    CommandDecorator.decorateEntity(uri.toString(), links, command)
+  }
+
+  /**
+   * The spray routes defining the command service.
+   */
+  def commandRoutes() = {
+    commonDirectives("commands") { implicit invocation: Invocation =>
+      pathPrefix("commands" / LongNumber) {
+        id =>
+          pathEnd {
+            requestUri {
+              uri =>
+                get {
+                  onComplete(engine.getCommand(id)) {
+                    case Success(Some(command)) => complete(decorate(uri, command))
+                    case Success(None) => complete(StatusCodes.NotFound)
+                    case Failure(ex) => throw ex
+                    case _ => reject()
+                  }
+                } ~
+                  post {
+                    entity(as[JsonTransform]) {
+                      xform =>
+                        {
+                          val action = xform.arguments.get.convertTo[CommandPost]
+                          action.status match {
+                            case "cancel" => onComplete(engine.cancelCommand(id)) {
+                              case Success(command) => complete("Command cancelled by client")
+                              case Failure(ex) => throw ex
+                            }
+                          }
+                        }
+                    }
+
+                  }
+            }
+          }
+      } ~
+        pathPrefix("commands") {
+          path("definitions") {
+            get {
+              import IADefaultJsonProtocol.listFormat
+              import DomainJsonProtocol.commandDefinitionFormat
+              onComplete(Future { engine.getCommandDefinitions().toList }) {
+                case Success(list) => complete(list)
+                case Failure(ex) => throw ex
+              }
+            }
+          } ~
+            pathEnd {
+              requestUri {
+                uri =>
+                  get {
+                    //TODO: cursor
+                    import spray.json._
+                    import ViewModelJsonImplicits._
+                    parameters("offset" ? 0, "count" ? RestServerConfig.defaultCount) { (offset, count) =>
+                      onComplete(engine.getCommands(offset, count)) {
+                        case Success(commands) => complete(CommandDecorator.decorateForIndex(uri.withQuery().toString(), commands))
+                        case Failure(ex) => throw ex
+                      }
+                    }
+                  } ~
+                    post {
+                      entity(as[JsonTransform]) {
+                        xform =>
+                          val template = CommandTemplate(name = xform.name, arguments = xform.arguments)
+                          info(s"Received command template for execution: $template")
+                          onComplete(Future { engine.execute(template) }) {
+                            case Success(Execution(command, futureResult)) => complete(decorate(uri + "/" + command.id, command))
+                            case Failure(e: DeserializationException) =>
+                              val message = s"Incorrectly formatted JSON found while parsing command '${xform.name}':" +
+                                s" ${e.getMessage}"
+                              failWith(new DeserializationException(message, e))
+                            case Failure(ex) => throw ex
+                          }
+                      }
+                    }
+              }
+
+            }
+        }
+    }
+  }
+
+  //TODO: internationalization
+  def getErrorMessage[T](value: Try[T]): String = value match {
+    case Success(x) => ""
+    case Failure(ex) => ex.getMessage
+  }
+}
