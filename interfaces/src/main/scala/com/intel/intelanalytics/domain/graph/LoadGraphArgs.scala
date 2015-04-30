4955a4b60c307a95e1fecbf66419cef9a0901711
Briton Barker <briton.barker@intel.com>
2015-04-30 15:47:47 -0700

    build fix from doc inf merge

--- interfaces/src/main/scala/com/intel/intelanalytics/domain/graph/LoadGraphArgs.scala
+++ interfaces/src/main/scala/com/intel/intelanalytics/domain/graph/LoadGraphArgs.scala
@@ -34,7 +34,7 @@ import com.intel.intelanalytics.domain.graph.construction.FrameRule
  */
 case class LoadGraphArgs(graph: GraphReference,
                          frameRules: List[FrameRule],
-                         append: Boolean = false) {
+                         append: Option[Boolean] = Some(false)) {
   require(graph != null, "graph must not be null")
   require(frameRules != null, "frame rules must not be null")
 }
