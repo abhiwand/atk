9faeeb34f67c21fbac8720c08645b87d0a52ad30
Suraj Bathija <suraj.p.bathija@intel.com>
2015-05-04 15:08:38 -0700

    adding hive back

--- engine-spark/src/main/scala/com/intel/intelanalytics/engine/spark/command/CommandExecutor.scala
+++ engine-spark/src/main/scala/com/intel/intelanalytics/engine/spark/command/CommandExecutor.scala
@@ -294,7 +294,8 @@ class CommandExecutor(engine: => SparkEngine, commands: CommandStorage)
           val pluginDependencyFiles = Array("--files", s"$tempConfFileName#application.conf$kerbFile", "--conf", s"config.resource=application.conf")
           val executionParams = Array(
             "--num-executors", s"${SparkEngineConfig.sparkOnYarnNumExecutors}",
-            "--driver-java-options", s"-XX:MaxPermSize=${SparkEngineConfig.sparkDriverMaxPermSize} $kerbOptions")
+            //"--driver-java-options", s"-XX:MaxPermSize=${SparkEngineConfig.sparkDriverMaxPermSize} $kerbOptions")
+            "--driver-java-options", s"-Dspark.executor.extraClassPath=/opt/cloudera/parcels/CDH/lib/hive/lib/*")
 
           val executionConfigs = {
             for {
@@ -304,7 +305,8 @@ class CommandExecutor(engine: => SparkEngine, commands: CommandStorage)
 
           // TODO: Once we get rid of setting SPARK_CLASSPATH in cdh, we should be setting only the driver-class-path
           val driver_classpath = SparkEngineConfig.sparkMaster match {
-            case "yarn-cluster" | "yarn-client" => Array(s"--driver-class-path", s"${pluginExtraClasspath.mkString(":")}")
+            //case "yarn-cluster" | "yarn-client" => Array(s"--driver-class-path", s"${pluginExtraClasspath.mkString(":")}")
+            case "yarn-cluster" | "yarn-client" => Array(s"--driver-class-path", s"${pluginExtraClasspath.mkString(":")}:/opt/cloudera/parcels/CDH/lib/hive/lib:/etc/hive/conf")
             case _ => Array[String]()
           }
           val verbose = Array("--verbose")
