31a8cf6538faaec73548411d2410129ca04368ef
scatrava <serban.catrava@intel.com>
2015-05-05 23:18:18 -0700

    added behavior for feature

---
+++ giraph-plugins/src/main/scala/org/apache/spark/sql/parquet/ia/giraph/frame/LabelPropagationEdgeInputFormat.scala
@@ -0,0 +1,120 @@
+  //////////////////////////////////////////////////////////////////////////////
+  // INTEL CONFIDENTIAL
+  //
+  // Copyright 2015 Intel Corporation All Rights Reserved.
+  //
+  // The source code contained or described herein and all documents related to
+  // the source code (Material) are owned by Intel Corporation or its suppliers
+  // or licensors. Title to the Material remains with Intel Corporation or its
+  // suppliers and licensors. The Material may contain trade secrets and
+  // proprietary and confidential information of Intel Corporation and its
+  // suppliers and licensors, and is protected by worldwide copyright and trade
+  // secret laws and treaty provisions. No part of the Material may be used,
+  // copied, reproduced, modified, published, uploaded, posted, transmitted,
+  // distributed, or disclosed in any way without Intel's prior express written
+  // permission.
+  //
+  // No license under any patent, copyright, trade secret or other intellectual
+  // property right is granted to or conferred upon you by disclosure or
+  // delivery of the Materials, either expressly, by implication, inducement,
+  // estoppel or otherwise. Any license under such intellectual property rights
+  // must be express and approved by Intel in writing.
+  //////////////////////////////////////////////////////////////////////////////
+
+  package org.apache.spark.sql.parquet.ia.giraph.frame
+
+  import java.util
+
+  import com.intel.giraph.io.{LabelPropagationEdgeData, LabelPropagationVertexId}
+  import com.intel.ia.giraph.lp.LabelPropagationConfiguration
+  import com.intel.intelanalytics.engine.spark.frame.RowWrapper
+  import org.apache.giraph.edge.{ DefaultEdge, Edge }
+  import org.apache.giraph.io._
+  import org.apache.hadoop.conf.Configuration
+  import org.apache.hadoop.fs.{ FileSystem, Path }
+  import org.apache.hadoop.mapreduce.{ InputSplit, JobContext, TaskAttemptContext }
+  import org.apache.spark.sql.catalyst.expressions.Row
+  import org.apache.spark.sql.parquet.RowReadSupport
+  import parquet.hadoop.{ ParquetInputFormat, ParquetRecordReader }
+
+  import scala.collection.JavaConverters._
+
+  /**
+   * InputFormat for Label propagation reads edges from Parquet Frame
+   */
+  class LabelPropagationEdgeInputFormat extends EdgeInputFormat[LabelPropagationVertexId, LabelPropagationEdgeData] {
+
+    private val parquetInputFormat = new ParquetInputFormat[Row](classOf[RowReadSupport])
+
+    override def checkInputSpecs(conf: Configuration): Unit = {
+    }
+
+    override def createEdgeReader(split: InputSplit, context: TaskAttemptContext): EdgeReader[LabelPropagationVertexId, LabelPropagationEdgeData] = {
+      val edgeReader = new LabelPropagationEdgeReader(new LabelPropagationConfiguration(context.getConfiguration))
+      // algorithm expects edges that go both ways (seems to be how undirected is modeled in Giraph)
+      new ReverseEdgeDuplicator(edgeReader)
+    }
+
+    override def getSplits(context: JobContext, minSplitCountHint: Int): util.List[InputSplit] = {
+      val path: String = new LabelPropagationConfiguration(context.getConfiguration).labelPropagationConfig.inputFormatConfig.parquetFileLocation
+      val fs: FileSystem = FileSystem.get(context.getConfiguration)
+
+      val statuses = if (fs.isDirectory(new Path(path))) {
+        fs.globStatus(new Path(path + "/*.parquet"))
+      }
+      else {
+        fs.globStatus(new Path(path))
+      }
+      val footers = parquetInputFormat.getFooters(context.getConfiguration, statuses.toList.asJava)
+
+      parquetInputFormat.getSplits(context.getConfiguration, footers).asInstanceOf[java.util.List[InputSplit]]
+    }
+  }
+
+  class LabelPropagationEdgeReader(config: LabelPropagationConfiguration)
+    extends EdgeReader[LabelPropagationVertexId, LabelPropagationEdgeData] {
+
+    private val conf = config.labelPropagationConfig
+    private val reader = new ParquetRecordReader[Row](new RowReadSupport)
+    private val row = new RowWrapper(conf.inputFormatConfig.frameSchema)
+
+    private var currentSourceId: LabelPropagationVertexId = null
+    private var currentEdge: DefaultEdge[LabelPropagationVertexId, LabelPropagationEdgeData] = null
+
+    override def initialize(inputSplit: InputSplit, context: TaskAttemptContext): Unit = {
+      reader.initialize(inputSplit, context)
+    }
+
+    override def getProgress: Float = {
+      reader.getProgress
+    }
+
+    override def getCurrentEdge: Edge[LabelPropagationVertexId, LabelPropagationEdgeData] = {
+      currentEdge
+    }
+
+    override def getCurrentSourceId: LabelPropagationVertexId = {
+      currentSourceId
+    }
+
+    override def nextEdge(): Boolean = {
+      val hasNext: Boolean = reader.nextKeyValue
+      if (hasNext) {
+        row.apply(reader.getCurrentValue)
+
+        val sourceId = new LabelPropagationVertexId(row.stringValue(conf.sourceIdColumnName))
+        val destId = new LabelPropagationVertexId(row.stringValue(conf.sourceIdColumnName))
+        val edgeWeight = new LabelPropagationEdgeData(row.stringValue(conf.edgeWeightColumnName).toDouble)
+
+        currentSourceId = sourceId
+        currentEdge = new DefaultEdge[LabelPropagationVertexId, LabelPropagationEdgeData]()
+        currentEdge.setTargetVertexId(destId)
+        currentEdge.setValue(edgeWeight)
+      }
+      hasNext
+    }
+
+    override def close(): Unit = {
+      reader.close()
+    }
+}
---
+++ giraph-plugins/src/main/scala/org/apache/spark/sql/parquet/ia/giraph/frame/LabelPropagationVertexOutputFormat.scala
@@ -0,0 +1,62 @@
+//////////////////////////////////////////////////////////////////////////////
+// INTEL CONFIDENTIAL
+//
+// Copyright 2015 Intel Corporation All Rights Reserved.
+//
+// The source code contained or described herein and all documents related to
+// the source code (Material) are owned by Intel Corporation or its suppliers
+// or licensors. Title to the Material remains with Intel Corporation or its
+// suppliers and licensors. The Material may contain trade secrets and
+// proprietary and confidential information of Intel Corporation and its
+// suppliers and licensors, and is protected by worldwide copyright and trade
+// secret laws and treaty provisions. No part of the Material may be used,
+// copied, reproduced, modified, published, uploaded, posted, transmitted,
+// distributed, or disclosed in any way without Intel's prior express written
+// permission.
+//
+// No license under any patent, copyright, trade secret or other intellectual
+// property right is granted to or conferred upon you by disclosure or
+// delivery of the Materials, either expressly, by implication, inducement,
+// estoppel or otherwise. Any license under such intellectual property rights
+// must be express and approved by Intel in writing.
+//////////////////////////////////////////////////////////////////////////////
+
+package org.apache.spark.sql.parquet.ia.giraph.frame
+
+import com.intel.giraph.io.{VertexData4LPWritable, LabelPropagationVertexId}
+import com.intel.ia.giraph.lp.LabelPropagationConfiguration
+import org.apache.giraph.io.VertexOutputFormat
+import org.apache.hadoop.mapreduce._
+import org.apache.spark.sql.catalyst.expressions.Row
+import org.apache.spark.sql.parquet.RowWriteSupport
+import parquet.hadoop.ParquetOutputFormat
+
+/**
+ * OutputFormat for parquet frame
+ */
+class LabelPropagationVertexOutputFormat extends VertexOutputFormat[LabelPropagationVertexId, VertexData4LPWritable, Nothing] {
+
+  private val resultsOutputFormat = new ParquetOutputFormat[Row](new RowWriteSupport)
+
+  override def createVertexWriter(context: TaskAttemptContext): LabelPropagationVertexWriter = {
+    new LabelPropagationVertexWriter(new LabelPropagationConfiguration(context.getConfiguration), resultsOutputFormat)
+  }
+
+  override def checkOutputSpecs(context: JobContext): Unit = {
+    new LabelPropagationConfiguration(context.getConfiguration).validate()
+  }
+
+  override def getOutputCommitter(context: TaskAttemptContext): OutputCommitter = {
+    val outputFormatConfig = new LabelPropagationConfiguration(context.getConfiguration).labelPropagationConfig.outputFormatConfig
+
+    // configure outputdir for committer
+    context.getConfiguration.set("mapreduce.output.fileoutputformat.outputdir", outputFormatConfig.parquetFileLocation)
+    val docCommitter = resultsOutputFormat.getOutputCommitter(context)
+
+    new MultiOutputCommitter(List(docCommitter))
+  }
+}
+
+
+
+
