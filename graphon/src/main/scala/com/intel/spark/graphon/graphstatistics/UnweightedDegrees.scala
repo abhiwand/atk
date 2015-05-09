b562db5c248e1ee8dc977f6b0607d43355771615
Anahita Bhiwandiwalla <anahita.bhiwandiwalla@intel.com>
2015-05-08 17:18:38 -0700

    Added a TODO in WeightedDegrees.scala and UnweightedDegrees.scala

--- graphon/src/main/scala/com/intel/spark/graphon/graphstatistics/UnweightedDegrees.scala
+++ graphon/src/main/scala/com/intel/spark/graphon/graphstatistics/UnweightedDegrees.scala
@@ -78,6 +78,7 @@ object UnweightedDegrees {
   private case class VertexDegreeRecord(vertexOption: Option[GBVertex], degree: Long)
 
   private def mergeVertexAndDegrees(vad1: VertexDegreeRecord, vad2: VertexDegreeRecord) = {
+    //TODO: Uncommenting the require statament causes the method to break on parquet graphs. But on commenting, the output of this seems inconsistent with the equivalent titan graph.
     //require(vad1.vertexOption.isEmpty || vad2.vertexOption.isEmpty)
     val v = if (vad1.vertexOption.isDefined) vad1.vertexOption else vad2.vertexOption
     VertexDegreeRecord(v, vad1.degree + vad2.degree)
