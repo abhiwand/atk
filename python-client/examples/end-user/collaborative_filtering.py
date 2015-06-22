0d0c53b9174f5cbc881b9936d9e08cc07e506932
scatrava <serban.catrava@intel.com>
2015-06-22 12:33:45 -0700

    updated documentation & moved collaborative filtering py

---
+++ python-client/examples/end-user/collaborative_filtering.py
@@ -0,0 +1,62 @@
+#
+# Copyright (c) 2015 Intel Corporation 
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+#
+# Copyright (c) 2015 Intel Corporation 
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+
+
+#!/usr/bin/python2.7
+import intelanalytics as ia
+
+ia.connect();
+
+#the default home directory is  hdfs://user/iauser all the sample data sets are saved to hdfs://user/iauser/datasets
+dataset = r"datasets/movie_data_random.csv"
+
+#csv schema definition
+schema = [("user", str),
+          ("item", str),
+          ("rating", str),
+          ("evaluation", str)]
+
+#csv schema definition
+csv = ia.CsvFile(dataset, schema, skip_header_lines=1)
+
+print "Building data frame 'cf'"
+
+frame = ia.Frame(csv, "cf")
+
+print "Done building data frame"
+
+print "Inspecting frame 'cf'"
+
+print frame.inspect()
+
+print frame.collaborative_filtering("user", "item", "rating", "evaluation", )
