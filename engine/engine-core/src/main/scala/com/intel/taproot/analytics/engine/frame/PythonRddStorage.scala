c27055dcd026a85d8de8d229e86a0ba690481939
Todd Lisonbee <todd.lisonbee@intel.com>
2015-07-21 03:33:59 -0700

    sql.Row change introduced bug: fixing serialization

--- engine/engine-core/src/main/scala/com/intel/taproot/analytics/engine/frame/PythonRddStorage.scala
+++ engine/engine-core/src/main/scala/com/intel/taproot/analytics/engine/frame/PythonRddStorage.scala
@@ -111,7 +111,7 @@ object PythonRddStorage {
       x => {
 
         val obj = new BasicBSONObject()
-        obj.put("array", x.toSeq.map(value => value match {
+        obj.put("array", x.toSeq.toArray.map(value => value match {
           case y: ArrayBuffer[_] => iterableToBsonList(y)
           case y: Vector[_] => iterableToBsonList(y)
           case _ => value
