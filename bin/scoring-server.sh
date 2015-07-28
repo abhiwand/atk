534161ea4c4254671c2854891dd36f71e1f9d9e7
Suraj Bathija <suraj.p.bathija@intel.com>
2015-07-28 16:00:46 -0700

    scoring read from tar

--- bin/scoring-server.sh
+++ bin/scoring-server.sh
@@ -11,12 +11,14 @@ pushd $DIR/..
 pwd
 
 export HOSTNAME=`hostname`
+export CLASSPATH=$CLASSPATH:/etc/hadoop/conf
+export HADOOP_CONF_DIR="/etc/hadoop/conf"
 
 
 # NOTE: Add this parameter to Java for connecting to a debugger
 # -agentlib:jdwp=transport=dt_socket,server=n,address=localhost:5005
 
 echo java $@ -XX:MaxPermSize=256m -cp "$LAUNCHER" com.intel.taproot.analytics.component.Boot scoring-models
-java $@ -XX:MaxPermSize=256m -cp "$LAUNCHER" com.intel.taproot.analytics.component.Boot scoring-engine
+java $@ -XX:MaxPermSize=256m -cp "$LAUNCHER:/etc/hadoop/conf" com.intel.taproot.analytics.component.Boot scoring-engine
 
 popd
