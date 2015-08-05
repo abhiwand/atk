b15732802a8e3831ac60ecc56da613f737aa89d4
scatrava <serban.catrava@intel.com>
2015-08-05 09:16:17 -0700

    upodated headers

--- integration-tests/tests/user_examples_tests.py
+++ integration-tests/tests/user_examples_tests.py
@@ -1,3 +1,19 @@
+#
+# Copyright (c) 2015 Intel Corporation 
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 import unittest
 import trustedanalytics as ia
 
@@ -38,7 +54,3 @@ class UserExamples(unittest.TestCase):
         assert vars["result"]["vertex_dictionary"]["user_id"].row_count == 18
         assert vars["result"]["vertex_dictionary"]["movie_id"].row_count == 11
         assert vars["result"]["edge_dictionary"]["rating"].row_count == 20
-
-
-
-
